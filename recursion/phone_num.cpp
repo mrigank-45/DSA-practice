4b27c8bfeec3987d122b4854813ee183277e73bc
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-06-20 00:22:04 +0530

    Rat Maze recursion done

---
+++ recursion/phone_num.cpp
@@ -0,0 +1,51 @@
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. 
+// Return the answer in any order. A mapping of digits to letters (just like on the telephone buttons) is given below. 
+// Input: digits = "23"
+// Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(string digit, string output, int index, vector<string> &ans, string mapping[])
+{
+
+    // base case
+    if (index >= digit.length())
+    {
+        ans.push_back(output);
+        return;
+    }
+
+    int number = digit[index] - '0';
+    string value = mapping[number];
+
+    for (int i = 0; i < value.length(); i++)
+    {
+        output.push_back(value[i]);
+        solve(digit, output, index + 1, ans, mapping);
+        output.pop_back();
+    }
+}
+
+vector<string> letterCombinations(string digits)
+{
+    vector<string> ans;
+    if (digits.length() == 0)
+        return ans;
+    string output;
+    int index = 0;
+    string mapping[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    solve(digits, output, index, ans, mapping);
+    return ans;
+}
+
+int main()
+{
+    string s = "37";
+    vector<string> combinations = letterCombinations(s);
+    for (int i = 0; i < combinations.size(); i++)
+    {
+        cout << combinations[i] << " ";
+    }
+    cout << endl;
+}
---
+++ recursion/rat_in_a_maze.cpp
@@ -0,0 +1,76 @@
+// Print all permutations of a string
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int row, int col, vector<string> &ans, string path, vector<vector<int>> &vis, int n, vector<vector<int>> &m)
+{
+    if (row == n - 1 && col == n - 1)
+    {
+        ans.push_back(path);
+        return;
+    }
+    // move down
+    if (row + 1 < n && !vis[row + 1][col] && m[row + 1][col] == 1)
+    {
+        vis[row][col] = 1;
+        path.push_back('D');
+        solve(row + 1, col, ans, path, vis, n, m);
+        vis[row][col] = 0;
+        path.pop_back();
+    }
+    // left 
+    if (col - 1 >= 0 && !vis[row][col - 1] && m[row][col - 1] == 1)
+    {
+        vis[row][col] = 1;
+        path.push_back('L');
+        solve(row, col - 1, ans, path, vis, n, m);
+        vis[row][col] = 0;
+        path.pop_back();
+    }
+    // right 
+    if (col + 1 < n && !vis[row][col + 1] && m[row][col + 1] == 1)
+    {
+        vis[row][col] = 1;
+        path.push_back('R');
+        solve(row, col + 1, ans, path, vis, n, m);
+        vis[row][col] = 0;
+        path.pop_back();
+    }
+    // upward 
+    if (row - 1 >= 0 && !vis[row - 1][col] && m[row - 1][col] == 1)
+    {
+        vis[row][col] = 1;
+        path.push_back('U');
+        solve(row - 1, col, ans, path, vis, n, m);
+        vis[row][col] = 0;
+        path.pop_back();
+    }
+}
+
+vector<string> findPath(vector<vector<int>> m, int n)
+{
+    vector<string> ans;
+    vector<vector<int>> vis(n, vector<int>(n, 0));
+    string path = "";
+    if (m[0][0] == 1)
+        solve(0, 0, ans, path, vis, n, m);
+    return ans;
+}
+
+int main()
+{
+    int n = 4;
+    vector<vector<int>> m = {{1, 0, 0, 0},
+                             {1, 1, 0, 1},
+                             {1, 1, 0, 0},
+                             {0, 1, 1, 1}};
+
+    vector<string> s = findPath(m, n);
+    for (int i = 0; i < s.size(); i++)
+    {
+        cout << s[i] << endl;
+    }
+
+    return 0;
+}
