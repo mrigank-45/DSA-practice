cb247f60ff724fb153fd58359d9f489127e531aa
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-06-22 01:39:58 +0530

    recursion done

--- recursion/partition.cpp
+++ recursion/partition.cpp
@@ -1,3 +1,7 @@
+// Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+// Input: s = "aab"
+// Output: [["a","a","b"],["aa","b"]]
+
 #include <bits/stdc++.h>
 using namespace std;
 
--- test.cpp
+++ test.cpp
@@ -1,62 +1,23 @@
+// C++ program to demonstrate functioning of substr()
 #include <bits/stdc++.h>
 using namespace std;
 
-bool canPush(vector<vector<int>> ans, vector<int> v){
-    for (int i = 0; i < ans.size(); i++)
-    {
-        if (ans[i]==v)
-        {
-            return false;
-        }
-        
-    }
-    return true;
-    
-}
-void combination(vector<int> &candidates, int target, vector<int> currComb, int currSum, int currIndex, vector<vector<int>>& ans)
-{
-    if (currSum > target)
-    {
-        return;
-    }
-    if (currSum == target)
-    {
-        
-        sort(currComb.begin(),currComb.end());
-        if (canPush(ans, currComb))
-        {
-            ans.push_back(currComb);
-        }
-        
-        cout << endl;
-
-        return;
-    }
-
-    for (int i = currIndex +1; i < candidates.size(); i++)
-    {
-        currComb.push_back(candidates[i]);
-        currSum += candidates[i];
-        combination(candidates, target, currComb, currSum, i, ans);
-        currComb.pop_back();
-        currSum -= candidates[i];
-    }
-}
-
-vector<vector<int>> combinationSum(vector<int> &candidates, int target)
-{
-    vector<int> currComb;
-    int currSum = 0;
-    int currIndex = -1;
-    vector<vector<int>> ans;
-    combination(candidates, target, currComb, currSum, currIndex, ans);
-    return ans;
-}
-
 int main()
 {
-    vector<int> candidates = {2, 3, 6, 7};
-    int target = 7;
+    // Take any string
+    string s1 = "Geeks";
 
-    combinationSum(candidates, 7);
+    // Copy two characters of s1 (starting
+    // from position 3)
+    string a = s1.substr(0, 3);
+    string b = s1.substr(3, s1.size() - 3);
+
+    // cout << a << endl;
+    // cout << b << endl;
+
+    vector<string> ss;
+    ss.pop_back();
+    cout<<ss.size();
+
+    return 0;
 }
