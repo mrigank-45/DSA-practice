ba8b7b172103e5de793659ed10b61c0731c6969c
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-06-27 15:26:51 +0530

    stacks done

--- sample.cpp
+++ sample.cpp
@@ -8,8 +8,7 @@ int main()
     string num = "112";
     int k = 1;
 
-    string ans = removeKdigits(num, k);
-    cout << ans << endl;
+    
 
     return 0;
 }
--- stack/trap_water.cpp
+++ stack/trap_water.cpp
@@ -1,41 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h>
 
-vector<int> maxSlidingWindow(vector<int> v, int k)
-{
-    int n = v.size();
-    pair<int, int> p;
-    int max = INT_MIN;
-    for (int i = 0; i < k; i++)
-    {
-        if (v[i] >= max)
-        {
-            max = v[i];
-            p.first = i;
-            p.second = max;
-        }
-    }
-    int r = k;
-    while (r < n)
-    {
-        if (v[k] >= max)
-        {
-            max = v[k];
-            p.first = k;
-            p.second = max;
-        }
-    }
+using namespace std;
+int trap(vector < int > & arr) {
+  int n = arr.size();
+  int prefix[n], suffix[n];
+  prefix[0] = arr[0];
+  for (int i = 1; i < n; i++) {
+    prefix[i] = max(prefix[i - 1], arr[i]);
+  }
+  suffix[n - 1] = arr[n - 1];
+  for (int i = n - 2; i >= 0; i--) {
+    suffix[i] = max(suffix[i + 1], arr[i]);
+  }
+  int waterTrapped = 0;
+  for (int i = 0; i < n; i++) {
+    waterTrapped += min(prefix[i], suffix[i]) - arr[i];
+  }
+  return waterTrapped;
 }
 
-int main()
-{
-    vector<int> arr;
-    int k = 3;
-    arr = {1, 3, -1, -3, 5, 3, 6, 7};
-    vector<int> ans = maxSlidingWindow(arr, k);
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << ans[i] << " ";
-    }
-    cout << endl;
+int main() {
+  vector < int > arr;
+  arr = {0,1,0,2,1,0,1,3,2,1,2,1};
+  cout << "The water that can be trapped is " << trap(arr) << endl;
 }
