43b6b6f5ae01decb1cf6f2143cb793ff5c496750
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-07-04 00:46:12 +0530

    tree view Qs

---
+++ binary_tree/LCA.cpp
@@ -0,0 +1,44 @@
+// Lowest Common Ancister: We can also solve this by finding paths to both node and then see the first common element from back.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+class Solution
+{
+    public:
+    //Function to return the lowest common ancestor in a Binary Tree.
+    Node* lca(Node* root ,int n1 ,int n2 )
+    {
+       //base case
+        if(root == NULL)
+        {
+            return NULL;
+        }
+        
+        if(root->data == n1 || root->data == n2) 
+        {
+            return root;
+        }
+        
+        Node* leftAns = lca(root->left, n1, n2);
+        Node* rightAns = lca(root->right, n1, n2);
+        
+        if(leftAns != NULL && rightAns != NULL) 
+        {
+            return root;
+        }
+        else if(leftAns != NULL && rightAns == NULL)
+            return leftAns;
+        else if(leftAns == NULL && rightAns != NULL)
+            return rightAns;
+        else
+            return NULL;
+    }
+};
---
+++ binary_tree/burn_tree.cpp
@@ -0,0 +1,97 @@
+// Minimum time to burn a tree (gfg)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+class Solution
+{
+public:
+    Node *createParentMapping(Node *root, int target, map<Node *, Node *> &nodeToParent)
+    {
+        Node *res = NULL;
+        nodeToParent[root] = NULL;
+        queue<Node *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            Node *front = q.front();
+            q.pop();
+            if (front->data == target)
+            {
+                res = front;
+            }
+            if (front->left)
+            {
+                nodeToParent[front->left] = front;
+                q.push(front->left);
+            }
+            if (front->right)
+            {
+                nodeToParent[front->right] = front;
+                q.push(front->right);
+            }
+        }
+        return res;
+    }
+    int burnTree(Node *root, map<Node *, Node *> &nodeToParent)
+    {
+        map<Node *, bool> visited;
+        queue<Node *> q;
+        q.push(root);
+        visited[root] = true;
+        int time = 0;
+        while (!q.empty())
+        {
+            bool flag = 0;               // addition hole flag true kore dibo
+            int size = q.size();
+            for (int i = 0; i < size; i++)
+            {
+                // process neighbouring nodes
+                Node *front = q.front();
+                q.pop();
+                if (front->left && !visited[front->left])
+                {
+                    flag = 1;
+                    q.push(front->left);
+                    visited[front->left] = 1;
+                }
+                if (front->right && !visited[front->right])
+                {
+                    flag = 1;
+                    q.push(front->right);
+                    visited[front->right] = 1;
+                }
+                if (nodeToParent[front] && !visited[nodeToParent[front]])
+                {
+                    flag = 1;
+                    q.push(nodeToParent[front]);
+                    visited[nodeToParent[front]] = 1;
+                }
+            }
+            if (flag == 1)
+            {
+                time++;
+            }
+        }
+        return time;
+    }
+    int minTime(Node *root, int target)
+    {
+        // algo
+        // 1=create nodetoparent mapping
+        // 2=find target node
+        // 3=burn the tree
+        int time = 0;
+        map<Node *, Node *> nodeToParent;
+        Node *targetNode = createParentMapping(root, target, nodeToParent);
+        time = burnTree(targetNode, nodeToParent);
+        return time;
+    }
+};
