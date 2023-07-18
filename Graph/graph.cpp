3cf465a2776b0d9ee5583497234ca468356b215f
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-07-18 19:55:39 +0530

    started graphs

---
+++ Graph/graph.cpp
@@ -0,0 +1,26 @@
+// create a graph using Adjacency list and print them. n->nodes,m->edges.
+#include <bits/stdc++.h>
+using namespace std;
+
+void createAndPrintAdjacency(int n, int m, vector<vector<int>> &edges)
+{
+    map<int, vector<int>> adjList;
+    for (int i = 0; i < m; i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+
+    for (auto it : adjList)
+    {
+        cout << it.first << "-> ";
+        for (int i = 0; i < it.second.size(); i++)
+        {
+            cout << it.second[i] << " ";
+        }
+        cout << endl;
+    }
+}
