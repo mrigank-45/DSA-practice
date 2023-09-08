dc8eda898079b00f112ec422845b867279c9f9cb
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-09-08 17:03:27 +0530

    common substring in dp

--- Dynamic programming/longestSubsequence.cpp
+++ Dynamic programming/longestSubsequence.cpp
@@ -50,6 +50,7 @@ public:
         }
         return dp[i][j] = ans;
     }
+    
     int tabulation(string &s1, string &s2)
     {
         vector<vector<int>> dp(s1.size() + 1, vector<int>(s2.size() + 1, 0));
--- Dynamic programming/longestSubstring.cpp
+++ Dynamic programming/longestSubstring.cpp
@@ -1,3 +1,7 @@
+// Longest Common Substring (codeStudio)
+// just some change in approach in longestSubsequence.cpp tabulation solution and we get this
+// we cannot solve it in memo as 3 variables will be changing (i,j,count).
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,7 +10,7 @@ class Solution
 public:
     int tabulation(string &str1, string &str2)
     {
-        // Creating 'DP' table.
+        // Creating 'DP' table, initialize as 0
         vector<vector<int>> dp(str1.size() + 1, vector<int>(str2.size() + 1, 0));
 
         // Variable to store the answer.
@@ -26,6 +30,7 @@ public:
                 }
                 else
                 {
+                    // set as 0 as no substring can be formed
                     dp[i][j] = 0;
                 }
             }
