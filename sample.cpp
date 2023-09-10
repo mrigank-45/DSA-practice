7549d82f7087e054e54e3efdfc0cfe4e4ed77856
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-09-10 20:40:03 +0530

    divisible Qs practice

--- sample.cpp
+++ sample.cpp
@@ -4,33 +4,31 @@ using namespace std;
 class Solution
 {
 public:
-    int tabulation(string &str1, string &str2)
+    vector<int> solve(int curr, int prev, vector<int> &nums)
     {
-        // Creating 'DP' table.
-        vector<vector<int>> dp(str1.size() + 1, vector<int>(str2.size() + 1, 0));
-
-        // Variable to store the answer.
-        int ans = 0;
-
-        // Iterate through first string.
-        for (int i = str1.length() - 1; i >= 0; i--)
+        if (curr == nums.size())
         {
-            // Iterate through second string.
-            for (int j = str2.size() - 1; j >= 0; j--)
-            {
-                // If characters are same at 'i' and 'j' then add 1 to the answer obtained from rest of the string.
-                if (str1[i] == str2[j])
-                {
-                    dp[i][j] = dp[i + 1][j + 1] + 1;
-                    ans = max(ans, dp[i][j]);
-                }
-                else
-                {
-                    dp[i][j] = 0;
-                }
-            }
+            return {};
         }
-        // Return the answer.
-        return ans;
+
+        vector<int> take({});
+        vector<int> notTake = solve(curr + 1, prev, nums);
+        if (prev == -1 || (nums[curr] % nums[prev] == 0 || nums[prev] % nums[curr] == 0))
+        {
+            take = solve(curr + 1, curr, nums);
+            take.insert(take.begin(), nums[curr]);
+        }
+        if(take.size() > notTake.size())
+        {
+            return take;
+        }
+        return notTake;
+    }
+
+    vector<int> largestDivisibleSubset(vector<int> &nums)
+    {
+        // vector<vector<int>> dp(nums.size() + 1, vector<int>(nums.size() + 1, -1));
+
+        return solve(0, -1, nums);
     }
 };
--- test.cpp
+++ test.cpp
@@ -2,67 +2,37 @@
 
 using namespace std;
 
-int solve(vector<int> &price, int ind, int N, vector<vector<int>> &dp)
+void solve(vector<int> &nums, int idx, vector<int> &temp)
 {
-    if (ind == 0)
+    if (idx == nums.size())
     {
-        return N * price[0];
+        if (temp.size() > ans.size())
+            ans = temp;
+        return;
     }
-
-    if (dp[ind][N] != -1)
-        return dp[ind][N];
-
-    int notTaken = 0 + solve(price, ind - 1, N, dp);
-
-    int taken = INT_MIN;
-    int rodLength = ind + 1;
-    if (rodLength <= N)
-        taken = price[ind] + solve(price, ind, N - rodLength, dp);
-
-    return dp[ind][N] = max(notTaken, taken);
-}
-
-int tabulation(vector<int> &price, int N)
-{
-    vector<vector<int>> dp(N + 1, vector<int>(N + 1, -1));
-
-    for (int i = 0; i < N; i++)
+    if (temp.size() == 0 || nums[idx] % temp.back() == 0)
     {
-        for (int j = 0; j <= N; j++)
-        {
-            if (i == 0)
-            {
-                dp[i][j] = j * price[i];
-            }
-            else
-            {
-                int notTaken = dp[i - 1][j];
-                int taken = INT_MIN;
-                int rodLength = i + 1;
-                if (rodLength <= j)
-                    taken = price[i] + dp[i][j - rodLength];
-
-                dp[i][j] = max(notTaken, taken);
-            }
-        }
+        temp.push_back(nums[idx]);
+        solve(nums, idx + 1, temp);
+        temp.pop_back();
     }
-
-    return dp[N - 1][N];
+    solve(nums, idx + 1, temp);
 }
-
-int cutRod(vector<int> &price, int N)
+vector<int> largestDivisibleSubset(vector<int> &nums)
 {
-
-    vector<vector<int>> dp(N, vector<int>(N + 1, -1));
-    return solve(price, N - 1, N, dp);
+    vector<int> temp;
+    sort(nums.begin(), nums.end());
+    vector<int> dp(nums.size(), -1);
+    solve(nums, 0, temp);
+    return ans;
 }
 
-int main()
-{
+// int main()
+// {
 
-    vector<int> price = {2, 5, 7, 8, 10};
+//     vector<int> price = {2, 5, 7, 8, 10};
 
-    int n = price.size();
+//     int n = price.size();
 
-    cout << "The Maximum price generated is " << cutRod(price, n);
-}
+//     cout << "The Maximum price generated is " << cutRod(price, n);
+// }
