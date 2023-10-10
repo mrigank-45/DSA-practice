00574166a5887e7643f2b691d125763f8d815abf
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-10-10 20:09:37 +0530

    recursion revision

--- recursion/all_permutations.cpp
+++ recursion/all_permutations.cpp
@@ -3,27 +3,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void permute(string& a, int l, int r)
+void permute(string &a, int l, int r)
 {
     // Base case
-    if (l>= r)
+    if (l >= r)
         cout << a << endl;
-    else {
+    else
+    {
         // Permutations made
-        for (int i = l; i <= r; i++) {
- 
+        for (int i = l; i <= r; i++)
+        {
+
             // Swapping done
             swap(a[l], a[i]);
- 
+
             // Recursion called
             permute(a, l + 1, r);
- 
+
             // backtrack
             swap(a[l], a[i]);
         }
     }
 }
- 
+
 int main()
 {
     string str = "abc";
--- recursion/phone_num.cpp
+++ recursion/phone_num.cpp
@@ -1,5 +1,5 @@
-// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. 
-// Return the answer in any order. A mapping of digits to letters (just like on the telephone buttons) is given in the Qs. 
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+// Return the answer in any order. A mapping of digits to letters (just like on the telephone buttons) is given in the Qs.
 // Input: digits = "23"
 // Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
 
