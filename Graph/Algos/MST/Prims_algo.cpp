2a6e4f6647f9f1d41583e88f36c6c144a2c2b7a8
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-10-16 11:52:15 +0530

    MST Revision

--- Graph/Algos/MST/Prims_algo.cpp
+++ Graph/Algos/MST/Prims_algo.cpp
@@ -1,6 +1,7 @@
 // Used to find minimum spanning tree (applicable to only connected undirected graphs with weights)
 
-// ALGO : Use 3 structure(weight array, inMST array, parent array) and a min heap to get min of the weight array, then follow 3 steps till heap is not empty. 1-> extract top 
+// ALGO : Use 3 structure(weight array, inMST array, parent array) and a min heap to get min of the weight array, then follow 3 steps
+// till heap is not empty. 1-> extract top
 // 2-> mark MST array true 3-> see if weight can be updated of neighbors, if yes, update weight, parent and insert in heap as well.
 
 //  Time Complexity = O(M * log(N))
@@ -40,7 +41,7 @@ vector<pair<pair<int, int>, int>> primsMST(vector<pair<int, int>> *adjList, int 
     // everytime weight array is updated, element is inserted to the heap.
     while (!pq.empty())
     {
-        // extract node fron heap (step 1)
+        // extract node from heap (step 1)
         int u = pq.top().second;
         pq.pop();
 
@@ -50,8 +51,8 @@ vector<pair<pair<int, int>, int>> primsMST(vector<pair<int, int>> *adjList, int 
         // Explore all adjacent of u. (step 3)
         for (auto x : adjList[u])
         {
-            int v = x.first;
-            int wt = x.second;
+            int v = x.second;
+            int wt = x.first;
 
             // If v is not in mst and weight of (u,v) is smaller then the current weight of v.
             if (!inMST[v] && weight[v] > wt)
@@ -68,7 +69,7 @@ vector<pair<pair<int, int>, int>> primsMST(vector<pair<int, int>> *adjList, int 
     }
 
     // store ans using parent and weight array.
-    vector<pair<pair<int, int>, int>> result;
+    vector<pair<pair<int, int>, int>> result; // {{node1,node2},weight}
     for (int i = 1; i < n; i++)
     {
         result.push_back({{min(parent[i] + 1, i + 1), max(parent[i] + 1, i + 1)}, weight[i]});
@@ -76,12 +77,13 @@ vector<pair<pair<int, int>, int>> primsMST(vector<pair<int, int>> *adjList, int 
 
     return result;
 }
-vector<pair<pair<int, int>, int>> calculatePrimsMST(int n, int m, vector<pair<pair<int, int>, int>> &g)
-{
 
+vector<pair<pair<int, int>, int>> calculatePrimsMST(int n, int m, vector<pair<pair<int, int>, int>> &g) // {{node1,node2},weight}
+{
+    // {node,weight}
     vector<pair<int, int>> *adjList = new vector<pair<int, int>>[n];
 
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m; i++) // doing -1 if it is not already a 0 based indexing
     {
         adjList[g[i].first.first - 1].push_back(make_pair(g[i].first.second - 1, g[i].second));
         adjList[g[i].first.second - 1].push_back(make_pair(g[i].first.first - 1, g[i].second));
