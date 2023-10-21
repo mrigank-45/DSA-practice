69f8b92d8d78cfc86e6294a1455ba9e5a72d3fc0
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-10-21 16:51:24 +0530

    DP Revision done

--- Dynamic programming/LIS.cpp
+++ Dynamic programming/LIS.cpp
@@ -15,6 +15,7 @@ public:
         // doing prev + 1 becs we have initialised prev with -1
         if (dp[curr][prev + 1] != -1)
             return dp[curr][prev + 1];
+
         int take = 0;
         int notTake = solve(curr + 1, prev, nums, dp);
         if (prev == -1 || nums[curr] < nums[prev])
