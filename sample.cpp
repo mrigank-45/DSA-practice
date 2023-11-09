0ad74b33b5913e652a133b008c1f5e9388675d59
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-11-09 21:00:55 +0530

    special number

--- sample.cpp
+++ sample.cpp
@@ -4,37 +4,67 @@ using namespace std;
 class Solution
 {
 public:
-    int countSymmetricIntegers(int low, int high)
+    int minimumOperations(string num)
     {
-        int c = 0;
-        for (int i = low; i <= high; i++)
+        // 52,57,05,00
+        int c1 = 0, c2 = 0, c3 = 0, c4 = 0;
+        cout<<"size: "<<num.size()<<endl;
+
+        for (int i = num.size() - 1; i >= 0; i--)
         {
-            if ((int)floor(log10(i) + 1) % 2) // no. of digits is odd
-                continue;
-            else
+            bool flag = false;
+
+            if ((num[i] == '5') && c1 == 0)
             {
-                int sum = 0;
-                int n = i;
-                int x = floor(log10(i) + 1);
-                int y = x / 2;
-                while (n != 0 && y--)
-                {
-                    sum = sum + n % 10;
-                    n = n / 10;
-                }
-                y = x / 2;
-                int sum1 = 0;
-                while (n != 0 && y--)
-                {
-                    sum1 = sum1 + n % 10;
-                    n = n / 10;
-                }
-                if (sum1 == sum)
-                {
-                    c++;
-                }
+                c1++;
+                cout<<"i: "<<i<<endl;
+                cout << "c1: " << c1 << " c2: " << c2 << " c3: " << c3 << " c4: " << c4 << endl;
+            }
+            if ((num[i] == '5') && c2 == 0)
+            {
+                c2++;
+                cout<<"i: "<<i<<endl;
+                cout << "c1: " << c1 << " c2: " << c2 << " c3: " << c3 << " c4: " << c4 << endl;
+            }
+            if ((num[i] == '0') && c3 == 0)
+            {
+                c3++;
+                cout<<"i: "<<i<<endl;
+                cout << "c1: " << c1 << " c2: " << c2 << " c3: " << c3 << " c4: " << c4 << endl;
+            }
+            if ((num[i] == '0') && c4 == 0)
+            {
+                c4++;
+                cout<<"i: "<<i<<endl;
+                cout << "c1: " << c1 << " c2: " << c2 << " c3: " << c3 << " c4: " << c4 << endl;
+                flag = true;
+            }
+
+            if ((num[i] == '2') && c1 == 1)
+            {
+                return num.size() - i - 2;
+            }
+            if ((num[i] == '7') && c2 == 1)
+            {
+                return num.size() - i - 2;
+            }
+            if ((num[i] == '5') && c3 == 1)
+            {
+                return num.size() - i - 2;
+            }
+            if ((num[i] == '0') && c4 == 1 && flag == false)
+            {
+                return num.size() - i - 2;
             }
         }
-        return c;
+
+        if (c3 == 1 || c4 == 1)
+        {
+            return num.size() - 1;
+        }
+        else
+        {
+            return num.size();
+        }
     }
 };
