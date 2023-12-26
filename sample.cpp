cba00128fe4c2c49fe999fd324df2234dc0d9505
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-12-26 15:02:59 +0530

    k avoding array

--- sample.cpp
+++ sample.cpp
@@ -4,54 +4,39 @@ using namespace std;
 class Solution
 {
 public:
-    long long maxSum(vector<int> &nums, int m, int k)
+    int minimumSum(int n, int k)
     {
-        map<int, int> frq;
-        int cnt = 0; 
-        long long int sum = 0;
-        long long int ans = 0;
-
-        for (int i = 0; i < k; i++)
-        {
-            if (frq[nums[i]] == 0)
-            {
-                cnt++;
-            }
-            frq[nums[i]]++;
-            sum += nums[i];
-        }
-
+        int sum = 0;
         int i = 0;
-        int j = k - 1;
-
-        while (j < nums.size())
+        while (n)
         {
-            if (cnt >= m)
-            {
-                ans = max(ans, sum);
-            }
-
-            if (frq[nums[i]] == 1)
-            {
-                cnt--;
-            }
-            frq[nums[i]]--;
-
-            sum -= nums[i];
             i++;
-            j++;
-            if (j == nums.size())
+            cout << "n: " << n << endl;
+            cout << "i: " << i << endl;
+            cout << "sum: " << sum << endl;
+
+            if (i == k / 2)
             {
-                break;
+                if (k % 2 == 0)
+                {
+                    sum += i;
+                    n--;
+                    i += k / 2 - 1;
+                    continue;
+                }
+                else
+                {
+                    sum += i;
+                    n--;
+                    i += k / 2;
+                    continue;
+                }
             }
-            if (frq[nums[j]] == 0)
-            {
-                cnt++;
-            }
-            frq[nums[j]]++;
-            sum += nums[j];
+
+            sum += i;
+            n--;
         }
 
-        return ans;
+        return sum;
     }
 };
