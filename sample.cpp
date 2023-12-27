90bb78f59c565b62922fdf2d415bbc09da1c14f3
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-12-27 22:03:58 +0530

    longest equal subarray

--- sample.cpp
+++ sample.cpp
@@ -4,85 +4,32 @@ using namespace std;
 class Solution
 {
 public:
-    int solve(vector<vector<int>> &offers, int i, vector<int> &DP)
+    int longestEqualSubarray(vector<int> &nums, int k)
     {
-        // base case
-        if (i == offers.size())
+        map<int, int> m;
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            m[nums[i]]++;
+        }
+
+        int maxi = INT_MIN;
+
+        for (auto it : m)
+        {
+            cout << it.first << " " << it.second << endl;
+            maxi = max(maxi, it.second);
+        }
+
+        if ((n - maxi) > k)
         {
             return 0;
         }
-
-        // check if result for current state is already memoized
-        if (DP[i] != -1)
+        else
         {
-            return DP[i];
+            return maxi;
         }
-
-        // include
-        int prev = offers[i][1];
-        int j = i + 1;
-        while (j < offers.size() && offers[j][0] <= prev)
-        {
-            j++;
-        }
-        int includeProfit = solve(offers, j, DP) + offers[i][2];
-
-        // exclude
-        int excludeProfit = solve(offers, i + 1, DP);
-
-        // calculate the maximum profit for the current state
-        int maxProfit = max(includeProfit, excludeProfit);
-
-        // memoize the result
-        DP[i] = maxProfit;
-
-        return maxProfit;
-    }
-
-    int tabulation(vector<vector<int>> &offers)
-    {
-        vector<int> DP(offers.size() + 1, -1);
-
-        for (int i = offers.size() - 1; i >= 0; i--)
-        {
-            // exclude
-            int excludeProfit = DP[i + 1];
-
-            // include
-            int prev = offers[i][1];
-            int j = i + 1;
-            while (j < offers.size() && offers[j][0] <= prev)
-            {
-                j++;
-            }
-            int includeProfit = -1;
-            if (j == i + 1)
-            {
-             includeProfit = DP[j] + offers[i][2];
-            }
-            else {
-                includeProfit = DP[j];
-            }
-            
-            
-            // calculate the maximum profit for the current state
-            int maxProfit = max(includeProfit, excludeProfit);
-
-            // memoize the result
-            DP[i] = maxProfit;
-        }
-
-        return DP[0];
-    }
-
-    int maximizeTheProfit(int n, vector<vector<int>> &offers)
-    {
-        sort(offers.begin(), offers.end());
-
-        vector<int> DP(offers.size() + 1, -1);
-
-        int result = solve(offers, 0, DP);
-
-        return result;
     }
 };
