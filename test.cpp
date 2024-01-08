b3d0db5f81145cd0a9b02ce7bb6b1914192488ce
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-01-09 02:40:16 +0530

    complete subarrays

--- test.cpp
+++ test.cpp
@@ -1,32 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Solution
 {
 public:
-    int minimumSeconds(vector<int> &nums)
+    int solve(int i, int j, vector<int> nums, map<int, int> mp, int distict, int total)
     {
-        unordered_map<int, vector<int>> mp;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            mp[nums[i]].push_back(i);
-        }
+        
+    }
+    int countCompleteSubarrays(vector<int> &nums)
+    {
 
-        int ans = nums.size(), x = 0;
-
-        for (auto &it : mp)
-        {
-            x = it.second[0];
-            int y = nums.size() - it.second.back() - 1;
-            x = x + y;
-            x = (x + 1) / 2;
-
-            for (int i = 0; i < it.second.size() - 1; i++)
-            {
-                x = max(x, (it.second[i + 1] - it.second[i]) / 2);
-            }
-            ans = min(ans, x);
-        }
-        return ans;
     }
 };
