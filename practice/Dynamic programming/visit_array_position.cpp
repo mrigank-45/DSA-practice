5110ef9ed6255cf0a4770ade3ee527c73e74caa4
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-01-13 16:41:03 +0530

    time complexity reduced

--- practice/Dynamic programming/visit_array_position.cpp
+++ practice/Dynamic programming/visit_array_position.cpp
@@ -1,5 +1,5 @@
 // Visit Array Positions to Maximize Score
-
+// need to take 2nd parameter(parity) as 1 or 0 only, not prev index as it will reduce time complexity
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -39,67 +39,6 @@ public:
         }
         return dp[0][nums[0] % 2];
     }
-
-    // gives TLE, even though same solution
-    // long long solve(vector<int> &nums, int x, int prev, int i, vector<vector<int>> dp)
-    // {
-    //     if (i >= nums.size())
-    //     {
-    //         return 0;
-    //     }
-
-    //     if(dp[prev][i] != -1){
-    //         return dp[prev][i];
-    //     }
-
-    //     long long include = 0, exclude = 0;
-
-    //     // parity check
-    //     if (nums[prev] % 2 == nums[i] % 2)
-    //     {
-    //         include = nums[i] + solve(nums, x, i, i + 1, dp);
-    //     }
-    //     else
-    //     {
-    //         include = nums[i] - x + solve(nums, x, i, i + 1, dp);
-    //     }
-    //     exclude = solve(nums, x, prev, i + 1, dp);
-
-    //     return dp[prev][i] = max(include, exclude);
-    // }
-
-    // long long tabulation1(vector<int> &nums, int x)
-    // {
-    //     // DP array
-    //     vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), -1));
-        
-    //     // base case 
-    //     for (int i = 0; i < nums.size() + 1; i++)
-    //     {
-    //         dp[i][nums.size()] = 0;
-    //     }
-
-    //     for(int i = nums.size() - 1; i >= 0; i--){
-    //         for(int j = nums.size() - 1; j >= 1; j--){
-    //             long long include = 0, exclude = 0;
-
-    //             // parity check
-    //             if (nums[i] % 2 == nums[j] % 2)
-    //             {
-    //                 include = nums[j] + dp[j][j + 1];
-    //             }
-    //             else
-    //             {
-    //                 include = nums[j] - x + dp[j][j + 1];
-    //             }
-    //             exclude = dp[i][j + 1];
-
-    //             dp[i][j] = max(include, exclude);
-    //         }
-    //     }
-
-    //     return dp[0][1];
-    // }
 
     long long maxScore(vector<int> &nums, int x)
     {
