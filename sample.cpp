0e3d201f99b06e8630f942259a03a0657ee08669
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-02-08 02:29:54 +0530

    Shortest and Lexicographically Smallest Beautiful String

--- sample.cpp
+++ sample.cpp
@@ -4,31 +4,68 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> findIndices(vector<int> &nums, int indexDifference, int valueDifference)
+    bool isLexicographicallySmaller(string a, string b)
     {
-        int n = nums.size();
-
-        vector<int> ans;
-
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < a.length(); ++i)
         {
-            for (int j = i; j < n; j++)
+            if (a[i] == b[i])
             {
-                if ((j - i >= indexDifference) && (abs(nums[j] - nums[i]) >= valueDifference))
-                {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
+                continue;
+            }
+            else if (a[i] < b[i])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
 
-        if (ans.size() == 0)
+        return false;
+    }
+
+    string shortestBeautifulSubstring(string s, int k)
+    {
+        vector<int> index;
+
+        for (int i = 0; i < s.size(); i++)
         {
-            ans.push_back(-1);
-            ans.push_back(-1);
+            if (s[i] == '1')
+            {
+                index.push_back(i);
+            }
         }
 
-        return ans;
+        if (k > index.size())
+        {
+            return "";
+        }
+
+        int cnt = INT_MAX;
+        pair<int, int> p = {0, 0};
+
+        int l = 0;
+        int r = k - 1;
+
+        while (r < index.size())
+        {
+            if (index[r] - index[l] + 1 < cnt)
+            {
+                cnt = index[r] - index[l] + 1;
+                p = {index[l], index[r]};
+            }
+            else if (index[r] - index[l] + 1 == cnt)
+            {
+                if (isLexicographicallySmaller(s.substr(index[l], index[r] - index[l] + 1), s.substr(p.first, p.second - p.first + 1)))
+                {
+                    p = {index[l], index[r]};
+                }
+            }
+            l++;
+            r++;
+        }
+
+        return s.substr(p.first, p.second - p.first + 1);
     }
 };
--- test.cpp
+++ test.cpp
@@ -1,75 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+bool isLexicographicallySmaller(string a, string b)
 {
-public:
-    // Create an unordered map to store the count of visited nodes for each node.
-    unordered_map<int, int> mp;
-
-    // Depth-First Search (DFS) function to count visited nodes and detect loops.
-    int dfs(vector<vector<int>> &graph, int src, int &loopEnd)
+    for (int i = 0; i < a.length(); ++i)
     {
-        // Check if we've visited this node before.
-        if (mp.find(src) != mp.end())
+        if (a[i] <= b[i])
         {
-            loopEnd = src;  // Mark the loop end if we encounter a previously visited node.
-            return mp[src]; // Return the count of visited nodes from this node.
+            continue;
         }
-
-        mp[src] = 0;   // Mark the current node as visited.
-        int count = 1; // Initialize the count of visited nodes from this node to 1.
-
-        // Recursively traverse all neighbors of the current node.
-        for (auto x : graph[src])
+        else if (a[i] > b[i])
         {
-            count += dfs(graph, x, loopEnd); // Update the count by recursively visiting neighbors.
+            return false;
         }
-
-        mp[src] = count; // Update the count of visited nodes for the current node.
-
-        // If we have detected a loop, update the counts for all nodes in the loop.
-        if (src == loopEnd)
-        {
-            int s = src;
-            do
-            {
-                mp[s] = count;   // Set the count for nodes in the loop.
-                s = graph[s][0]; // Move to the next node in the loop.
-            } while (s != loopEnd);
-        }
-
-        return count; // Return the total count of visited nodes from this node.
     }
 
-    // Main function to count visited nodes for each node in the given graph.
-    vector<int> countVisitedNodes(vector<int> &edges)
-    {
-        int n = edges.size();
-        vector<vector<int>> graph(n);
+    return true;
+}
 
-        // Create the graph using the given edges.
-        for (int i = 0; i < n; i++)
-        {
-            graph[i].push_back(edges[i]);
-        }
+int main()
+{
 
-        vector<int> ans;
-
-        // Iterate through all nodes and count visited nodes for each node.
-        for (int i = 0; i < n; i++)
-        {
-            if (mp.find(i) != mp.end())
-            {
-                ans.push_back(mp[i]); // If already computed, push the count to the answer.
-                continue;
-            }
-
-            int loopEnd = -1;                   // Initialize the loopEnd variable to -1.
-            int count = dfs(graph, i, loopEnd); // Count visited nodes starting from this node.
-            ans.push_back(count);               // Push the count to the answer vector.
-        }
-
-        return ans; // Return the vector containing counts of visited nodes for each node.
-    }
-};
+    cout<<isLexicographicallySmaller("10101101101111","11101011011011");
+    return 0;
+}
