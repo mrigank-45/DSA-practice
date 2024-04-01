077f28d612a6dcea90e32ba14355da8b7db98569
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-04-01 21:45:01 +0530

    Minimum Increment Operations to Make Array Beautiful

---
+++ Basic_maths/max.cpp
@@ -0,0 +1,2 @@
+// INT_MAX for int
+// 1e18 (INT_MAX) for long long int
--- sample.cpp
+++ sample.cpp
@@ -4,66 +4,55 @@ using namespace std;
 class Solution
 {
 public:
-    long long minSum(vector<int> &nums1, vector<int> &nums2)
+    long long solve(int i, int n, vector<long long> &diff, vector<long long> &dp)
     {
-        long long int val1 = 0, val2 = 0, flag1 = 0, flag2 = 0;
-
-        for (long long int i = 0; i < nums1.size(); i++)
+        if (i > n - 3)
         {
-            if (nums1[i] == 0)
+            return 0;
+        }
+        if (dp[i] != -1)
+        {
+            return dp[i];
+        }
+
+        long long take = 1e18;
+
+        for (int c = i; c < n && c < i + 3; c++)
+        {
+
+            long long temp = (diff[c] + solve(c + 1, n, diff, dp));
+
+            take = min(take, temp);
+        }
+
+        return dp[i] = take;
+    }
+
+    long long minIncrementOperations(vector<int> &nums, int k)
+    {
+        if (k == 0)
+        {
+            return 0;
+        }
+
+        int n = nums.size();
+
+        vector<long long> diff;  // precompute the differnce of each element with k
+
+        for (auto i : nums)
+        {
+            if (i < k)
             {
-                val1++;
-                flag1 = 1;
+                diff.push_back(k - i);
             }
             else
             {
-                val1 = val1 + nums1[i];
+                diff.push_back(0);
             }
         }
 
-        for (long long int i = 0; i < nums2.size(); i++)
-        {
-            if (nums2[i] == 0)
-            {
-                val2++;
-                flag2 = 1;
-            }
-            else
-            {
-                val2 = val2 + nums2[i];
-            }
-        }
+        vector<long long> dp(n, -1);
 
-        if (val1 == val2)
-        {
-            return val1;
-        }
-        else
-        {
-            if (val1 < val2)
-            {
-                if (flag1 == 1)
-                {
-                    return val2;
-                }
-                else
-                {
-                    return -1;
-                }
-            }
-            else
-            {
-                if (flag2 == 1)
-                {
-                    return val1;
-                }
-                else
-                {
-                    return -1;
-                }
-            }
-        }
-
-        return -1;
+        return solve(0, n, diff, dp);
     }
 };
