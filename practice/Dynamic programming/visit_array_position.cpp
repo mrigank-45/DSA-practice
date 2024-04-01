60ad736e2a9e1be20f4a96b62cacc238f05df6c5
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-04-01 19:22:52 +0530

    dp revision

--- practice/Dynamic programming/visit_array_position.cpp
+++ practice/Dynamic programming/visit_array_position.cpp
@@ -6,20 +6,21 @@ using namespace std;
 class Solution
 {
 public:
-    long long memo(int ind, vector<int> &nums, int x, int parity, vector<vector<long long>> &dp)
+    long long solve(int ind, vector<int> &nums, int x, int parity, vector<vector<long long>> &dp)
     {
         if (ind == nums.size())
             return 0;
         if (dp[ind][parity] != -1)
             return dp[ind][parity];
-        long long ntake = memo(ind + 1, nums, x, parity, dp);
+        long long ntake = solve(ind + 1, nums, x, parity, dp);
         long long take = 0;
         if (nums[ind] % 2 == parity)
-            take = nums[ind] + memo(ind + 1, nums, x, parity, dp);
+            take = nums[ind] + solve(ind + 1, nums, x, parity, dp);
         else
-            take = nums[ind] - x + memo(ind + 1, nums, x, nums[ind] % 2, dp);
+            take = nums[ind] - x + solve(ind + 1, nums, x, nums[ind] % 2, dp);
         return dp[ind][parity] = max(take, ntake);
     }
+    
     long long tabulation(vector<int> &nums, int x)
     {
         int n = nums.size();
@@ -45,7 +46,7 @@ public:
         long long n = nums.size();
         vector<vector<long long>> dp(n, vector<long long>(2, -1));
         int parity = nums[0] % 2;
-        // return nums[0] + memo(1,nums,x,parity,dp);
-        return tabulation(nums,x);
+        // return nums[0] + solve(1,nums,x,parity,dp);
+        return tabulation(nums, x);
     }
 };
