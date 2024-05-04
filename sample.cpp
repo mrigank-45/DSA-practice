9f2c43c87abde400bf2b6ca63286afcf09f6e6dc
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-05-05 01:44:38 +0530

    Minimized Maximum of Products Distributed to Any Store

--- sample.cpp
+++ sample.cpp
@@ -4,54 +4,44 @@ using namespace std;
 class Solution
 {
 public:
-
-    int numSubarrayProductLessThanK(vector<int> &nums, int k)
+    bool possible(int mid, vector<int> &quantities, int n)
     {
-        int n = nums.size();
-
-        int ans = 0;
-        int l = 0, r = 0;
-
-        long long int temp = nums[0];
-
-        while (l < n)
+        if(mid == 0){
+            return false;
+        }
+        int m = quantities.size();
+        int sum = 0;
+        for (int i = 0; i < m; i++)
         {
-            if (l == n)
-            {
-                break;
-            }
-            if (temp < (long long int)k)
-            {
-                ans++;
-                r++;
-                if (r < n)
-                {
-                    temp = temp * nums[r];
-                }
+            sum += quantities[i]/ mid + (quantities[i] % mid != 0 ? 1 : 0);
+        }
+        return sum <= n;
+    }
 
-                if (r == n)
-                {
-                    l++;
-                    r = l;
-                    if (l < n)
-                    {
-                        temp = nums[l];
-                    }
-                    continue;
-                }
+    int minimizedMaximum(int n, vector<int> &quantities)
+    {
+        int maxi = *max_element(quantities.begin(), quantities.end());
+
+        int s = 0, e = maxi;
+        int mid = (s + e) / 2;
+        int ans = 0;
+        while (s < e)
+        {
+            if (possible(mid, quantities, n))
+            {
+                e = mid;
+                ans = mid;
             }
             else
             {
-                l++;
-                r = l;
-                if (l < n)
-                {
-                    temp = nums[l];
-                }
-                continue;
+                s = mid + 1;
             }
-        }
 
+            mid = (s + e) / 2;
+        }
+        if(ans == 0){
+            return maxi;
+        }
         return ans;
     }
 };
