07b0a36284a6f04158aba4e88ef1bdad044fa83b
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-05-06 20:43:43 +0530

    Nearest smaller tower GFG

--- sample.cpp
+++ sample.cpp
@@ -4,35 +4,84 @@ using namespace std;
 class Solution
 {
 public:
-    // Function to find the minimum number of platforms required at the
-    // railway station such that no train waits.
-    int findPlatform(int arr[], int dep[], int n)
+    vector<pair<int, int>> nextSmallerElement1(vector<int> &arr, int n)
     {
-        vector<pair<int, int>> v;
+        stack<pair<int, int>> s;
+        s.push({-1, -1});
+        vector<pair<int, int>> ans(n);
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            int curr = arr[i];
+            while (s.top().first >= curr)
+            {
+                s.pop();
+            }
+            ans[i] = s.top();
+            s.push({curr, i});
+        }
+        return ans;
+    }
+    vector<pair<int, int>> nextSmallerElement2(vector<int> &arr, int n)
+    {
+        stack<pair<int, int>> s;
+        s.push({-1, -1});
+        vector<pair<int, int>> ans(n);
+
+        for (int i = 0; i <= n - 1; i++)
+        {
+            int curr = arr[i];
+            while (s.top().first >= curr)
+            {
+                s.pop();
+            }
+            ans[i] = s.top();
+            s.push({curr, i});
+        }
+        return ans;
+    }
+
+    vector<int> nearestSmallerTower(vector<int> arr)
+    {
+        int n = arr.size();
+        vector<pair<int, int>> ans1 = nextSmallerElement1(arr, n);
+        vector<pair<int, int>> ans2 = nextSmallerElement2(arr, n);
+        vector<int> ans;
 
         for (int i = 0; i < n; i++)
         {
-            v.push_back({arr[i], 0});
-            v.push_back({dep[i], 1});
-        }
-
-        sort(v.begin(), v.end());
-
-        int temp = 0;
-        int ans = 0;
-
-        for (int i = 0; i < v.size(); i++)
-        {
-            if (v[i].second == 0)
+            if (ans1[i].first == -1 && ans2[i].first != -1)
             {
-                temp++;
-                ans = max(ans, temp);
+                ans.push_back(ans2[i].second);
             }
-            else{
-                temp--;
+            else if (ans2[i].first == -1 && ans1[i].first != -1)
+            {
+                ans.push_back(ans1[i].second);
+            }
+            else if (ans1[i].first == -1 && ans2[i].first == -1)
+            {
+                ans.push_back(-1);
+            }
+            else
+            {
+                if (abs(ans1[i].second - i) < abs(ans2[i].second - i))
+                {
+                    ans.push_back(ans1[i].second);
+                }
+                else if(abs(ans1[i].second - i) > abs(ans2[i].second - i))
+                {
+                    ans.push_back(ans2[i].second);
+                }
+                else{
+                    if(ans1[i].first < ans2[i].first){
+                        ans.push_back(ans1[i].second);
+                    }
+                    else{
+                        ans.push_back(ans2[i].second);
+                    }
+                }
             }
         }
-
         return ans;
     }
 };
