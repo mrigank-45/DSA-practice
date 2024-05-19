d27d0ad7a65046dcb7bd23ddb42e36e7cf6289b0
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-05-19 18:43:58 +0530

    Next Greater Element III

--- sample.cpp
+++ sample.cpp
@@ -4,41 +4,61 @@ using namespace std;
 class Solution
 {
 public:
-    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    int nextGreaterElement(int n)
     {
+        deque<int> digits;
 
-        int n = strs.size();
-        vector<string> strs1 = strs;
+        int temp = 0;
+        int point = 0;
 
-        for (int i = 0; i < n; i++)
+        while (n)
         {
-            sort(strs1[i].begin(), strs1[i].end());
+            if (n % 10 < temp)
+            {
+                auto it = upper_bound(digits.begin(), digits.end(), n % 10);
+                int result = *it;
+                digits.erase(it);
+                digits.push_back(n % 10);
+
+                point = result;
+                n /= 10;
+
+                break;
+            }
+
+            temp = n % 10;
+            digits.push_back(n % 10);
+            n /= 10;
         }
 
-        map<string, pair<bool, vector<int>>> mp;
-
-        for (int i = 0; i < n; i++)
+        if (point == 0)
         {
-            if (mp.find(strs1[i]) == mp.end())
+            return -1;
+        }
+
+        sort(digits.begin(), digits.end());
+
+        digits.push_front(point);
+
+        while (n)
+        {
+            digits.push_front(n % 10);
+            n /= 10;
+        }
+
+        int ans = 0;
+
+        for (auto val : digits)
+        {
+            // int overflow check
+            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && val > 7))
             {
-                mp[strs1[i]] = {true, {i}};
+                return -1;
             }
             else
             {
-                mp[strs1[i]].second.push_back(i);
+                ans = ans * 10 + val;
             }
-        }
-
-        vector<vector<string>> ans;
-
-        for(auto it: mp)
-        {
-            vector<string> temp;
-            for(auto i: it.second.second)
-            {
-                temp.push_back(strs[i]);
-            }
-            ans.push_back(temp);
         }
 
         return ans;
