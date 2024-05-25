6b73e923ef4e0e96cf6f37b5e2e61af6ebe79c77
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-05-25 18:17:42 +0530

    Angle Between Hands of a Clock

--- sample.cpp
+++ sample.cpp
@@ -4,8 +4,12 @@ using namespace std;
 class Solution
 {
 public:
-    long long int maxSumWithK(long long int a[], long long int n, long long int k)
+    double angleClock(int hour, int minutes)
     {
-        
+        double minuteAngle = 6 * minutes;             
+        double hourAngle = 30 * hour + 0.5 * minutes; 
+
+        double angle = abs(minuteAngle - hourAngle);
+        return min(angle, 360 - angle); 
     }
 };
