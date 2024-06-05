2f6bc5dedee706ce2dd31175d7993749ef9b7be8
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-06-05 18:07:52 +0530

    Jump Game II

--- sample.cpp
+++ sample.cpp
@@ -4,44 +4,31 @@ using namespace std;
 class Solution
 {
 public:
-    int mod = 1e9 + 7;
-    bool isPalindrome(string s)
+    int solve(vector<int> &nums, int i, vector<int> &dp)
     {
-        int i = 0, j = s.size() - 1;
-        while (i < j)
+        if (i == nums.size() - 1)
         {
-            if (s[i] != s[j])
-                return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-    int solve(int i, int j, string path, vector<vector<char>> &mat, vector<vector<bool>> &vis, int n, int m)
-    {
-        path += mat[i][j];
-        if (i == n - 1 && j == m - 1)
-        {
-            if (isPalindrome(path))
-                return 1;
-            else
-                return 0;
-        }
-        if (i < 0 || i >= n || j < 0 || j >= m || vis[i][j])
             return 0;
-        int ans = 0;
-        vis[i][j] = true;
-        if (i + 1 < n)
-            ans = (ans + solve(i + 1, j, path, mat, vis, n, m)) % mod;
-        if (j + 1 < m)
-            ans = (ans + solve(i, j + 1, path, mat, vis, n, m)) % mod;
-        vis[i][j] = false;
-        return ans % mod;
+        }
+
+        if (dp[i] != -1)
+        {
+            return dp[i];
+        }
+
+        int ans = 10000;
+        for (int j = 1; j <= nums[i] && i + j < nums.size(); j++)
+        {
+            if ((nums[i + j] == 0 && i + j == nums.size() - 1) || nums[i+j] != 0)
+            {
+                ans = min(ans, 1 + solve(nums, i + j, dp));
+            }
+        }
+        return dp[i] = ans;
     }
-    int countPalindromicPaths(vector<vector<char>> matrix)
+    int jump(vector<int> &nums)
     {
-        int n = matrix.size(), m = matrix[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-        return solve(0, 0, "", matrix, vis, n, m);
+        vector<int> dp(nums.size(), -1);
+        return solve(nums, 0, dp);
     }
 };
