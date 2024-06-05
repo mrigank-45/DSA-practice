f81552582ecb8a803c33a50ff9e4b8e38e214f63
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-06-05 05:35:15 +0530

    Number of Palindromic paths in a Matrix

--- sample.cpp
+++ sample.cpp
@@ -4,16 +4,44 @@ using namespace std;
 class Solution
 {
 public:
-    string largestDivisible(string s)
+    int mod = 1e9 + 7;
+    bool isPalindrome(string s)
     {
-        int flag = 0;
-        sort(s.begin(), s.end());
-        reverse(s.begin(), s.end());
-        do
+        int i = 0, j = s.size() - 1;
+        while (i < j)
         {
-            if (stoi(s) % 17 == 0 && s[0] != '0')
-                return s;
-        } while (prev_permutation(s.begin(), s.end()));
-        return "Not Possible";
+            if (s[i] != s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    int solve(int i, int j, string path, vector<vector<char>> &mat, vector<vector<bool>> &vis, int n, int m)
+    {
+        path += mat[i][j];
+        if (i == n - 1 && j == m - 1)
+        {
+            if (isPalindrome(path))
+                return 1;
+            else
+                return 0;
+        }
+        if (i < 0 || i >= n || j < 0 || j >= m || vis[i][j])
+            return 0;
+        int ans = 0;
+        vis[i][j] = true;
+        if (i + 1 < n)
+            ans = (ans + solve(i + 1, j, path, mat, vis, n, m)) % mod;
+        if (j + 1 < m)
+            ans = (ans + solve(i, j + 1, path, mat, vis, n, m)) % mod;
+        vis[i][j] = false;
+        return ans % mod;
+    }
+    int countPalindromicPaths(vector<vector<char>> matrix)
+    {
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        return solve(0, 0, "", matrix, vis, n, m);
     }
 };
--- test.cpp
+++ test.cpp
@@ -4,57 +4,28 @@ using namespace std;
 class Solution
 {
 public:
-    bool isOutOfBounds(int i, int j, int n, int m)
+    int mod = 1e9 + 7;
+    int solve(int i1, int j1, int i2, int j2, vector<vector<char>> &mat, int n, int m, vector<vector<vector<vector<int>>>> &dp)
     {
-        return i < 0 || j < 0 || i >= n || j >= m;
-    }
-    int dfs(vector<vector<int>> grid, int i, int j)
-    {
-        if (grid[i][j] == 0)
-        {
+        if (i1 < 0 || j1 < 0 || i2 < 0 || j2 < 0 || i1 >= n || i2 < i1 || j1 >= m || j2 < j1)
             return 0;
-        }
-        grid[i][j] = -1;
-
+        if (mat[i1][j1] != mat[i2][j2])
+            return 0;
+        if (abs((i2 - i1) + (j2 - j1)) <= 1)
+            return 1;
+        if (dp[i1][j1][i2][j2] != -1)
+            return dp[i1][j1][i2][j2];
         int ans = 0;
-
-        if (!isOutOfBounds(i + 1, j, grid.size(), grid[0].size()) && grid[i + 1][j] != -1 && grid[i + 1][j] != 0)
-        {
-            ans = max(ans, grid[i + 1][j] + dfs(grid, i + 1, j));
-        }
-        if (!isOutOfBounds(i - 1, j, grid.size(), grid[0].size()) && grid[i - 1][j] != -1 && grid[i - 1][j] != 0)
-        {
-            ans = max(ans, grid[i - 1][j] + dfs(grid, i - 1, j));
-        }
-        if (!isOutOfBounds(i, j + 1, grid.size(), grid[0].size()) && grid[i][j + 1] != -1 && grid[i][j + 1] != 0)
-        {
-            ans = max(ans, grid[i][j + 1] + dfs(grid, i, j + 1));
-        }
-        if (!isOutOfBounds(i, j - 1, grid.size(), grid[0].size()) && grid[i][j - 1] != -1 && grid[i][j - 1] != 0)
-        {
-            ans = max(ans, grid[i][j - 1] + dfs(grid, i, j - 1));
-        }
-
-        return ans;
+        ans = (ans + solve(i1 + 1, j1, i2, j2 - 1, mat, n, m, dp) % mod) % mod;
+        ans = (ans + solve(i1 + 1, j1, i2 - 1, j2, mat, n, m, dp) % mod) % mod;
+        ans = (ans + solve(i1, j1 + 1, i2, j2 - 1, mat, n, m, dp) % mod) % mod;
+        ans = (ans + solve(i1, j1 + 1, i2 - 1, j2, mat, n, m, dp) % mod) % mod;
+        return dp[i1][j1][i2][j2] = ans;
     }
-    int getMaximumGold(vector<vector<int>> &grid)
+    int countPalindromicPaths(vector<vector<char>> matrix)
     {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        int ans = 0;
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (grid[i][j] != 0)
-                {
-                    ans = max(ans, grid[i][j] + dfs(grid, i, j));
-                }
-            }
-        }
-
-        return ans;
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<vector<vector<int>>>> dp(n, vector<vector<vector<int>>>(m, vector<vector<int>>(n, vector<int>(m, -1))));
+        return solve(0, 0, n - 1, m - 1, matrix, n, m, dp) % mod;
     }
 };
