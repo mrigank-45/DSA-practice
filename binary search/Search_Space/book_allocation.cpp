5c5ec34921dc842f0aace91c02992d21da54d000
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-06-07 00:43:19 +0530

    Allocate minimum number of pages

--- binary search/Search_Space/book_allocation.cpp
+++ binary search/Search_Space/book_allocation.cpp
@@ -1,15 +1,7 @@
+// Allocate minimum number of pages | GFG
 // You have N books, each with Ai number of pages. M students need to be allocated contiguous books, with each student getting at
 // least one book. Out of all the permutations, the goal is to find the permutation where the student with the most pages allocated
 // to him gets the minimum number of pages, out of all possible permutations.
-
-// Input: N = 4, A[] = {12,34,67,90}, M = 2
-// Output: 113
-// Explanation: Allocation can be done in
-// following ways:
-// {12} and {34, 67, 90} Maximum Pages = 191
-// {12, 34} and {67, 90} Maximum Pages = 157
-// {12, 34, 67} and {90} Maximum Pages =113.
-// Therefore, the minimum of these cases is 113, which is selected as the output.
 
 #include <bits/stdc++.h>
 using namespace std;
