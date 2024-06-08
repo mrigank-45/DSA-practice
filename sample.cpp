ecd5852d21f8c8832ca27ae12226029d00466a30
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-06-09 03:08:47 +0530

    Minimum sum

--- sample.cpp
+++ sample.cpp
@@ -1,34 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int longestValidParentheses(string s) {
-        int n=s.size();
+    string add_strings(string s1, string s2)
+    {
+        int n1 = s1.size(), n2 = s2.size();
+        int i = n1 - 1, j = n2 - 1, carry = 0;
+        string res = "";
 
-        stack<int>st;
-        st.push(-1);
-        int maxx=0;
-
-        for(int i=0;i<n;i++)
+        while (i >= 0 || j >= 0 || carry)
         {
-            if(s[i]=='(')
+            int sum = 0;
+            if (i >= 0)
             {
-                st.push(i);
+                sum += s1[i] - '0';
+                i--;
+            }
+            if (j >= 0)
+            {
+                sum += s2[j] - '0';
+                j--;
+            }
+            sum += carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            res += to_string(sum);
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
+    string solve(int arr[], int n)
+    {
+        sort(arr, arr + n);
+
+        string s1 = "", s2 = "";
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                s1 += to_string(arr[i]);
             }
             else
             {
-                st.pop();
-                if(!st.empty())
-                {
-                    maxx=max(maxx,i-st.top());
-                }
-                else
-                {
-                    st.push(i);
-                }
+                s2 += to_string(arr[i]);
             }
         }
-        return maxx;
+
+        string res = add_strings(s1, s2);
+
+        for (int i = 0; i < res.size(); i++)
+        {
+            if (res[i] != '0')
+            {
+                return res.substr(i);
+            }
+        }
+
+        return "0";
     }
 };
