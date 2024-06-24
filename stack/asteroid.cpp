7d9d09688325d299761c332521e16efbf2a88112
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-06-25 01:46:35 +0530

    stack and queue revised

--- stack/asteroid.cpp
+++ stack/asteroid.cpp
@@ -1,11 +1,4 @@
-// We are given an array asteroids of integers representing asteroids in a row.
-// For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative
-// meaning left). Each asteroid moves at the same speed.
-// Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same
-// size, both will explode. Two asteroids moving in the same direction will never meet.
-
-// Input: asteroids = [5,10,-5] || Output: [5,10]
-// Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+// Asteroid Collision | Leetcode
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -57,17 +50,4 @@ vector<int> asteroidCollision(vector<int> v)
     }
 
     return temp;
-}
-
-int main()
-{
-    vector<int> v = {-1, 5, -2, 10, -5, -20};
-    vector<int> ans = asteroidCollision(v);
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << ans[i] << ' ';
-    }
-    cout << endl;
-
-    return 0;
 }
