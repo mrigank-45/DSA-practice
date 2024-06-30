1dadd8b6302005844194a5d6f1c330ce0f7cc35a
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-01 04:32:06 +0530

    Largest 1-Bordered Square

--- sample.cpp
+++ sample.cpp
@@ -1,61 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution
 {
 public:
-    bool isCousins(TreeNode *root, int x, int y)
+    vector<int> solve(vector<vector<int>> &grid, int i, int j, vector<vector<vector<int>>> &dp)
     {
-        map<int, pair<int, int>> mp;
-
-        mp[root->val] = {0, -1};
-
-        queue<TreeNode *> q;
-
-        q.push(root);
-
-        int d = 0;
-
-        while (!q.empty())
+        if (dp[i][j][0] != -1)
         {
-            int size = q.size();
-            d++;
+            return dp[i][j];
+        }
+        int right = 0, left = 0, up = 0, down = 0;
+        if (j + 1 < grid[0].size() && grid[i][j + 1] == 1)
+        {
+            right = solve(grid, i, j + 1, dp)[0];
+        }
+        if (j - 1 >= 0 && grid[i][j - 1] == 1)
+        {
+            left = solve(grid, i, j - 1, dp)[1];
+        }
+        if (i - 1 >= 0 && grid[i - 1][j] == 1)
+        {
+            up = solve(grid, i - 1, j, dp)[2];
+        }
+        if (i + 1 < grid.size() && grid[i + 1][j] == 1)
+        {
+            down = solve(grid, i + 1, j, dp)[3];
+        }
 
-            for (int i = 0; i < size; i++)
+        dp[i][j] = {right + 1, left + 1, up + 1, down + 1};
+
+        return dp[i][j];
+    }
+    int largest1BorderedSquare(vector<vector<int>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(4, -1)));
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
             {
-                TreeNode *node = q.front();
-                q.pop();
-
-                if (node->left != NULL)
+                if (grid[i][j] == 1 && dp[i][j][0] == -1)
                 {
-                    mp[node->left->val] = {d, node->val};
-                    q.push(node->left);
-                }
-
-                if (node->right != NULL)
-                {
-                    mp[node->right->val] = {d, node->val};
-                    q.push(node->right);
+                    vector<int> v = solve(grid, i, j, dp);
+                    dp[i][j] = v;
                 }
             }
         }
-        if (mp[x].first == mp[y].first && mp[x].second != mp[y].second)
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++)
         {
-            return true;
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    int right = dp[i][j][0];
+                    int left = dp[i][j][1];
+                    int up = dp[i][j][2];
+                    int down = dp[i][j][3];
+
+                    // for right and down
+                    int temp = min(right, down);
+                    if (i + temp - 1 < n && j + temp - 1 < n && dp[i + temp - 1][j + temp - 1][1] >= temp && dp[i + temp - 1][j + temp - 1][2] >= temp)
+                    {
+                        ans = max(ans, temp);
+                    }
+
+                    // for left and down
+                    temp = min(left, down);
+                    if (i - temp + 1 >= 0 && j - temp + 1 >= 0 && dp[i - temp + 1][j - temp + 1][0] >= temp && dp[i - temp + 1][j - temp + 1][3] >= temp)
+                    {
+                        ans = max(ans, temp);
+                    }
+                }
+            }
         }
-        else
-        {
-            return false;
-        }
+
+        return ans;
     }
 };
