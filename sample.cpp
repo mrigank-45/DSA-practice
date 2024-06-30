bc741351f4a69dadd67321319d47c385cfb0a076
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-06-30 19:28:10 +0530

    Sum Tree DE Shaw

--- sample.cpp
+++ sample.cpp
@@ -1,11 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Node
+{
+    int data;
+    Node *left, *right;
+};
+
 class Solution
 {
 public:
-    int maxProfit(int k, vector<int> &prices)
+    bool flag = 1;
+    int solve(Node *root)
     {
-        int n = prices.size();
+        if (root == NULL)
+        {
+            return 0;
+        }
+
+        if (root->left == NULL && root->right == NULL)
+        {
+            return root->data;
+        }
+
+        int leftSum = solve(root->left);
+        int rightSum = solve(root->right);
+
+        if (root->data != leftSum + rightSum)
+        {
+            flag = 0;
+        }
+
+        return root->data + leftSum + rightSum;
+    }
+    bool isSumTree(Node *root)
+    {
+        if (root == NULL || (root->left == NULL && root->right == NULL))
+        {
+            return true;
+        }
+
+        int temp = solve(root);
+
+        return flag;
     }
 };
