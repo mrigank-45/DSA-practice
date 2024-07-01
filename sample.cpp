c4dffaaef9cd35943793c14e515cbcd4324f75f9
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-02 00:42:21 +0530

    Capacity To Ship Packages Within D Days

--- sample.cpp
+++ sample.cpp
@@ -4,33 +4,63 @@ using namespace std;
 class Solution
 {
 public:
-    int countTriplets(vector<int> nums)
+    bool allowed(int val, int arr[], int n, int d)
     {
-        int n = nums.size();
-        if(n<=2) return 0;
+        int sum = 0;
+        int cnt = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (sum > val)
+            {
+                return false;
+            }
+            if (sum + arr[i] > val)
+            {
+                cnt++;
+                sum = arr[i];
+            }
+            else
+            {
+                sum += arr[i];
+            }
+        }
+        return sum<=val && cnt <= d;
+    }
+
+    int leastWeightCapacity(int arr[], int n, int d)
+    {
+        if(n == 1){
+            return arr[0];
+        }
+
+        int mini = INT_MAX;
+        for (int i = 0; i < n; i++)
+        {
+            mini = min(mini, arr[i]);
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+        }
+        if(d == 1){
+            return sum;
+        }
+        int s = mini, e = sum;
         int ans = 0;
-        map<int, vector<int>> mp;
-
-        for (int i = 0; i < n; i++)
+        while (s < e)
         {
-            for (int j = i + 1; j < n; j++)
+            int mid = (s + e) / 2;
+            if (allowed(mid, arr, n, d))
             {
-                if (nums[j] > nums[i])
-                {
-                    mp[i].push_back(j);
-                }
+                ans = mid;
+                e = mid;
+            }
+            else
+            {
+                s = mid + 1;
             }
         }
-
-        for (int i = 0; i < n; i++)
-        {
-            vector<int> v = mp[i];
-            for (int j = 0; j < v.size(); j++)
-            {
-                ans = ans + mp[v[j]].size();
-            }
-        }
-
         return ans;
     }
 };
