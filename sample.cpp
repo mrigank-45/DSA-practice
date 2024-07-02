5336160fa9243a989a40b03da3b07278adeb0199
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-03 04:45:54 +0530

    Minimum Number of Operations to Make Array XOR Equal to K

--- sample.cpp
+++ sample.cpp
@@ -4,62 +4,27 @@ using namespace std;
 class Solution
 {
 public:
-    int missingInteger(vector<int> &nums)
+    int minOperations(vector<int> &nums, int k)
     {
         int n = nums.size();
-        int start = 0;
-        int len = 1;
-        pair<int, int> res = {0, 0};
-
+        int res = nums[0];
         for (int i = 1; i < n; i++)
         {
-            if (nums[i] == nums[i - 1] + 1)
-            {
-                if (i == n - 1)
-                {
-                    if (i - start + 1 >= len)
-                    {
-                        res = {start, i};
-                    }
-                }
-                continue;
-            }
-            else
-            {
-                if (i - start > len)
-                {
-                    res = {start, i - 1};
-                    len = i - start;
-                    cout<<i<<endl;
-                    cout << res.first << " " << res.second << endl;
-                }
-                start = i;
-            }
+            res = res ^ nums[i];
         }
-        int sum = 0;
+        int cnt = 0;
+        for (int i = 0; i <= 31; i++)
+        {
+            int a = res;
+            int b = k;
+            a = a >> i;
+            b = b >> i;
 
-        for (int i = res.first; i <= res.second; i++)
-        {
-            sum += nums[i];
-        }
-
-        map<int, int> mp;
-        for (int i = 0; i < n; i++)
-        {
-            mp[nums[i]]++;
-        }
-        int ans = sum;
-        while (1)
-        {
-            if (mp.find(ans) == mp.end())
+            if ((a & 1) != (b & 1))
             {
-                return ans;
-            }
-            else
-            {
-                ans++;
+                cnt++;
             }
         }
-        return ans;
+        return cnt;
     }
 };
