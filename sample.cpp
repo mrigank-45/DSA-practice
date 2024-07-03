4586875901d1611cc4b0d1170780dabed68cd31f
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-03 23:08:58 +0530

    Maximum Size of a Set After Removals

--- sample.cpp
+++ sample.cpp
@@ -4,27 +4,146 @@ using namespace std;
 class Solution
 {
 public:
-    int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f)
+    int maximumSetSize(vector<int> &nums1, vector<int> &nums2)
     {
-        if (a == e && (c != a || d < min(b, f) || d > max(b, f)))
+        int n = nums1.size();
+        int cnt1 = n / 2, cnt2 = n / 2;
+
+        unordered_map<int, int> mp1;
+        unordered_map<int, int> mp2;
+
+        for (int i = 0; i < n; i++)
         {
-            return 1;
+            mp1[nums1[i]]++;
         }
-        else if (b == f && (d != b || c < min(a, e) || c > max(a, e)))
+        for (int i = 0; i < n; i++)
         {
-            return 1;
+            mp2[nums2[i]]++;
         }
-        else if (c + d == e + f && (c + d != a + b || a < min(c, e) || a > max(c, e)))
+
+        // Step 1
+        for (auto it : mp1)
         {
-            return 1;
+            if (cnt1 == 0)
+            {
+                break;
+            }
+
+            if (it.second > 1)
+            {
+                if (it.second - 1 > cnt1)
+                {
+                    mp1[it.first] -= cnt1;
+                    cnt1 = 0;
+                }
+                else
+                {
+                    cnt1 -= it.second - 1;
+                    mp1[it.first] = 1;
+                }
+            }
         }
-        else if (c - d == e - f && (c - d != a - b || b < min(d, f) || b > max(d, f)))
+        for (auto it : mp2)
         {
-            return 1;
+            if (cnt2 == 0)
+            {
+                break;
+            }
+
+            if (it.second > 1)
+            {
+                if (it.second - 1 > cnt2)
+                {
+                    mp2[it.first] -= cnt2;
+                    cnt2 = 0;
+                }
+                else
+                {
+                    cnt2 -= it.second - 1;
+                    mp2[it.first] = 1;
+                }
+            }
         }
-        else
+
+        // step 2
+        if (cnt1 > 0)
         {
-            return 2;
+            for (auto it : mp1)
+            {
+                if (cnt1 == 0)
+                {
+                    break;
+                }
+                if (mp2.find(it.first) != mp2.end() && mp2[it.first] > 0)
+                {
+                    mp1[it.first] = 0;
+                    cnt1--;
+                }
+            }
         }
+        if (cnt2 > 0)
+        {
+            for (auto it : mp2)
+            {
+                if (cnt2 == 0)
+                {
+                    break;
+                }
+                if (mp1.find(it.first) != mp1.end() && mp1[it.first] > 0)
+                {
+                    mp2[it.first] = 0;
+                    cnt2--;
+                }
+            }
+        }
+
+        // step 3
+        if (cnt1 > 0)
+        {
+            for (auto it : mp1)
+            {
+                if (cnt1 == 0)
+                {
+                    break;
+                }
+                if (it.second > 0)
+                {
+                    mp1[it.first] = 0;
+                    cnt1--;
+                }
+            }
+        }
+        if (cnt2 > 0)
+        {
+            for (auto it : mp2)
+            {
+                if (cnt2 == 0)
+                {
+                    break;
+                }
+                if (it.second > 0)
+                {
+                    mp2[it.first] = 0;
+                    cnt2--;
+                }
+            }
+        }
+
+        unordered_set<int> s;
+        for (auto it : mp1)
+        {
+            if (it.second > 0)
+            {
+                s.insert(it.first);
+            }
+        }
+        for (auto it : mp2)
+        {
+            if (it.second > 0)
+            {
+                s.insert(it.first);
+            }
+        }
+        return s.size();
     }
 };
