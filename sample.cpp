038685ce05d01ac21a022eaa20dd9d6390e3c1fe
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-03 17:34:57 +0530

    Min Moves to Capture The Queen

--- sample.cpp
+++ sample.cpp
@@ -1,74 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct ComparePairs
-{
-    bool operator()(pair<int, int> &p1, pair<int, int> &p2) const
-    {
-
-        return p1.second > p2.second;
-    }
-};
-
 class Solution
 {
 public:
-    int minimumOperationsToMakeEqual(int x, int y)
+    int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f)
     {
-
-        if (x < y)
+        if (a == e && (c != a || d < min(b, f) || d > max(b, f)))
         {
-            return y - x;
+            return 1;
         }
-
-        pair<int, int> p;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> q;
-        unordered_map<int, int> m;
-        m[x] = 1;
-
-        q.push({x, 0});
-        int curr, op;
-        int mod11, mod5, inv11, inv5, div11, div5;
-
-        while (!q.empty())
+        else if (b == f && (d != b || c < min(a, e) || c > max(a, e)))
         {
-            p = q.top();
-            q.pop();
-            curr = p.first;
-            op = p.second;
-
-            if (curr == y)
-                return op;
-
-            if (curr % 11 == 0)
-                q.push({curr / 11, op + 1});
-
-            if (curr % 5 == 0)
-                q.push({curr / 5, op + 1});
-
-            if (curr < y)
-                q.push({y, op + y - curr});
-
-            if (curr > y)
-            {
-                mod11 = curr % 11;
-                mod5 = curr % 5;
-                inv11 = 11 - mod11;
-                inv5 = 5 - mod5;
-
-                div11 = curr / 11;
-                div5 = curr / 5;
-
-                q.push({div11, op + mod11 + 1});
-                q.push({div5, op + mod5 + 1});
-                q.push({div11 + 1, op + inv11 + 1});
-                q.push({div5 + 1, op + inv5 + 1});
-            }
-
-            if (curr > y)
-                q.push({curr - 1, op + 1});
+            return 1;
         }
-
-        return 0;
+        else if (c + d == e + f && (c + d != a + b || a < min(c, e) || a > max(c, e)))
+        {
+            return 1;
+        }
+        else if (c - d == e - f && (c - d != a - b || b < min(d, f) || b > max(d, f)))
+        {
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }
     }
 };
