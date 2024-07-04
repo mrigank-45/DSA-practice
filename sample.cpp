b4f814029befff0f3f015dd70e2a6b0616f36596
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-04 23:35:43 +0530

    Minimum Number of Pushes to Type Word I

--- sample.cpp
+++ sample.cpp
@@ -4,60 +4,47 @@ using namespace std;
 class Solution
 {
 public:
-    int solve(int i, int j, int n, int m, vector<vector<int>> &matrix, vector<vector<int>> &dp)
+    int minimumPushes(string word)
     {
-        if (dp[i][j] != -1)
+        map<char, int> mp1;
+
+        for (int i = 0; i < word.size(); i++)
         {
-            return dp[i][j];
+            mp1[word[i]]++;
         }
 
-        int ans = INT_MIN;
+        priority_queue<int> pq;
 
-        // right
-        if (i >= 0 && i < n && j + 1 >= 0 && j + 1 < m && matrix[i][j + 1] > matrix[i][j])
+        for (auto it : mp1)
         {
-            ans = max(ans, solve(i, j + 1, n, m, matrix, dp));
-        }
-        // left
-        if (i >= 0 && i < n && j - 1 >= 0 && j - 1 < m && matrix[i][j - 1] > matrix[i][j])
-        {
-            ans = max(ans, solve(i, j - 1, n, m, matrix, dp));
-        }
-        // up
-        if (i - 1 >= 0 && i - 1 < n && j >= 0 && j < m && matrix[i - 1][j] > matrix[i][j])
-        {
-            ans = max(ans, solve(i - 1, j, n, m, matrix, dp));
-        }
-        // down
-        if (i + 1 >= 0 && i + 1 < n && j >= 0 && j < m && matrix[i + 1][j] > matrix[i][j])
-        {
-            ans = max(ans, solve(i + 1, j, n, m, matrix, dp));
+            pq.push(it.second);
         }
 
-        if (ans != INT_MIN)
-        {
-            return dp[i][j] = 1 + ans;
-        }
-        else
-        {
-            return dp[i][j] = 1;
-        }
-    }
-    int longestIncreasingPath(vector<vector<int>> &matrix)
-    {
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int cnt = 0;
+        int flag = 0;
 
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        int ans = 1;
-
-        for (int i = 0; i < n; i++)
+        while (!pq.empty())
         {
-            for (int j = 0; j < m; j++)
+            int val = pq.top();
+            pq.pop();
+            if (flag / 8 == 0)
             {
-                ans = max(ans, solve(i, j, n, m, matrix, dp));
+                cnt += val;
             }
+            else if (flag / 8 == 1)
+            {
+                cnt += val * 2;
+            }
+            else if (flag / 8 == 2)
+            {
+                cnt += val * 3;
+            }
+            else
+            {
+                cnt += val * 4;
+            }
+            flag++;
         }
-        return ans;
+        return cnt;
     }
 };
--- test.cpp
+++ test.cpp
@@ -1,25 +1,17 @@
-// lower_bound and upper_bound in vector
- 
-#include <algorithm> // for lower_bound, upper_bound and sort
-#include <iostream>
-#include <vector> // for vector
- 
+#include <bits/stdc++.h>
 using namespace std;
- 
+
 int main()
 {
-    // Note that the array is sorted
-    int gfg[] = { 5, 6, 6, 6, 8 };
- 
-    vector<int> v(gfg, gfg + 8); 
- 
-    int lower = *lower_bound(v.begin(), v.end(), 7);
-    int upper = *upper_bound(v.begin(), v.end(), 10);
- 
-    cout << "lower_bound for 6 is "
-         << (lower) << '\n';
-    cout << "upper_bound for 6 is "
-         << (upper) << '\n';
- 
-    return 0;
+     map<int, int> mp;
+     mp[2] = 5;
+     mp[8] = 7;
+     mp[3] = 1;
+
+     for (auto it : mp)
+     {
+          cout << it.first << ": " << it.second << endl;
+     }
+
+     return 0;
 }
