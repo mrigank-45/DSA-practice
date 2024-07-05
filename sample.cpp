443a935c5c444b5eae0a07a28c3a7c2d0c521edd
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-06 03:58:49 +0530

    Alice and Bob Playing Flower Game

--- sample.cpp
+++ sample.cpp
@@ -4,53 +4,30 @@ using namespace std;
 class Solution
 {
 public:
-    int maximumLength(vector<int> &nums)
+    long long flowerGame(int n, int m)
     {
-        int n = nums.size();
-        map<int, int> mp;
-        for (int i = 0; i < n; i++)
+        int e = 0, o = 0;
+        if (n % 2 == 0)
         {
-            mp[nums[i]]++;
-        }
-        int ans = 1;
-        for (auto it : mp)
-        {
-            if (it.second < 2 || it.first == 1)
-            {
-                continue;
-            }
-            else
-            {
-                int cnt = 2;
-                int k = 2;
-                while (1)
-                {
-                    if (pow(it.first, k) <= INT_MAX && mp[pow(it.first, k)] > 1)
-                    {
-                        cnt += 2;
-                        k = k + k;
-                    }
-                    else if (pow(it.first, k) <= INT_MAX && mp[pow(it.first, k)] == 1)
-                    {
-                        cnt++;
-                        break;
-                    }
-                    else
-                    {
-                        cnt--;
-                        break;
-                    }
-                }
-                ans = max(ans, cnt);
-            }
-        }
-        if (mp[1] % 2 == 0)
-        {
-            ans = max(ans, mp[1] - 1);
+            e = n / 2;
+            o = n / 2;
         }
         else
         {
-            ans = max(ans, mp[1]);
+            e = ((n - 1) / 2);
+            o = ((n - 1) / 2) + 1;
+        }
+        int ans = 0;
+        for (int i = 1; i <= m; i++)
+        {
+            if (i % 2 == 0)
+            {
+                ans += o;
+            }
+            else
+            {
+                ans += e;
+            }
         }
         return ans;
     }
