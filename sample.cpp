c820cad37367cd158049d857a6d5f40c32e92cbb
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-05 06:06:01 +0530

    Count the Number of Houses at a Certain Distance I

--- sample.cpp
+++ sample.cpp
@@ -4,33 +4,66 @@ using namespace std;
 class Solution
 {
 public:
-    int minimumPushes(string word)
+    int bfs(int i, int k, map<int, vector<int>> &mp, int n)
     {
-        map<char, int> mp1;
+        unordered_map<int, bool> vis;
+        queue<int> q;
 
-        for (int i = 0; i < word.size(); i++)
+        q.push(i);
+        vis[i] = true;
+
+        while (!q.empty() && k > 0)
         {
-            mp1[word[i]]++;
+            int size = q.size();
+            for (int i = 0; i < size; i++)
+            {
+                int node = q.front();
+                q.pop();
+
+                for (auto it : mp[node])
+                {
+                    if (!vis[it])
+                    {
+                        q.push(it);
+                        vis[it] = true;
+                    }
+                }
+            }
+            k--;
+        }
+        return q.size();
+    }
+
+    vector<int> countOfPairs(int n, int x, int y)
+    {
+        map<int, vector<int>> mp;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            mp[i].push_back(i + 1);
+            mp[i + 1].push_back(i);
+        }
+        if (x - 1 != y || y - 1 != x)
+        {
+            mp[x - 1].push_back(y - 1);
+            mp[y - 1].push_back(x - 1);
         }
 
-        priority_queue<int> pq;
-
-        for (auto it : mp1)
+        vector<int> res(n, 0);
+        for (int k = 1; k <= n; k++)
         {
-            pq.push(it.second);
+            int temp = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int out = bfs(i, k, mp, n);
+                if (k == 2)
+                {
+                    cout << out << endl;
+                }
+                temp += out;
+            }
+            res[k - 1] = temp;
         }
-
-        int cnt = 0;
-        int flag = 0;
-
-        while (!pq.empty())
-        {
-            int val = pq.top();
-            pq.pop();
-            int temp = flag / 8 + 1;
-            cnt += val * temp;
-            flag++;
-        }
-        return cnt;
+        return res;
     }
 };
