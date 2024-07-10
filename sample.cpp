6b76f89488dde3d0d9e89950e04862e45c37bfb8
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-10 19:54:46 +0530

    Apply Operations to Make String Empty

--- sample.cpp
+++ sample.cpp
@@ -4,65 +4,36 @@ using namespace std;
 class Solution
 {
 public:
-    int solve(int i, int j, int score, vector<int> &nums, vector<vector<int>> &dp)
+    string lastNonEmptyString(string s)
     {
-        if (i >= j)
+        map<char, pair<int, int>> mp;
+
+        for (int i = 0; i < s.size(); i++)
         {
-            return 0;
+            mp[s[i]].first++;
+            mp[s[i]].second = i;
         }
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int ans = 0;
-
-        if (nums[i] + nums[j] == score)
+        int maxi = 1;
+        for (auto it : mp)
         {
-            ans = max(ans, 1 + solve(i + 1, j - 1, score, nums, dp));
-        }
-        if (nums[i] + nums[i + 1] == score)
-        {
-            ans = max(ans, 1 + solve(i + 2, j, score, nums, dp));
-        }
-        if (nums[j] + nums[j - 1] == score)
-        {
-            ans = max(ans, 1 + solve(i, j - 2, score, nums, dp));
-        }
-
-        return dp[i][j] = ans;
-
-    }
-    int maxOperations(vector<int> &nums)
-    {
-        int n = nums.size();
-        
-        bool flag = true;
-        for (int i = 0; i < n; i++)
-        {
-            if (nums[i] != nums[0])
+            if (it.second.first > maxi)
             {
-                flag = false;
-                break;
+                maxi = it.second.first;
             }
         }
-        if (flag)
+        map<int, char> mp2;
+        for (auto it : mp)
         {
-            return n / 2;
+            if (it.second.first == maxi)
+            {
+                mp2.insert({it.second.second, it.first});
+            }
         }
-        int size = max(nums[0] + nums[1],max(nums[0] + nums[n - 1], nums[n - 1] + nums[n - 2]));
-
-
-        int ans = 0;
-
-        vector<vector<int>> dp1(n + 1, vector<int>(n + 1, -1));
-        ans = max(ans, 1 + solve(1, n - 2, nums[0] + nums[n - 1], nums, dp1));
-
-        vector<vector<int>> dp2(n + 1, vector<int>(n + 1, -1));
-        ans = max(ans, 1 + solve(2, n - 1, nums[0] + nums[1], nums, dp2));
-
-        vector<vector<int>> dp3(n + 1, vector<int>(n + 1, -1));
-        ans = max(ans, 1 + solve(0, n - 3, nums[n - 1] + nums[n - 2], nums, dp3));
-
+        string ans = "";
+        for (auto it : mp2)
+        {
+            ans += it.second;
+        }
         return ans;
     }
 };
