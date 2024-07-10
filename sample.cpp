2439e0fefdbf338f47c307aba25e296370c8a635
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-10 23:03:45 +0530

    Maximize Consecutive Elements in an Array After Modification

--- sample.cpp
+++ sample.cpp
@@ -4,36 +4,40 @@ using namespace std;
 class Solution
 {
 public:
-    string lastNonEmptyString(string s)
+    int dfs(int i, int last, int n, const vector<int> &nums, unordered_map<string, int> &dp)
     {
-        map<char, pair<int, int>> mp;
+        if (i >= n)
+            return 0;
 
-        for (int i = 0; i < s.size(); i++)
+        string key = to_string(last) + "-" + to_string(i);
+        if (dp.find(key) != dp.end())
+            return dp[key];
+
+        if (nums[i] < last)
+            return dfs(i + 1, last, n, nums, dp);
+        int take = 0;
+
+        if (nums[i] == last)
+            take = max(1 + dfs(i + 1, nums[i] + 1, n, nums, dp), take);
+        if (nums[i] == last + 1)
+            take = max(1 + dfs(i + 1, nums[i], n, nums, dp), take);
+
+        dp[key] = take;
+        return take;
+    }
+
+    int maxSelectedElements(vector<int> &nums)
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        unordered_map<string, int> dp;
+
+        int res = 1;
+        for (int i = 0; i < n; i++)
         {
-            mp[s[i]].first++;
-            mp[s[i]].second = i;
+            res = max(res, max(dfs(i + 1, nums[i], n, nums, dp) + 1, dfs(i + 1, nums[i] + 1, n, nums, dp) + 1));
         }
-        int maxi = 1;
-        for (auto it : mp)
-        {
-            if (it.second.first > maxi)
-            {
-                maxi = it.second.first;
-            }
-        }
-        map<int, char> mp2;
-        for (auto it : mp)
-        {
-            if (it.second.first == maxi)
-            {
-                mp2.insert({it.second.second, it.first});
-            }
-        }
-        string ans = "";
-        for (auto it : mp2)
-        {
-            ans += it.second;
-        }
-        return ans;
+
+        return res;
     }
 };
