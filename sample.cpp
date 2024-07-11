100e5a5d355f982859df5ed10d8e08883eb1bc3c
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-11 20:00:07 +0530

    Find the Length of the Longest Common Prefix

--- sample.cpp
+++ sample.cpp
@@ -4,72 +4,39 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> z_fn(string &s)
+    int longestCommonPrefix(vector<int> &arr1, vector<int> &arr2)
     {
-        int n = s.size(), l = 0, r = 0;
-        vector<int> z(n);
-        for (int i = 1; i < n; i++)
-        {
-            if (i > r)
-            {
-                l = r = i;
-                while (r < n && s[r] == s[r - l])
-                    r++;
-                z[i] = r - l;
-                r--;
-            }
-            else
-            {
-                int cur = i - l;
-                if (z[cur] + i < r + 1)
-                    z[i] = z[cur];
-                else
-                {
-                    l = i;
-                    while (r < n && s[r] == s[r - l])
-                        r++;
-                    z[i] = r - l;
-                    r--;
-                }
-            }
-        }
-        return z;
-    }
-    long long countPrefixSuffixPairs(vector<string> &words)
-    {
-        int n = words.size();
-        map<string, vector<int>> mp;
-        long long cnt = 0;
-        for (int i = 0; i < n; i++)
-        {
-            string s = words[i];
-            vector<int> z = z_fn(s);
-            mp[s] = z;
-        }
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                if (words[i].size() > words[j].size())
-                {
-                    continue;
-                }
-                if (words[i].size() == words[j].size())
-                {
-                    if (words[i] == words[j])
-                    {
-                        cnt++;
-                    }
-                    continue;
-                }
-                string temp = words[j].substr(0, words[i].size());
-                if(temp == words[i] && mp[words[j]][words[j].size() - words[i].size()] == words[i].size())
-                {
-                    cnt++;
-                }
-            }
-        }
+        int n = arr1.size(), m = arr2.size();
+        map<string, int> mp;
 
-        return cnt;
+        for (int i = 0; i < m; i++)
+        {
+            string temp = "";
+            string s = to_string(arr2[i]);
+            for (int j = 0; j < s.size(); j++)
+            {
+                temp += s[j];
+                mp[temp]++;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            string temp = "";
+            string s = to_string(arr1[i]);
+            for (int j = 0; j < s.size(); j++)
+            {
+                temp += s[j];
+                if (mp[temp] > 0)
+                {
+                    ans = max(ans, (int)temp.size());
+                }
+            }
+            if (mp[temp] > 0)
+            {
+                ans = max(ans, (int)temp.size());
+            }
+        }
+        return ans;
     }
 };
