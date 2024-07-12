89dca3392e77edf09aed8c177b3e2ea137d08973
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-12 19:16:40 +0530

    Count Pairs of Connectable Servers in a Weighted Tree Network

--- sample.cpp
+++ sample.cpp
@@ -4,36 +4,81 @@ using namespace std;
 class Solution
 {
 public:
-    int minOperations(vector<int> &nums, int k)
+    int helper(map<int, vector<pair<int, int>>> &adj, int node, int k, int parent, int val)
     {
-        long long n = nums.size();
         int ans = 0;
-
-        priority_queue<long long, vector<long long>, greater<long long>> pq;
-        for (long long i = 0; i < n; i++)
+        for (auto edge : adj[node])
         {
-            pq.push(nums[i]);
+            if (edge.first == parent)
+                continue;
+            if ((val + edge.second) % k == 0)
+            {
+                ans += 1 + helper(adj, edge.first, k, node, val + edge.second);
+            }
+            else
+            {
+                ans += helper(adj, edge.first, k, node, val + edge.second);
+            }
+        }
+        return ans;
+    }
+
+    vector<int> countPairsOfConnectableServers(vector<vector<int>> &edges, int signalSpeed)
+    {
+        int n = edges.size() + 1;
+        int k = signalSpeed;
+        map<int, vector<pair<int, int>>> adj;
+
+        for (auto edge : edges)
+        {
+            adj[edge[0]].push_back({edge[1], edge[2]});
+            adj[edge[1]].push_back({edge[0], edge[2]});
         }
 
-        while (!pq.empty())
+        map<int, vector<int>> valid;
+        for (int i = 0; i < n; i++)
         {
-            if (pq.top() >= k)
+            for (auto edge : adj[i])
             {
-                break;
+                if (edge.second % k == 0)
+                {
+                    valid[i].push_back(1 + helper(adj, edge.first, k, i, edge.second));
+                }
+                else
+                {
+                    valid[i].push_back(helper(adj, edge.first, k, i, edge.second));
+                }
             }
-            if (pq.size() == 1)
-            {
-                ans = -1;
-                break;
-            }
-            long long a = pq.top();
-            pq.pop();
-            long long b = pq.top();
-            pq.pop();
-            long long temp = min(a, b) * 2 + max(a, b);
-            pq.push(temp);
-            ans++;
         }
+
+        vector<int> ans(n, 0);
+
+        for (auto it : valid)
+        {
+            int temp = 0;
+            vector<int> v = it.second;
+            if (v.size() == 0)
+            {
+                ans[it.first] = 0;
+                continue;
+            }
+            for (int i = 0; i < v.size(); i++)
+            {
+
+                if (v[i] == 0)
+                    continue;
+                for (int j = i + 1; j < v.size(); j++)
+                {
+
+                    if (v[j] == 0)
+                        continue;
+
+                    temp += v[i] * v[j];
+                }
+            }
+            ans[it.first] = temp;
+        }
+
         return ans;
     }
 };
