c111896725883806d55dd152a52a4751597bd751
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-24 18:39:33 +0530

    Count Substrings Starting and Ending with Given Character

--- Basic_maths/GCD.cpp
+++ Basic_maths/GCD.cpp
@@ -1,14 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int gcd(int a, int b) {
-	if (b == 0) {
+int gcd(int a, int b)
+{
+	if (b == 0)
+	{
 		return a;
 	}
 	return gcd(b, a % b);
 }
+int nCr(int n, int r)
+{
+	double sum = 1;
+	for (int i = 1; i <= r; i++)
+	{
+		sum = sum * (n - r + i) / i;
+	}
+	return (int)sum;
+}
+
 int main()
 {
 
 	int a = 4, b = 8;
-	cout <<"The GCD of the two numbers is "<<gcd(a, b);
+	cout << "The GCD of the two numbers is " << gcd(a, b);
 }
--- sample.cpp
+++ sample.cpp
@@ -1,43 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// If there's a subsequence of length l with sum k, it will contribute 2^(n-l) to the answer.
-// reason: this length l can be combined with any of the remaining n-l elements to form a subsequence with sum k.
-
 class Solution
 {
 public:
-    int mod = 1e9 + 7, n;
-    int dp[101][101][101];
-
-    // calculate 2^x
-    int func(int x)
+    long long nCr(long long n, long long r)
     {
-        int ans = 1;
-        while (x--)
-            ans = (ans * 2) % mod;
+        double sum = 1;
+        for (int i = 1; i <= r; i++)
+        {
+            sum = sum * (n - r + i) / i;
+        }
+        return (long long)sum;
+    }
+    long long countSubstrings(string s, char c)
+    {
+        long long cnt = 0;
+        for (long long i = 0; i < s.size(); i++)
+        {
+            if(s[i] == c)
+            {
+                cnt++;
+            }
+        }
+        long long ans = nCr(cnt,2) + cnt;
+
         return ans;
-    }
-    int helper(int ind, vector<int> &nums, int curr_sum, int len, int k)
-    {
-        if (curr_sum > k)
-            return 0;
-        if (curr_sum == k)
-            return func(n - len);
-        if (ind == n)
-            return 0;
-        if (dp[ind][curr_sum][len] != -1)
-            return dp[ind][curr_sum][len];
-        int cnt = 0;
-        cnt = (cnt + helper(ind + 1, nums, curr_sum + nums[ind], len + 1, k)) % mod;
-        cnt = (cnt + helper(ind + 1, nums, curr_sum, len, k)) % mod;
-        return dp[ind][curr_sum][len] = cnt;
-    }
 
-    int sumOfPower(vector<int> &nums, int k)
-    {
-        n = nums.size();
-        memset(dp, -1, sizeof(dp));
-        return helper(0, nums, 0, 0, k);
     }
 };
