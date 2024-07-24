35affcee1e2e06d256d55e6b20cbc47709597a65
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-25 04:35:34 +0530

    Minimum Moves to Pick K Ones

--- sample.cpp
+++ sample.cpp
@@ -4,28 +4,59 @@ using namespace std;
 class Solution
 {
 public:
-    long long nCr(long long n, long long r)
+    long long minimumMoves(vector<int> &nums, int k, int maxChanges)
     {
-        double sum = 1;
-        for (int i = 1; i <= r; i++)
+        int type = -1;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
         {
-            sum = sum * (n - r + i) / i;
-        }
-        return (long long)sum;
-    }
-    long long countSubstrings(string s, char c)
-    {
-        long long cnt = 0;
-        for (long long i = 0; i < s.size(); i++)
-        {
-            if(s[i] == c)
+            if (nums[i] != 1)
+                continue;
+            if (nums[i] == 1)
             {
-                cnt++;
+                if (type < 0)
+                    type = 0;
+            }
+            if ((nums[i] == 1) && ((i - 1 >= 0 && nums[i - 1] == 1) || (i + 1 < n && nums[i + 1] == 1)))
+            {
+                if (type < 1)
+                    type = 1;
+            }
+            if ((nums[i] == 1) && (i - 1 >= 0 && nums[i - 1] == 1) && (i + 1 < n && nums[i + 1] == 1))
+            {
+                if (type < 2)
+                    type = 2;
+                break;
             }
         }
-        long long ans = nCr(cnt,2) + cnt;
+        long long moves = 0;
 
-        return ans;
+        if (type == 0)
+        {
+            k--;
+        }
+        else if (type == 1)
+        {
+            k = k - 2;
 
+            moves++;
+        }
+        else if (type == 2)
+        {
+            k = k - 3;
+            moves = moves + 2;
+        }
+
+        if (k > maxChanges)
+        {
+            return -1;
+        }
+        else
+        {
+            moves = moves + (k * 2);
+        }
+
+        return moves;
     }
 };
