68edf0ec768835d2547b0f1bad5b8a926bf27034
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-25 11:14:53 +0530

    Apply Operations to Make Sum of Array Greater Than or Equal to k

--- sample.cpp
+++ sample.cpp
@@ -4,59 +4,25 @@ using namespace std;
 class Solution
 {
 public:
-    long long minimumMoves(vector<int> &nums, int k, int maxChanges)
+    int minOperations(int k)
     {
-        int type = -1;
-        int n = nums.size();
+        int ans = INT_MAX;
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i <= k; i++)
         {
-            if (nums[i] != 1)
-                continue;
-            if (nums[i] == 1)
+            int moves = i;
+            int val = i + 1;
+            if (k % val == 0)
             {
-                if (type < 0)
-                    type = 0;
+                moves += (k / val) - 1;
             }
-            if ((nums[i] == 1) && ((i - 1 >= 0 && nums[i - 1] == 1) || (i + 1 < n && nums[i + 1] == 1)))
+            else
             {
-                if (type < 1)
-                    type = 1;
+                moves += k / val;
             }
-            if ((nums[i] == 1) && (i - 1 >= 0 && nums[i - 1] == 1) && (i + 1 < n && nums[i + 1] == 1))
-            {
-                if (type < 2)
-                    type = 2;
-                break;
-            }
-        }
-        long long moves = 0;
-
-        if (type == 0)
-        {
-            k--;
-        }
-        else if (type == 1)
-        {
-            k = k - 2;
-
-            moves++;
-        }
-        else if (type == 2)
-        {
-            k = k - 3;
-            moves = moves + 2;
+            ans = min(ans, moves);
         }
 
-        if (k > maxChanges)
-        {
-            return -1;
-        }
-        else
-        {
-            moves = moves + (k * 2);
-        }
-
-        return moves;
+        return ans;
     }
 };
