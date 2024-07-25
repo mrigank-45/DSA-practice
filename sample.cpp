c1b611127d58343dc77ee52a9748f9bea2f0aa0e
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-25 11:29:58 +0530

    Most Frequent IDs

--- sample.cpp
+++ sample.cpp
@@ -4,23 +4,37 @@ using namespace std;
 class Solution
 {
 public:
-    int minOperations(int k)
+    vector<long long> mostFrequentIDs(vector<int> &nums, vector<int> &freq)
     {
-        int ans = INT_MAX;
+        int n = nums.size();
 
-        for (int i = 0; i <= k; i++)
+        map<long long, long long> mp;
+
+        for (int i = 0; i < n; i++)
         {
-            int moves = i;
-            int val = i + 1;
-            if (k % val == 0)
+            mp[nums[i]] = 0;
+        }
+
+        priority_queue<pair<long long, long long>> pq;
+
+        for (auto it : mp)
+        {
+            pq.push({0, it.first});
+        }
+
+        vector<long long> ans;
+
+        for (int i = 0; i < n; i++)
+        {
+            mp[nums[i]] += freq[i];
+            pq.push({mp[nums[i]], nums[i]});
+
+            while (pq.top().first != mp[pq.top().second])
             {
-                moves += (k / val) - 1;
+                pq.pop();
             }
-            else
-            {
-                moves += k / val;
-            }
-            ans = min(ans, moves);
+
+            ans.push_back(pq.top().first);
         }
 
         return ans;
