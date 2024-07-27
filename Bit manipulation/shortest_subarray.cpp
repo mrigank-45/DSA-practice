7ae54f02162844c21a78cb87ec6b819297059170
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-27 20:16:42 +0530

    Shortest Subarray With OR at Least K II

--- Bit manipulation/shortest_subarray.cpp
+++ Bit manipulation/shortest_subarray.cpp
@@ -1,3 +1,5 @@
+// Shortest Subarray With OR at Least K II
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -13,10 +15,8 @@ public:
         int left = 0;      // Left pointer of the sliding window
         int ans = INT_MAX;
 
-        // Iterate through each element in nums
         for (int i = 0; i < n; i++)
         {
-            // Update the bitwiseOR by OR-ing the current element
             bitwiseOR |= nums[i];
 
             // Update the setBitCount after including nums[i] in the window
@@ -31,7 +31,6 @@ public:
             // When condition is met, shrink the window
             while (left <= i && bitwiseOR >= k)
             {
-                // Update the minimum length of subarray
                 ans = min(ans, i - left + 1);
 
                 // Remove nums[left] from the window and update setBitCount accordingly
@@ -48,13 +47,11 @@ public:
                         updatedBitwiseOR |= (1 << bit);
                     }
                 }
-                // Update the bitwiseOR with updated value
                 bitwiseOR = updatedBitwiseOR;
-                left++; // Move the left pointer to the right
+                left++; 
             }
         }
 
-        // Return the minimum length of subarray if found, else return -1
         return ans == INT_MAX ? -1 : ans;
     }
 };
