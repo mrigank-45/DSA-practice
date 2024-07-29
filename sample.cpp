c9f6dceeb2af6ea11759c597314adee64d6649c5
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-29 22:15:15 +0530

    string game

--- sample.cpp
+++ sample.cpp
@@ -4,39 +4,14 @@ using namespace std;
 class Solution
 {
 public:
-    int numberOfSubstrings(string s)
+    int maxLength(int n, string s)
     {
-        int n = s.size();
-        int count = 0;
-        for (int winLen = 1; winLen <= n; winLen++)
-        {
-            int zeroCount = 0;
-            float maxZero = sqrt(winLen);
-            int l = 0;
-            int r = winLen;
-            for (int i = l; i < r; i++)
-            {
-                if (s[i] == '0')
-                    zeroCount++;
-            }
-            if (zeroCount < maxZero)
-                count++;
-            while (r < n)
-            {
-                if (s[r] == '0' && s[l] != '0')
-                {
-                    zeroCount++;
-                }
-                else if (s[r] != '0' && s[l] == '0')
-                {
-                    zeroCount--;
-                }
-                l++;
-                r++;
-                if (zeroCount < maxZero)
-                    count++;
-            }
+        unordered_map<char,int> mp;
+
+        for(int i = 0; i<n; i++){
+            mp[s[i]]++;
         }
-        return count;
+
+        return mp.size();
     }
 };
