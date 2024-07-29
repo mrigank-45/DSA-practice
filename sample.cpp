d63cb92c81c7de8731fd61ee3470d67cdf8fd34c
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-29 22:41:19 +0530

    magic tree

--- sample.cpp
+++ sample.cpp
@@ -4,14 +4,56 @@ using namespace std;
 class Solution
 {
 public:
-    int maxLength(int n, string s)
+    vector<long long> magicTree(int n, vector<vector<int>> &edges)
     {
-        unordered_map<char,int> mp;
+        vector<vector<pair<long long, long long>>> adj(n);
+        vector<long long> answer(n, LLONG_MAX);
+        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> pq;
 
-        for(int i = 0; i<n; i++){
-            mp[s[i]]++;
+        // Build the adjacency list
+        for (const auto &edge : edges)
+        {
+            long long u = edge[0];
+            long long v = edge[1];
+            long long t = edge[2];
+            adj[u].emplace_back(v, t);
+            adj[v].emplace_back(u, t);
         }
 
-        return mp.size();
+        // Add all magical nodes (nodes with degree 1) to the priority queue
+        for (long long i = 0; i < n; ++i)
+        {
+            if (adj[i].size() == 1)
+            {
+                pq.push({0, i});
+                answer[i] = 0;
+            }
+        }
+
+        // Dijkstra's algorithm
+        while (!pq.empty())
+        {
+            long long current_time = pq.top().first;
+            long long node = pq.top().second;
+
+            pq.pop();
+
+            if (current_time > answer[node])
+                continue;
+
+            for (const auto &neighbor : adj[node])
+            {
+                long long next_node = neighbor.first;
+                long long travel_time = neighbor.second;
+
+                if (current_time + travel_time < answer[next_node])
+                {
+                    answer[next_node] = current_time + travel_time;
+                    pq.push({answer[next_node], next_node});
+                }
+            }
+        }
+
+        return answer;
     }
 };
