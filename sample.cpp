3e6d59c59e19491c60c7545422467181a1f996c5
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-07-30 20:54:44 +0530

    Range Sum Digits

--- sample.cpp
+++ sample.cpp
@@ -1,54 +1,126 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class RangeFreqQuery
+class Solution
 {
 public:
-    int n;
-    vector<map<int, int>> seg;
-    void makeTree(int low, int high, vector<int> &arr, int i)
+    vector<long long> tree;
+    long long size;
+
+    void SegmentTree(vector<long long> &array)
+    {
+        size = array.size();
+        tree.resize(4 * size);
+        buildTree(array, 0, 0, size - 1);
+    }
+
+    // TC: O(n)
+    void buildTree(vector<long long> &array, long long treeIndex, long long left, long long right)
+    {
+        if (left == right)
+        {
+            tree[treeIndex] = array[left];
+            return;
+        }
+        long long mid = left + (right - left) / 2;
+        buildTree(array, 2 * treeIndex + 1, left, mid);
+        buildTree(array, 2 * treeIndex + 2, mid + 1, right);
+        tree[treeIndex] = tree[2 * treeIndex + 1] + tree[2 * treeIndex + 2];
+    }
+
+    // TC: O(logn)
+    long long query(long long treeIndex, long long low, long long high, long long queryLeft, long long queryRight)
+    {
+        // in range
+        if (queryLeft <= low && high <= queryRight)
+        {
+            return tree[treeIndex];
+        }
+        // out of range
+        if (high < queryLeft || low > queryRight)
+        {
+            return 0;
+        }
+        // partially in range
+        long long mid = low + (high - low) / 2;
+        long long left = query(2 * treeIndex + 1, low, mid, queryLeft, queryRight);
+        long long right = query(2 * treeIndex + 2, mid + 1, high, queryLeft, queryRight);
+
+        return left + right;
+    }
+
+    // TC: O(logn)
+    void rangeUpdate(long long treeIndex, long long low, long long high, long long queryLeft, long long queryRight)
     {
         if (low == high)
         {
-            map<int, int> m;
-            m[arr[low]]++;
-            seg[i] = m;
+            long long val = tree[treeIndex];
+            tree[treeIndex] = countDigits(val);
             return;
         }
-        int mid = (low + high) >> 1;
-        makeTree(low, mid, arr, 2 * i);
-        makeTree(mid + 1, high, arr, 2 * i + 1);
-        map<int, int> m;
-        m = seg[2 * i];
-        for (auto it : seg[2 * i + 1])
+        // out of range
+        if (high < queryLeft || low > queryRight)
         {
-            if (m.find(it.first) == m.end())
-                m[it.first] = it.second;
-            else
-                m[it.first] += it.second;
+            return;
         }
-        seg[i] = m;
+
+        long long mid = low + (high - low) / 2;
+        rangeUpdate(2 * treeIndex + 1, low, mid, queryLeft, queryRight);
+        rangeUpdate(2 * treeIndex + 2, mid + 1, high, queryLeft, queryRight);
+        tree[treeIndex] = tree[2 * treeIndex + 1] + tree[2 * treeIndex + 2];
     }
-    RangeFreqQuery(vector<int> &arr)
+
+    long long querySum(long long left, long long right)
     {
-        n = arr.size();
-        map<int, int> m;
-        seg = vector<map<int, int>>(4 * n + 1, m);
-        makeTree(0, n - 1, arr, 1);
+        return query(0, 0, size - 1, left, right);
     }
-    int query(int node, int l, int r, int ql, int qr, int val)
+
+    void rangeChange(long long left, long long right)
     {
-        if (l > r || ql > r || l > qr)
-            return 0;
-        if (ql <= l && r <= qr)
-            return seg[node][val];
-        int mid = (l + r) >> 1;
-        int left = query(node + node, l, mid, ql, qr, val);
-        int right = query(node + node + 1, mid + 1, r, ql, qr, val);
-        return left + right;
+        rangeUpdate(0, 0, size - 1, left, right);
     }
-    int query(int left, int right, int value)
+
+    long long countDigits(long long num)
     {
-        return query(1, 0, n - 1, left, right, value);
+        if (num == 0)
+            return 1;
+        long long count = 0;
+        while (num > 0)
+        {
+            num /= 10;
+            count++;
+        }
+        return count;
+    }
+    vector<long long> RangeSumDigits(int n, vector<int> &arr, long long Q, vector<vector<int>> &Queries)
+    {
+        vector<long long> array(n);
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = (long long)arr[i];
+        }
+        SegmentTree(array);
+        vector<long long> ans;
+
+        // Process each query
+        for (const auto &query : Queries)
+        {
+            long long type = query[0];
+            long long l = query[1] - 1;
+            long long r = query[2] - 1;
+
+            if (type == 1)
+            {
+                // Change the value of the given index
+                rangeChange(l, r);
+            }
+            else if (type == 2)
+            {
+                // Query the sum of digits in the given range
+                ans.push_back(querySum(l, r));
+            }
+        }
+
+        return ans;
     }
 };
