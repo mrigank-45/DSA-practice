36f6225fd20823ad7c3df7b1f8042fcad67147e1
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-08-08 17:13:11 +0530

    Minimum Array End

---
+++ Bit manipulation/min_array_end.cpp
@@ -0,0 +1,18 @@
+// Minimum Array End
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    long long minEnd(int n, int x)
+    {
+        long long ans = x;
+        for (int i = 0; i < n - 1; i++)
+        {
+            ans = (ans + 1) | x;
+        }
+        return ans;
+    }
+};
