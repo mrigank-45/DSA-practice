3ca7e0d02e1dc59bc4f8c02c37759a28bf8e99c6
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-08-19 01:06:28 +0530

    Taking Maximum Energy From the Mystic Dungeon

--- sample.cpp
+++ sample.cpp
@@ -1,44 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<bool> solution(vector<vector<int>> &operations)
+class Solution
 {
-    int max_width = 0;
-    int max_height = 0;
-    vector<bool> result;
-
-    for (const auto &op : operations)
+public:
+    int solve(vector<int> &energy, int k, int n, int i, int choose, vector<vector<int>> &dp)
     {
-        if (op[0] == 0)
+        if (i >= n)
         {
-            int width = op[1];
-            int height = op[2];
-            max_width = max(max_width, max(width, height));
-            max_height = max(max_height, min(width, height));
+            return choose == 1 ? 0 : INT_MIN;
         }
-        else if (op[0] == 1)
+
+        if (dp[i][choose] != INT_MIN)
         {
-            int box_width = op[1];
-            int box_height = op[2];
-            bool can_fit1 = (max_width <= max(box_width, box_height) &&
-                             max_height <= min(box_width, box_height));
-            bool can_fit2 = (max_height <= max(box_width, box_height) &&
-                             max_width <= min(box_width, box_height));
-            result.push_back(can_fit1 || can_fit2);
+            return dp[i][choose];
         }
-    }
-    return result;
-}
 
-int main()
-{
-    vector<vector<int>> operations = {{0, 1, 3}, {0, 4, 2}, {1, 3, 4}, {1, 3, 2}};
-    vector<bool> res = solution(operations);
+        if (choose == 1)
+        {
+            dp[i][choose] = energy[i] + solve(energy, k, n, i + k, 1, dp);
+        }
+        else
+        {
+            int take = energy[i] + solve(energy, k, n, i + k, 1, dp);
+            int skip = solve(energy, k, n, i + 1, 0, dp);
+            dp[i][choose] = max(take, skip);
+        }
 
-    for (bool b : res)
-    {
-        cout << (b ? "true" : "false") << endl;
+        return dp[i][choose];
     }
 
-    return 0;
-}
+    int maximumEnergy(vector<int> &energy, int k)
+    {
+        int n = energy.size();
+        vector<vector<int>> dp(n, vector<int>(2, INT_MIN));
+        return solve(energy, k, n, 0, 0, dp);
+    }
+};
