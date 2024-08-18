6c37988010ec125a101a3fbcdad1e0662d8e8468
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-08-19 01:53:11 +0530

    Find the Minimum Cost Array Permutation

--- sample.cpp
+++ sample.cpp
@@ -4,58 +4,58 @@ using namespace std;
 class Solution
 {
 public:
-    int m, n;
-    vector<vector<int>> dp;
-    int ans = INT_MIN;
-
-    int solve(vector<vector<int>> &grid, int i, int j, int score)
+    void dfs(vector<int> &perm, vector<bool> &used, int current_cost, const vector<int> &nums, int &min_cost, vector<int> &best_permutation)
     {
-        if (dp[i][j] != INT_MIN)
+        int n = nums.size();
+        if (perm.size() == n)
         {
-            return dp[i][j] + score;
-        }
-
-        int maxScore = score;
-
-        for (int x = j + 1; x < n; ++x)
-        {
-            int newScore = solve(grid, i, x, score + grid[i][x] - grid[i][j]);
-            maxScore = max(maxScore, newScore);
-        }
-
-        for (int y = i + 1; y < m; ++y)
-        {
-            int newScore = solve(grid, y, j, score + grid[y][j] - grid[i][j]);
-            maxScore = max(maxScore, newScore);
-        }
-
-        dp[i][j] = maxScore - score;
-
-        return maxScore;
-    }
-
-    int maxScore(vector<vector<int>> &grid)
-    {
-        m = grid.size();
-        n = grid[0].size();
-        dp.resize(m, vector<int>(n, INT_MIN));
-
-        for (int i = 0; i < m; ++i)
-        {
-            for (int j = 0; j < n; ++j)
+            int final_cost = current_cost + abs(perm.back() - nums[perm[0]]);
+            if (final_cost < min_cost)
             {
-                for (int x = j + 1; x < n; ++x)
-                {
-                    ans = max(ans, solve(grid, i, x, grid[i][x] - grid[i][j]));
-                }
+                min_cost = final_cost;
+                best_permutation = perm;
+            }
+            return;
+        }
 
-                for (int y = i + 1; y < m; ++y)
+        for (int i = 0; i < n; ++i)
+        {
+            if (!used[i])
+            {
+                if (!perm.empty())
                 {
-                    ans = max(ans, solve(grid, y, j, grid[y][j] - grid[i][j]));
+                    int next_cost = current_cost + abs(perm.back() - nums[i]);
+                    if (next_cost < min_cost)
+                    {
+                        used[i] = true;
+                        perm.push_back(i);
+                        dfs(perm, used, next_cost, nums, min_cost, best_permutation);
+                        perm.pop_back();
+                        used[i] = false;
+                    }
+                }
+                else
+                {
+                    used[i] = true;
+                    perm.push_back(i);
+                    dfs(perm, used, 0, nums, min_cost, best_permutation);
+                    perm.pop_back();
+                    used[i] = false;
                 }
             }
         }
+    }
 
-        return ans;
+    vector<int> findPermutation(vector<int> &nums)
+    {
+        int n = nums.size();
+        int min_cost = numeric_limits<int>::max();
+        vector<int> best_permutation;
+
+        vector<bool> used(n, false);
+        vector<int> perm;
+
+        dfs(perm, used, 0, nums, min_cost, best_permutation);
+        return best_permutation;
     }
 };
--- test.cpp
+++ test.cpp
@@ -1,72 +1,62 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<string> solution(const vector<vector<string>>& paragraphs, int width) {
-    vector<string> result;
-    const string border(width + 2, '*'); // Top and bottom border
-    result.push_back(border);
+class Solution
+{
+public:
+    vector<int> findPermutation(vector<int> &nums)
+    {
+        int n = nums.size();
+        int min_cost = numeric_limits<int>::max();
+        vector<int> best_permutation;
 
-    for (const auto& paragraph : paragraphs) {
-        string line = "*"; // Start the line with a left border asterisk
+        vector<bool> used(n, false);
+        vector<int> perm;
 
-        for (size_t i = 0; i < paragraph.size(); ++i) {
-            const auto& chunk = paragraph[i];
-            if (line.size() + chunk.size() + 1 > width + 1) { // Check if adding the next chunk exceeds the width
-                // Center the current line if it has content
-                if (line.size() > 1) {
-                    int total_spaces = width - (line.size() - 1); // Total spaces needed
-                    int left_spaces = total_spaces / 2;
-                    int right_spaces = total_spaces - left_spaces;
-                    line = "*" + string(left_spaces, ' ') + line.substr(1) + string(right_spaces, ' ') + "*";
-                    result.push_back(line);
+        dfs(perm, used, 0, nums, min_cost, best_permutation);
+        return best_permutation;
+    }
+
+private:
+    void dfs(vector<int> &perm, vector<bool> &used, int current_cost, const vector<int> &nums, int &min_cost, vector<int> &best_permutation)
+    {
+        int n = nums.size();
+        if (perm.size() == n)
+        {
+            int final_cost = current_cost + abs(perm.back() - nums[perm[0]]);
+            if (final_cost < min_cost)
+            {
+                min_cost = final_cost;
+                best_permutation = perm;
+            }
+            return;
+        }
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (!used[i])
+            {
+                if (!perm.empty())
+                {
+                    int next_cost = current_cost + abs(perm.back() - nums[i]);
+                    if (next_cost < min_cost)
+                    {
+                        used[i] = true;
+                        perm.push_back(i);
+                        dfs(perm, used, next_cost, nums, min_cost, best_permutation);
+                        perm.pop_back();
+                        used[i] = false;
+                    }
                 }
-                line = "*"; // Start a new line
-                line += chunk; // Add the current chunk to the new line
-            } else {
-                if (line.size() > 1) { // If line already has some content, add a space before the next chunk
-                    line += " ";
+                else
+                {
+                    used[i] = true;
+                    perm.push_back(i);
+                    dfs(perm, used, 0, nums, min_cost, best_permutation);
+                    perm.pop_back();
+                    used[i] = false;
                 }
-                line += chunk;
             }
         }
-
-        // Push the last line of the paragraph only if it has content
-        if (line.size() > 1) {
-            int total_spaces = width - (line.size() - 1); // Total spaces needed
-            int left_spaces = total_spaces / 2;
-            int right_spaces = total_spaces - left_spaces;
-            line = "*" + string(left_spaces, ' ') + line.substr(1) + string(right_spaces, ' ') + "*";
-            result.push_back(line);
-        }
     }
-
-    result.push_back(border); // Bottom border
-
-    return result;
-}
-
-int main() {
-    // vector<vector<string>> paragraphs = {
-    //     {"there", "are"},
-    //     {"four seasons", "in a year"},
-    //     {"summer", "autumn", "winter", "spring"}
-    // };
-    // int width = 12;
-
-     vector<vector<string>> paragraphs = {
-        {"hello", "world"},
-        {"How", "areYou", "doing"},
-        {"Please look", "and align", "to center"}
-    };
-    int width = 16;
-
-    vector<string> formattedPage = solution(paragraphs, width);
-    for (const auto& line : formattedPage) {
-        cout << line << endl;
-    }
-
-    return 0;
-}
+};
