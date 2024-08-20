abf5fa4ba8ad386d9aed1ae5dac38d0299e4a7ea
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-08-20 13:55:49 +0530

    Special Array II

--- sample.cpp
+++ sample.cpp
@@ -4,58 +4,35 @@ using namespace std;
 class Solution
 {
 public:
-    void dfs(vector<int> &perm, vector<bool> &used, int current_cost, const vector<int> &nums, int &min_cost, vector<int> &best_permutation)
+    vector<bool> isArraySpecial(vector<int> &nums, vector<vector<int>> &queries)
     {
         int n = nums.size();
-        if (perm.size() == n)
-        {
-            int final_cost = current_cost + abs(perm.back() - nums[perm[0]]);
-            if (final_cost < min_cost)
-            {
-                min_cost = final_cost;
-                best_permutation = perm;
-            }
-            return;
-        }
+        vector<int> subarray_id(n);
+        int current_id = 0;
 
-        for (int i = 0; i < n; ++i)
+        subarray_id[0] = current_id;
+
+        for (int i = 1; i < n; ++i)
         {
-            if (!used[i])
+            if (nums[i] % 2 != nums[i - 1] % 2)
             {
-                if (!perm.empty())
-                {
-                    int next_cost = current_cost + abs(perm.back() - nums[i]);
-                    if (next_cost < min_cost)
-                    {
-                        used[i] = true;
-                        perm.push_back(i);
-                        dfs(perm, used, next_cost, nums, min_cost, best_permutation);
-                        perm.pop_back();
-                        used[i] = false;
-                    }
-                }
-                else
-                {
-                    used[i] = true;
-                    perm.push_back(i);
-                    dfs(perm, used, 0, nums, min_cost, best_permutation);
-                    perm.pop_back();
-                    used[i] = false;
-                }
+                subarray_id[i] = current_id;
+            }
+            else
+            {
+                current_id++;
+                subarray_id[i] = current_id;
             }
         }
-    }
 
-    vector<int> findPermutation(vector<int> &nums)
-    {
-        int n = nums.size();
-        int min_cost = numeric_limits<int>::max();
-        vector<int> best_permutation;
+        vector<bool> ans;
+        for (const auto &query : queries)
+        {
+            int l = query[0];
+            int r = query[1];
+            ans.push_back(subarray_id[l] == subarray_id[r]);
+        }
 
-        vector<bool> used(n, false);
-        vector<int> perm;
-
-        dfs(perm, used, 0, nums, min_cost, best_permutation);
-        return best_permutation;
+        return ans;
     }
 };
