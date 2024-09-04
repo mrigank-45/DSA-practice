1d0229bb0959c14523f371ab0e951a03ac7737d7
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-09-04 21:27:06 +0530

    Peaks in Array

--- sample.cpp
+++ sample.cpp
@@ -1,50 +1,122 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+class SegmentTree
 {
 public:
-    long long solve(vector<int> &power, int n, int i, map<int, int> &mp, vector<long long> &dp, vector<int> &nextIndex)
+    SegmentTree(vector<int> &nums) : n(nums.size()), nums(nums)
     {
-        if (i >= n)
+        tree.resize(4 * n, 0);
+        build(0, 0, n - 1);
+    }
+
+    int query(int l, int r)
+    {
+        return query(0, 0, n - 1, l, r);
+    }
+
+    void update(int idx, int val)
+    {
+        nums[idx] = val;
+        update(0, 0, n - 1, idx);
+    }
+
+private:
+    int n;
+    vector<int> nums;
+    vector<int> tree;
+
+    bool isPeak(int i)
+    {
+        if (i <= 0 || i >= n - 1)
+            return false;
+        return nums[i] > nums[i - 1] && nums[i] > nums[i + 1];
+    }
+
+    void build(int node, int start, int end)
+    {
+        if (start == end)
+        {
+            tree[node] = isPeak(start) ? 1 : 0;
+        }
+        else
+        {
+            int mid = (start + end) / 2;
+            int leftChild = 2 * node + 1;
+            int rightChild = 2 * node + 2;
+            build(leftChild, start, mid);
+            build(rightChild, mid + 1, end);
+            tree[node] = tree[leftChild] + tree[rightChild];
+        }
+    }
+
+    int query(int node, int start, int end, int l, int r)
+    {
+        if (r < start || l > end)
         {
             return 0;
         }
-
-        if (dp[i] != -1)
+        if (l <= start && end <= r)
         {
-            return dp[i];
+            return tree[node];
         }
-
-        long long ans = INT_MIN;
-        long long product = ((long long)mp[power[i]] * (long long)power[i]);
-
-        ans = max(ans, product + solve(power, n, nextIndex[i], mp, dp, nextIndex));
-
-        ans = max(ans, solve(power, n, i + mp[power[i]], mp, dp, nextIndex));
-
-        return dp[i] = ans;
+        int mid = (start + end) / 2;
+        int leftChild = 2 * node + 1;
+        int rightChild = 2 * node + 2;
+        return query(leftChild, start, mid, l, r) + query(rightChild, mid + 1, end, l, r);
     }
 
-    long long maximumTotalDamage(vector<int> &power)
+    void update(int node, int start, int end, int idx)
     {
-        int n = power.size();
-        sort(power.begin(), power.end());
-        map<int, int> mp;
-        for (int i = 0; i < n; i++)
+        if (start == end)
         {
-            mp[power[i]]++;
+            tree[node] = isPeak(start) ? 1 : 0;
+        }
+        else
+        {
+            int mid = (start + end) / 2;
+            int leftChild = 2 * node + 1;
+            int rightChild = 2 * node + 2;
+            if (idx <= mid)
+            {
+                update(leftChild, start, mid, idx);
+            }
+            else
+            {
+                update(rightChild, mid + 1, end, idx);
+            }
+            tree[node] = tree[leftChild] + tree[rightChild];
+        }
+        // Update the neighbors as well if necessary
+        if (idx > 0)
+            update(node, start, end, idx - 1);
+        if (idx < n - 1)
+            update(node, start, end, idx + 1);
+    }
+};
+
+class Solution
+{
+public:
+    vector<int> countOfPeaks(vector<int> &nums, vector<vector<int>> &queries)
+    {
+        SegmentTree segTree(nums);
+        vector<int> result;
+
+        for (const auto &query : queries)
+        {
+            if (query[0] == 1)
+            { // Query type 1: count peaks in range [li, ri]
+                int li = query[1], ri = query[2];
+                result.push_back(segTree.query(li, ri));
+            }
+            else if (query[0] == 2)
+            { // Query type 2: update nums[indexi] to vali
+                int indexi = query[1], vali = query[2];
+                segTree.update(indexi, vali);
+            }
         }
 
-        vector<long long> dp(n, -1);
-        vector<int> nextIndex(n);  
-
-        for (int i = 0; i < n; i++)
-        {
-            int val = power[i] + 2;
-            nextIndex[i] = upper_bound(power.begin(), power.end(), val) - power.begin();
-        }
-
-        return solve(power, n, 0, mp, dp, nextIndex);
+        return result;
     }
 };
--- test.cpp
+++ test.cpp
@@ -1,49 +1,141 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+class SegmentTree
 {
 public:
-    long long solve(vector<int> &power, int n, int i, map<int, int> &mp, vector<long long> &dp)
+    SegmentTree(vector<int> &nums) : n(nums.size()), nums(nums)
     {
-        if (i == n)
+        tree.resize(4 * n, 0);
+        build(0, 0, n - 1);
+    }
+
+    int query(int l, int r)
+    {
+        return queryTree(0, 0, n - 1, l, r);
+    }
+
+    void update(int idx, int val)
+    {
+        nums[idx] = val;
+        updateTree(0, 0, n - 1, idx);
+    }
+
+private:
+    int n;
+    vector<int> nums;
+    vector<int> tree;
+
+    bool isPeak(int i)
+    {
+        if (i <= 0 || i >= n - 1)
+            return false;
+        return nums[i] > nums[i - 1] && nums[i] > nums[i + 1];
+    }
+
+    void build(int node, int start, int end)
+    {
+        if (start == end)
+        {
+            tree[node] = isPeak(start) ? 1 : 0;
+        }
+        else
+        {
+            int mid = (start + end) / 2;
+            int leftChild = 2 * node + 1;
+            int rightChild = 2 * node + 2;
+            build(leftChild, start, mid);
+            build(rightChild, mid + 1, end);
+            tree[node] = tree[leftChild] + tree[rightChild];
+        }
+    }
+
+    int queryTree(int node, int start, int end, int l, int r)
+    {
+        if (r < start || l > end)
         {
             return 0;
         }
-
-        if (dp[i] != -1)
+        if (l <= start && end <= r)
         {
-            return dp[i];
+            return tree[node];
         }
-
-        long long ans = INT_MIN;
-
-        // cast
-        if (mp.find(power[i]) == mp.end() || mp[power[i]] == 0)
-        {
-
-            mp[power[i] + 1]++;
-            mp[power[i] - 1]++;
-            mp[power[i] + 2]++;
-            mp[power[i] - 2]++;
-            ans = max(ans, power[i] + solve(power, n, i + 1, mp, dp));
-            mp[power[i] + 1]--;
-            mp[power[i] - 1]--;
-            mp[power[i] + 2]--;
-            mp[power[i] - 2]--;
-        }
-
-        // not cast
-        ans = max(ans, solve(power, n, i + 1, mp, dp));
-
-        return dp[i] = ans;
+        int mid = (start + end) / 2;
+        int leftChild = 2 * node + 1;
+        int rightChild = 2 * node + 2;
+        return queryTree(leftChild, start, mid, l, r) + queryTree(rightChild, mid + 1, end, l, r);
     }
-    long long maximumTotalDamage(vector<int> &power)
-    {
-        int n = power.size();
-        map<int, int> mp;
-        vector<long long> dp(n + 1, -1);
 
-        return solve(power, n, 0, mp, dp);
+    void updateTree(int node, int start, int end, int idx)
+    {
+        if (start == end)
+        {
+            tree[node] = isPeak(start) ? 1 : 0;
+        }
+        else
+        {
+            int mid = (start + end) / 2;
+            int leftChild = 2 * node + 1;
+            int rightChild = 2 * node + 2;
+            if (idx <= mid)
+            {
+                updateTree(leftChild, start, mid, idx);
+            }
+            else
+            {
+                updateTree(rightChild, mid + 1, end, idx);
+            }
+            tree[node] = tree[leftChild] + tree[rightChild];
+        }
+
+        // Update the peaks for the neighboring elements
+        if (idx > start)
+        {
+            int leftMid = (start + end) / 2;
+            if (idx <= leftMid)
+                updateTree(node, start, leftMid, idx - 1);
+            else
+                updateTree(node, leftMid + 1, end, idx + 1);
+        }
     }
 };
+
+class Solution
+{
+public:
+    vector<int> countOfPeaks(vector<int> &nums, vector<vector<int>> &queries)
+    {
+        SegmentTree segTree(nums);
+        vector<int> result;
+
+        for (const auto &query : queries)
+        {
+            if (query[0] == 1)
+            { // Query type 1: count peaks in range [li, ri]
+                int li = query[1], ri = query[2];
+                result.push_back(segTree.query(li, ri));
+            }
+            else if (query[0] == 2)
+            { // Query type 2: update nums[indexi] to vali
+                int indexi = query[1], vali = query[2];
+                segTree.update(indexi, vali);
+            }
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    vector<int> nums = {4, 1, 4, 2, 1, 5};
+    vector<vector<int>> queries = {{2, 2, 4}, {1, 0, 2}, {1, 0, 4}};
+    Solution sol;
+    vector<int> result = sol.countOfPeaks(nums, queries);
+
+    for (int r : result) {
+        cout << r << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
