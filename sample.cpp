a7c6d69973a3f839d0cbf057039fc371b56b64ae
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-09-04 20:05:19 +0530

    Count Pairs That Form a Complete Day I

--- sample.cpp
+++ sample.cpp
@@ -4,43 +4,21 @@ using namespace std;
 class Solution
 {
 public:
-    int solve(int i, int curr, vector<int> &rewardValues, int n, vector<vector<int>> &dp)
+    int countCompleteDayPairs(vector<int> &hours)
     {
-        if (i < 0)
+        int n = hours.size();
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
         {
-            return 0;
+            for (int j = i + 1; j < n; j++)
+            {
+                if ((hours[i] + hours[j]) % 24 == 0)
+                {
+                    cnt++;
+                }
+            }
         }
-
-        if (dp[i][curr] != -1)
-        {
-            return dp[i][curr];
-        }
-
-        // not choose
-        int res = solve(i - 1, curr, rewardValues, n, dp);
-
-        // choose
-        if (rewardValues[i] < curr)
-        {
-            res = max(res, rewardValues[i] + solve(i - 1, min(curr - rewardValues[i],rewardValues[i]), rewardValues, n, dp));
-        }
-        else if (curr == 0)
-        {
-            res = max(res, rewardValues[i] + solve(i - 1, rewardValues[i], rewardValues, n, dp));
-        }
-
-        return dp[i][curr] = res;
-    }
-    int maxTotalReward(vector<int> &rewardValues)
-    {
-        int n = rewardValues.size();
-        sort(rewardValues.begin(), rewardValues.end());
-
-        int max_value = *max_element(rewardValues.begin(), rewardValues.end());
-
-        // 2d DP
-        vector<vector<int>> dp(n + 1, vector<int>(max_value + 1, -1));
-
-        return solve(n-1, 0, rewardValues, n, dp);
+        return cnt;
     }
 };
