1d1ea5619bf19e2d5a7676e2f2f29ee08ead797b
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-10-04 19:55:09 +0530

    Find the Maximum Length of Valid Subsequence II

--- sample.cpp
+++ sample.cpp
@@ -4,54 +4,43 @@ using namespace std;
 class Solution
 {
 public:
-    int maximumLength(vector<int> &nums)
+    int solve(int i, int rem, vector<vector<int>> &dp, vector<int> &nums, int k)
+    {
+        if (dp[i][rem] != 0)
+        {
+            return dp[i][rem];
+        }
+
+        int res = 1;
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if ((nums[j] + nums[i]) % k == rem)
+            {
+                res += solve(j, rem, dp, nums, k);
+                break;
+            }
+        }
+        dp[i][rem] = res;
+        return res;
+    }
+    int maximumLength(vector<int> &nums, int k)
     {
         int n = nums.size();
-        int even_cnt = 0, odd_cnt = 0, first_even = -1, first_odd = -1;
+        vector<vector<int>> dp(n, vector<int>(k, 0));
+
+        int res = 0;
         for (int i = 0; i < n; i++)
         {
-            if (nums[i] % 2 == 0)
+            for (int j = i + 1; j < n; j++)
             {
-                if (first_even == -1)
+                int len = 1 + solve(j, (nums[i] + nums[j]) % k, dp, nums, k);
+                if (len > res)
                 {
-                    first_even = i;
+                    res = len;
                 }
-                even_cnt++;
-            }
-            else
-            {
-                if (first_odd == -1)
-                {
-                    first_odd = i;
-                }
-                odd_cnt++;
             }
         }
 
-        // odd_first subsequence
-        int odd_length = 1;
-        int parity = 0;
-        for (int i = first_odd + 1; i < n; i++)
-        {
-            if (nums[i] % 2 == parity)
-            {
-                odd_length++;
-                parity = 1 - parity;
-            }
-        }
-
-        // even_first subsequence
-        int even_length = 1;
-        parity = 1;
-        for (int i = first_even + 1; i < n; i++)
-        {
-            if (nums[i] % 2 == parity)
-            {
-                even_length++;
-                parity = 1 - parity;
-            }
-        }
-
-        return max(max(even_cnt, odd_cnt), max(even_length, odd_length));
+        return res;
     }
 };
--- test.cpp
+++ test.cpp
@@ -1,61 +1,31 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
-string expandString(string &inputStr) {
-    string result;
-    int i = 0;
-    while (i < inputStr.size()) {
-        if (inputStr[i] == '(') {
-            // Find the closing parenthesis
-            int j = i + 1;
-            while (j < inputStr.size() && inputStr[j] != ')') {
-                j++;
-            }
-            // Extract the pattern
-            string pattern = inputStr.substr(i + 1, j - i - 1);
-            i = j + 1; // Move to the '{'
+class Solution
+{
+public:
+    int maximumLength(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        int max_len = 0;
 
-            // Find the closing curly bracket
-            j = i + 1;
-            while (j < inputStr.size() && inputStr[j] != '}') {
-                j++;
+        for (int rem = 0; rem < k; rem++)
+        {
+            vector<int> dp(n, 1);
+            for (int i = 0; i < n; i++)
+            {
+                for (int prev = 0; prev < i; prev++)
+                {
+                    if ((nums[prev] + nums[i]) % k == rem)
+                    {
+                        dp[i] = max(dp[i], dp[prev] + 1);
+                    }
+                }
             }
-            // Extract the count
-            int count = stoi(inputStr.substr(i + 1, j - i - 1));
-            i = j + 1; // Move past the '}'
 
-            // Expand the pattern
-            for (int k = 0; k < count; k++) {
-                result += pattern;
-            }
-        } else {
-            result += inputStr[i];
-            i++;
+            max_len = max(max_len, *max_element(dp.begin(), dp.end()));
         }
+
+        return max_len;
     }
-    return result;
-}
-
-int main() {
-    // Test case 1
-    string input1 = "(ab){3}";
-    cout << "Input: " << input1 << endl;
-    cout << "Output: " << expandString(input1) << endl; // Expected output: ababab
-
-    // Test case 2
-    string input2 = "(ab){3}(cd){2}";
-    cout << "Input: " << input2 << endl;
-    cout << "Output: " << expandString(input2) << endl; // Expected output: abababcdcd
-
-    // Additional test cases
-    string input3 = "(a){5}(b){3}";
-    cout << "Input: " << input3 << endl;
-    cout << "Output: " << expandString(input3) << endl; // Expected output: aaaaabbb
-
-    string input4 = "(xyz){2}(pq){4}";
-    cout << "Input: " << input4 << endl;
-    cout << "Output: " << expandString(input4) << endl; // Expected output: xyzxyzpqpqpqpq
-
-    return 0;
-}
+};
