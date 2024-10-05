03113135e3b2762c08c57fff4bc6940c000aff6a
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-10-05 23:12:02 +0530

    Maximum Strong Pair XOR I

--- sample.cpp
+++ sample.cpp
@@ -1,43 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution
 {
 public:
-    vector<int> ans;
-    void solve(TreeNode *root, int sum)
+    int maximumStrongPairXor(vector<int> &nums)
     {
-        if (root == NULL)
+        int n = nums.size();
+        int ans = INT_MIN;
+
+        for (int i = 0; i < n; i++)
         {
-            return;
+            for (int j = i; j < n; j++)
+            {
+                if(abs(nums[i] - nums[j]) <= min(nums[i], nums[j]))
+                {
+                    ans = max(ans, nums[i] ^ nums[j]);
+                }
+            }
         }
-        sum = sum * 10 + root->val;
-        if (root->left == NULL && root->right == NULL)
-        {
-            ans.push_back(sum);
-            return;
-        }
-        solve(root->left, sum);
-        solve(root->right, sum);
-    }
-    int sumNumbers(TreeNode *root)
-    {
-        solve(root, 0);
-        int res = 0;
-        for (int i = 0; i < ans.size(); i++)
-        {
-            res += ans[i];
-        }
-        return res;
+        return ans;
     }
 };
