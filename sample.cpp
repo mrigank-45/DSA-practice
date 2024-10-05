3f06f169f7fc4aad45a08b3a1891448d43b7235f
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-10-05 23:46:02 +0530

    High-Access Employees

--- sample.cpp
+++ sample.cpp
@@ -4,21 +4,39 @@ using namespace std;
 class Solution
 {
 public:
-    int maximumStrongPairXor(vector<int> &nums)
+    vector<string> findHighAccessEmployees(vector<vector<string>> &access_times)
     {
-        int n = nums.size();
-        int ans = INT_MIN;
+        int n = access_times.size();
+        unordered_set<string> ans;
+        vector<string> res;
 
+        sort(access_times.begin(), access_times.end());
+
+        int time = stoi(access_times[0][1]);
+        string curr = access_times[0][0];
         for (int i = 0; i < n; i++)
         {
-            for (int j = i; j < n; j++)
+            curr = access_times[i][0];
+            time = stoi(access_times[i][1]);
+            i += 2;
+            if (i >= n)
             {
-                if(abs(nums[i] - nums[j]) <= min(nums[i], nums[j]))
-                {
-                    ans = max(ans, nums[i] ^ nums[j]);
-                }
+                break;
+            }
+            if (access_times[i][0] == curr && stoi(access_times[i][1]) - time < 100)
+            {
+                ans.insert(curr);
+            }
+            else
+            {
+                i -= 2;
             }
         }
-        return ans;
+
+        for(auto &x: ans)
+        {
+            res.push_back(x);
+        }
+        return res;
     }
 };
