b42b5b7a31516bd28c4af9d2d634e2d84cc8a97e
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-10-14 19:44:10 +0530

    Count Submatrices With Equal Frequency of X and Y

--- sample.cpp
+++ sample.cpp
@@ -4,25 +4,41 @@ using namespace std;
 class Solution
 {
 public:
-    long long maxKelements(vector<int> &nums, int k)
+    int numberOfSubmatrices(vector<vector<char>> &grid)
     {
-        int n = nums.size();
-        priority_queue<int> pq;
+        int rows = grid.size();
+        int cols = grid[0].size();
 
-        for(int i = 0; i < n; i++)
+        vector<vector<int>> prefix_X(rows + 1, vector<int>(cols + 1, 0));
+        vector<vector<int>> prefix_Y(rows + 1, vector<int>(cols + 1, 0));
+
+        for (int i = 1; i <= rows; ++i)
         {
-            pq.push(nums[i]);
+            for (int j = 1; j <= cols; ++j)
+            {
+                prefix_X[i][j] = prefix_X[i - 1][j] + prefix_X[i][j - 1] - prefix_X[i - 1][j - 1] + (grid[i - 1][j - 1] == 'X' ? 1 : 0);
+                prefix_Y[i][j] = prefix_Y[i - 1][j] + prefix_Y[i][j - 1] - prefix_Y[i - 1][j - 1] + (grid[i - 1][j - 1] == 'Y' ? 1 : 0);
+            }
         }
-        long long ans = 0;
 
-        while(k--)
+        int result = 0;
+
+        for (int r2 = 0; r2 < rows; ++r2)
         {
-            int ele = pq.top();
-            ans += ele;
-            pq.pop();
-            pq.push(ceil((double)ele / 3));
-        }
-        return ans;
+            for (int c2 = 0; c2 < cols; ++c2)
+            {
+                int x_count = 0, y_count = 0;
 
+                x_count = prefix_X[r2 + 1][c2 + 1];
+                y_count = prefix_Y[r2 + 1][c2 + 1];
+
+                if (x_count > 0 && x_count == y_count)
+                {
+                    result++;
+                }
+            }
+        }
+
+        return result;
     }
 };
