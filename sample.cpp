21526db4a2042b81eb01c33dae7a0669321e6fbb
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-10-15 16:36:35 +0530

    Separate Black and White Balls

--- sample.cpp
+++ sample.cpp
@@ -4,41 +4,21 @@ using namespace std;
 class Solution
 {
 public:
-    int numberOfSubmatrices(vector<vector<char>> &grid)
+    long long minimumSteps(string s)
     {
-        int rows = grid.size();
-        int cols = grid[0].size();
+        int n = s.size();
+        long long ans = 0;
+        long long cnt = 0;
 
-        vector<vector<int>> prefix_X(rows + 1, vector<int>(cols + 1, 0));
-        vector<vector<int>> prefix_Y(rows + 1, vector<int>(cols + 1, 0));
-
-        for (int i = 1; i <= rows; ++i)
+        for (int i = 0; i < n; i++)
         {
-            for (int j = 1; j <= cols; ++j)
+            if (s[i] == '0')
             {
-                prefix_X[i][j] = prefix_X[i - 1][j] + prefix_X[i][j - 1] - prefix_X[i - 1][j - 1] + (grid[i - 1][j - 1] == 'X' ? 1 : 0);
-                prefix_Y[i][j] = prefix_Y[i - 1][j] + prefix_Y[i][j - 1] - prefix_Y[i - 1][j - 1] + (grid[i - 1][j - 1] == 'Y' ? 1 : 0);
+                ans += i - cnt;
+                cnt++;
             }
         }
 
-        int result = 0;
-
-        for (int r2 = 0; r2 < rows; ++r2)
-        {
-            for (int c2 = 0; c2 < cols; ++c2)
-            {
-                int x_count = 0, y_count = 0;
-
-                x_count = prefix_X[r2 + 1][c2 + 1];
-                y_count = prefix_Y[r2 + 1][c2 + 1];
-
-                if (x_count > 0 && x_count == y_count)
-                {
-                    result++;
-                }
-            }
-        }
-
-        return result;
+        return ans;
     }
 };
