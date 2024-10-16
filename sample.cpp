62f74b9f80b212d64e7e39901aedd4d1586f2c97
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-10-17 00:37:13 +0530

    Knight Probability in Chessboard

--- sample.cpp
+++ sample.cpp
@@ -1,26 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class SortedStack
+
+class Solution
 {
 public:
-    stack<int> s;
-    void sort();
-};
+    const vector<pair<int, int>> knightMoves = {
+        {2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}};
 
-void SortedStack ::sort()
-{
-    stack<int> temp;
-    while (!s.empty())
+    bool isValid(int x, int y, int N)
     {
-        int x = s.top();
-        s.pop();
-        while (!temp.empty() && temp.top() > x)
-        {
-            s.push(temp.top());
-            temp.pop();
-        }
-        temp.push(x);
+        return (x >= 0 && x < N && y >= 0 && y < N);
     }
-    s = temp;
-}
+
+    double dp[101][26][26];
+
+    double solve(int n, int k, int x, int y)
+    {
+        if (!isValid(x, y, n)) return 0.0;
+        if (k == 0) return 1.0;
+
+        if (dp[k][x][y] != -1) return dp[k][x][y];
+
+        double ans = 0.0;
+        for (auto move : knightMoves)
+        {
+            ans += solve(n, k - 1, x + move.first, y + move.second);
+        }
+
+        return dp[k][x][y] = ans;
+    }
+
+    double knightProbability(int n, int k, int row, int column)
+    {
+        memset(dp, -1, sizeof(dp));
+        double ways = solve(n, k, row, column);
+        return ways / pow(8, k);
+    }
+};
--- test.cpp
+++ test.cpp
@@ -1,12 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
+class Solution
 {
-    int a = -4;
-    int b = 5;
-    cout << a % b << endl;
-}
+    vector<pair<short, short>> dir = {{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}};
+    double solve(int n, int k, int i, int j, vector<vector<vector<double>>> &dp)
+    {
+        if (i < 0 || j < 0 || j >= n || i >= n)
+            return 0;
+        if (k == 0)
+            return 1;
+        if (dp[k][i][j] != -1)
+            return dp[k][i][j];
+        double ans = 0;
+        for (short p = 0; p < 8; p++)
+        {
+            ans += solve(n, k - 1, i + dir[p].first, j + dir[p].second, dp);
+        }
+        return dp[k][i][j] = ans;
+    }
+
+public:
+    double knightProbability(int n, int k, int row, int column)
+    {
+        vector<vector<vector<double>>> dp(k + 1, vector<vector<double>>(n + 1, vector<double>(n + 1, -1)));
+        return solve(n, k, row, column, dp) / pow(8, k);
+    }
+};
