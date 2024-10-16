bd2760352977e31c2c22f06099c20fd8f30c9867
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-10-16 23:33:49 +0530

    Sort a Stack

--- sample.cpp
+++ sample.cpp
@@ -1,39 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+class SortedStack
 {
 public:
-    bool canArrange(vector<int> &arr, int k)
-    {
-        int n = arr.size();
-        unordered_map<int, int> mp; // {rem, freq}
-
-        for (int i = 0; i < n; i++)
-        {
-            int rem = arr[i] % k;
-            if (rem < 0)
-                rem += k;
-            mp[rem]++;
-        }
-
-        for (auto it : mp)
-        {
-            int rem = it.first;
-            int freq = it.second;
-
-            if (rem == 0 || 2 * rem == k)
-            {
-                if (freq % 2 != 0)
-                    return false;
-            }
-            else
-            {
-                if (mp[k - rem] != freq)
-                    return false;
-            }
-        }
-
-        return true;
-    }
+    stack<int> s;
+    void sort();
 };
+
+void SortedStack ::sort()
+{
+    stack<int> temp;
+    while (!s.empty())
+    {
+        int x = s.top();
+        s.pop();
+        while (!temp.empty() && temp.top() > x)
+        {
+            s.push(temp.top());
+            temp.pop();
+        }
+        temp.push(x);
+    }
+    s = temp;
+}
