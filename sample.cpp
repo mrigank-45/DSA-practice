68e145f708e379211e9831e04e687ca0816f19d8
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-10-22 22:08:15 +0530

    Jump Game III

--- sample.cpp
+++ sample.cpp
@@ -1,89 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to calculate S(n, i)
-long long stirlingSecondKind(int n, int i)
-{
-    vector<vector<long long>> dp(n + 1, vector<long long>(i + 1, 0));
-    dp[0][0] = 1;
-
-    for (int ni = 1; ni <= n; ++ni)
-    {
-        for (int ii = 1; ii <= i; ++ii)
-        {
-            dp[ni][ii] = ii * dp[ni - 1][ii] + dp[ni - 1][ii - 1];
-        }
-    }
-
-    return dp[n][i];
-}
-
-// Function to calculate C(x, i)
-long long binomialCoefficient(int x, int i)
-{
-    if (i > x)
-    {
-        return 0;
-    }
-    if (i == 0 || i == x)
-    {
-        return 1;
-    }
-
-    if (i > x - i)
-    {
-        i = x - i;
-    }
-
-    long long result = 1;
-    for (int k = 0; k < i; ++k)
-    {
-        result *= (x - k);
-        result /= (k + 1);
-    }
-
-    return result;
-}
-
-// Function to i factorial
-long long factorial(int i)
-{
-    long long result = 1;
-    for (int j = 1; j <= i; ++j)
-    {
-        result *= j;
-    }
-
-    return result;
-}
-
-// Function for modular exponentiation: (base^exp) % mod
-long long modPow(long long base, long long exp, long long mod) {
-    long long result = 1;
-    base = base % mod;
-    while (exp > 0) {
-        if (exp % 2 == 1) {
-            result = (result * base) % mod;
-        }
-        base = (base * base) % mod;
-        exp /= 2;
-    }
-    return result;
-}
-
 class Solution
 {
 public:
-    int numberOfWays(int n, int x, int y)
+    int dfs(vector<int> &arr, int curr, vector<bool> &visited, vector<int> &dp)
     {
-        int ans = 0;
-        int mod = 1e9 + 7;
+        if (curr < 0 || curr >= arr.size())
+            return 0;
 
-        for (int i = 1; i <= x; i++)
-        {
-            ans += ((((((stirlingSecondKind(n, i) % mod) * (binomialCoefficient(x, i) % mod)) % mod)
-            * (factorial(i) % mod)) % mod) * (modPow(y, i, mod) % mod)) % mod;
-        }
-        return ans;
+        if (visited[curr])
+            return 0;
+
+        if (arr[curr] == 0)
+            return 1;
+
+        if (dp[curr] != -1)
+            return dp[curr];
+
+        visited[curr] = true;
+
+        int left = dfs(arr, curr - arr[curr], visited, dp);
+        int right = dfs(arr, curr + arr[curr], visited, dp);
+
+        visited[curr] = false;
+
+        return dp[curr] = left || right;
+    }
+    bool canReach(vector<int> &arr, int curr)
+    {
+        int n = arr.size();
+
+        vector<bool> visited(n, false);
+        vector<int> dp(n, -1);
+
+        return dfs(arr,curr,visited,dp);
     }
 };
--- test.cpp
+++ test.cpp
@@ -1,38 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+int solve(vector<int> &arr, int n, int i, bool flag, vector<vector<int>> &dp)
 {
-public:
-    string printMinNumberForPattern(string s)
+    if (i == n)
     {
-        int n = s.length();
-        string ans = "";
-
-        // Initialize the result with numbers 1 to n+1
-        for (int i = 0; i <= n; i++)
-        {
-            ans += to_string(i + 1);
-        }
-
-        // Traverse the pattern and reverse portions where there's a 'D' sequence
-        for (int i = 0; i < n; i++)
-        {
-            if (s[i] == 'D')
-            {
-                // Find the end of the current D-sequence
-                int j = i;
-                while (j < n && s[j] == 'D')
-                {
-                    j++;
-                }
-                // Reverse the segment corresponding to the D sequence
-                reverse(ans.begin() + i, ans.begin() + j + 1);
-                // Move i to the end of the D-sequence
-                i = j - 1;
-            }
-        }
-
-        return ans;
+        return 0;
     }
-};
+
+    if (dp[i][flag] != -1)
+    {
+        return dp[i][flag];
+    }
+
+    if (flag)
+    {
+        return dp[i][flag] = max(arr[i] + solve(arr, n, i + 1, !flag, dp), solve(arr, n, i + 1, flag, dp));
+    }
+    else
+    {
+        return dp[i][flag] = solve(arr, n, i + 1, !flag, dp);
+    }
+}
+
+int tabulation(vector<int> &arr)
+{
+    int n = arr.size();
+    vector<vector<int>> dp(n + 1, vector<int>(2, -1));
+
+    dp[n][0] = 0;
+    dp[n][1] = 0;
+
+    for(int i = n - 1; i >= 0; i--)
+    {
+        dp[i][1] = max(arr[i] + dp[i + 1][0], dp[i + 1][1]);
+        dp[i][0] = dp[i + 1][1];
+    }
+
+    return dp[0][1];
+}
+
+int tabulationSpace(vector<int> &arr)
+{
+    int n = arr.size();
+
+    pair<int,int> curr, next;
+    next.first = 0;
+    next.second = 0;
+
+    for(int i = n - 1; i >= 0; i--)
+    {
+        curr.second = max(arr[i] + next.first, next.second);
+        curr.first = next.second;
+        
+        next = curr;
+    }
+
+    return curr.second;
+}
+
+int maxLoot(vector<int> &arr)
+{
+    int n = arr.size();
+    vector<vector<int>> dp(n + 1, vector<int>(2, -1));
+
+    return solve(arr, n, 0, 1, dp);
+}
+
+int main()
+{
+    vector<int> arr = {6, 7, 1, 3, 8, 2, 4};
+    cout << tabulationSpace(arr);
+    return 0;
+}
