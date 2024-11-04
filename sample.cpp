c51efcfa0151fdcc0d483e6969e1a527a885526e
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-11-04 14:41:11 +0530

    Daily Temperatures

--- sample.cpp
+++ sample.cpp
@@ -1,34 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution
 {
 public:
-    int solve(TreeNode *root, int maxVal)
+
+    vector<int> dailyTemperatures(vector<int> &temperatures)
     {
-        if (root == NULL)
+        int n = temperatures.size();
+        stack<int> s;
+        vector<int> ans(n);
+
+        for (int i = n - 1; i >= 0; i--)
         {
-            return 0;
+            int curr = temperatures[i];
+            while (!s.empty() && temperatures[s.top()] <= curr)
+            {
+                s.pop();
+            }
+
+            ans[i] = s.empty() ? 0 : s.top() - i;
+            s.push(i);
         }
-        int ans = 0;
-        if (root->val >= maxVal)
-            ans++;
-        ans += solve(root->left, max(maxVal, root->val));
-        ans += solve(root->right, max(maxVal, root->val));
         return ans;
-    }
-    int goodNodes(TreeNode *root)
-    {
-        return solve(root, INT_MIN);
     }
 };
