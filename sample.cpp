4076ac380eb614b34cbf31359bfa65181b054712
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-11-11 00:55:00 +0530

    Partition Labels

--- sample.cpp
+++ sample.cpp
@@ -4,36 +4,42 @@ using namespace std;
 class Solution
 {
 public:
-    bool dfs(int src, int parent, vector<int> adj[], vector<bool> &vis)
+    vector<int> partitionLabels(string s)
     {
-        vis[src] = true;
-        for (auto adjnode : adj[src])
-        {
-            if (!vis[adjnode])
-            {
-                if (dfs(adjnode, src, adj, vis))
-                    return true;
-            }
-            else if (adjnode != parent)
-                return true;
-        }
-        return false;
-    }
+        int n = s.size();
 
-    vector<int> findRedundantConnection(vector<vector<int>> &edges)
-    {
-        int n = edges.size();
-        vector<int> adj[n + 1];
+        unordered_map<char, int> mp;
         for (int i = 0; i < n; i++)
         {
-            adj[edges[i][0]].push_back(edges[i][1]);
-            adj[edges[i][1]].push_back(edges[i][0]);
-            vector<bool> vis(n + 1, false);
-            if (dfs(edges[i][0], -1, adj, vis))
+            mp[s[i]]++;
+        }
+        vector<int> ans;
+
+        int len = 0;
+        unordered_map<char, int> temp;
+
+        for(int i = 0; i < n; i++)
+        {
+            len++;
+            temp[s[i]]++;
+
+            bool flag = true;
+            for(auto x : temp)
             {
-                return edges[i];
+                if(x.second != mp[x.first])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag){
+                ans.push_back(len);
+                len = 0;
+                temp.clear();
             }
         }
-        return {};
+
+        return ans;
     }
 };
