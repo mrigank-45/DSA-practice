84d7a149cf994ed5bacadfd6e02413cf436fc6fc
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-11-11 00:02:51 +0530

    Meeting Schedule II

--- sample.cpp
+++ sample.cpp
@@ -1,89 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class DisjointSet
+class Interval
 {
-    vector<int> rank, parent;
-
 public:
-    DisjointSet(int n)
+    int start, end;
+    Interval(int start, int end)
     {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        // initialize the parent and rank array
-        for (int i = 0; i <= n; i++)
-        {
-            parent[i] = i;
-            rank[i] = 0;
-        }
-    }
-
-    int findParent(int node)
-    {
-        // base case: it means it's last node
-        if (node == parent[node])
-            return node;
-        return parent[node] = findParent(parent[node]); // For Path Compression
-        // return findParent(parent[node]);
-    }
-
-    void unionByRank(int u, int v)
-    {
-        int ulp_u = findParent(u);
-        int ulp_v = findParent(v);
-        if (ulp_u == ulp_v)
-            return;
-        if (rank[ulp_u] < rank[ulp_v])
-        {
-            parent[ulp_u] = ulp_v;
-        }
-        else if (rank[ulp_v] < rank[ulp_u])
-        {
-            parent[ulp_v] = ulp_u;
-        }
-        else
-        {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++; // update the rank
-        }
+        this->start = start;
+        this->end = end;
     }
 };
 
 class Solution
 {
 public:
-    int minCostConnectPoints(vector<vector<int>> &points)
+    int minMeetingRooms(vector<Interval> &intervals)
     {
-        int n = points.size();
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), [](Interval &a, Interval &b) {
+            return a.start < b.start;
+        });
 
-        vector<pair<int, pair<int, int>>> edges;
-        for (int i = 0; i < n; i++)
+        vector<pair<int, int>> days;
+
+        for (auto interval : intervals)
         {
-            for (int j = i + 1; j < n; j++)
+            int start = interval.start;
+            int end = interval.end;
+
+            bool isInserted = false;
+            for (int i = 0; i < days.size(); i++)
             {
-                int wt = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
-                edges.push_back({wt, {i, j}});
+                if (days[i].second <= start)
+                {
+                    days[i].first = start;
+                    days[i].second = end;
+                    isInserted = true;
+                    break;
+                }
+            }
+
+            if (!isInserted)
+            {
+                days.push_back({start, end});
             }
         }
-        sort(edges.begin(), edges.end());
 
-        // initialy we assume all nodes are disconnected
-        DisjointSet ds(n);
-        int mstWt = 0;
-
-        for (auto it : edges)
-        {
-            int wt = it.first;
-            int u = it.second.first;
-            int v = it.second.second;
-
-            if (ds.findParent(u) != ds.findParent(v)) // if not in same component, union them
-            {
-                mstWt += wt; // add the weight to MST
-                ds.unionByRank(u, v);
-            }
-        }
-
-        return mstWt;
+        return days.size();
     }
 };
