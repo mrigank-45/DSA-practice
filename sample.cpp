b0b4338c5f3c024ae03be1bd4050c6083dd86906
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-11-14 02:06:31 +0530

    Upper bound and Lower bounds

--- sample.cpp
+++ sample.cpp
@@ -1,70 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+vector<int> nextSmallerElement(vector<int> &arr, int n)
 {
-public:
-    bool checkValidString(string s)
+    stack<int> s;
+    s.push(-1);
+    vector<int> ans(n);
+
+    for (int i = n - 1; i >= 0; i--)
     {
-        int n = s.size();
-
-        stack<char> st;
-
-        for (int i = 0; i < n; i++)
+        int curr = arr[i];
+        while (s.top() >= curr)
         {
-            if (s[i] == '(' || s[i] == '*')
-            {
-                st.push(s[i]);
-            }
-            else
-            {
-                int cnt = 0;
-                while (!st.empty() && st.top() != '(')
-                {
-                    cnt++;
-                    st.pop();
-                }
-                if (!st.empty())
-                {
-                    st.pop();
-                    while (cnt--)
-                    {
-                        st.push('*');
-                    }
-                }
-                else
-                {
-                    if(cnt==0)
-                    {
-                        return false;
-                    }
-                    cnt--;
-                    while (cnt--)
-                    {
-                        st.push('*');
-                    }
-                }
-            }
+            s.pop();
         }
-
-        int cnt = 0;
-
-        while (!st.empty())
-        {
-            if (st.top() == '*')
-            {
-                cnt++;
-            }
-            else
-            {
-                cnt--;
-            }
-            if (cnt < 0)
-            {
-                return false;
-            }
-            st.pop();
-        }
-        return true;
+        // ans is stack ka top
+        ans[i] = s.top();
+        s.push(curr);
     }
-};
+    return ans;
+}
+
+vector<int> prevGreaterElement(vector<int> &arr, int n)
+{
+    stack<int> s;
+    s.push(1000);
+    vector<int> ans(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int curr = arr[i];
+        while (s.top() <= curr)
+        {
+            s.pop();
+        }
+        // ans is stack ka top
+        ans[i] = s.top();
+        s.push(curr);
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> v = {7, 4, 5, 2, 6};
+    // vector<int> ans = nextSmallerElement(v, 5);
+    vector<int> ans = prevGreaterElement(v, 5);
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << ' ';
+    }
+    cout << endl;
+
+    return 0;
+}
