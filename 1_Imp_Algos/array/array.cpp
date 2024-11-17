8a63c294e68daac936efd004e912ae696bf426ed
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2024-11-18 00:32:48 +0530

    Count the Number of Fair Pairs

--- 1_Imp_Algos/array/array.cpp
+++ 1_Imp_Algos/array/array.cpp
@@ -6,14 +6,12 @@ using namespace std;
 int main()
 {
      // Note that the array is sorted
-     int gfg[] = {5, 6, 6, 6, 8};
-
-     vector<int> v(gfg, gfg + 8); // 5 5 5 6 6 6 7 7
+     vector<int> v = {5, 6, 6, 6, 8};
 
      int lower = *lower_bound(v.begin(), v.end(), 7);  // gives iterator to element equal to or greater than val, else gives v.end()
      int upper = *upper_bound(v.begin(), v.end(), 10); // gives iterator to element greater than val, else gives v.end()
 
-     int index = upper_bound(v.begin(), v.end(), 7) - v.begin(); // gives index of element greater than val
+     int index = upper_bound(v.begin(), v.end(), 12) - v.begin(); // gives index of element greater than val
      cout << "index: " << index << endl;
 
      int cnt = v.end() - upper_bound(v.begin(), v.end(), 6); // gives count of elements greater than val
--- sample.cpp
+++ sample.cpp
@@ -1,57 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> nextSmallerElement(vector<int> &arr, int n)
+class Solution
 {
-    stack<int> s;
-    s.push(-1);
-    vector<int> ans(n);
-
-    for (int i = n - 1; i >= 0; i--)
+public:
+    long long countFairPairs(vector<int> &nums, int lower, int upper)
     {
-        int curr = arr[i];
-        while (s.top() >= curr)
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        long long count = 0;
+
+        for(int i = 0; i < n; i++)
         {
-            s.pop();
+            int l = lower - nums[i];
+            int u = upper - nums[i];
+
+            int a = lower_bound(nums.begin(), nums.end(), l) - nums.begin();
+
+            int b = upper_bound(nums.begin(), nums.end(), u) - nums.begin() - 1;
+
+            if(a==n || b==-1 || a > b)
+            {
+                continue;
+            }
+
+            count += b - a + 1;
+
         }
-        // ans is stack ka top
-        ans[i] = s.top();
-        s.push(curr);
+
+        return count;
     }
-    return ans;
-}
-
-vector<int> prevGreaterElement(vector<int> &arr, int n)
-{
-    stack<int> s;
-    s.push(1000);
-    vector<int> ans(n);
-
-    for (int i = 0; i < n; i++)
-    {
-        int curr = arr[i];
-        while (s.top() <= curr)
-        {
-            s.pop();
-        }
-        // ans is stack ka top
-        ans[i] = s.top();
-        s.push(curr);
-    }
-    return ans;
-}
-
-int main()
-{
-    vector<int> v = {7, 4, 5, 2, 6};
-    // vector<int> ans = nextSmallerElement(v, 5);
-    vector<int> ans = prevGreaterElement(v, 5);
-
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << ans[i] << ' ';
-    }
-    cout << endl;
-
-    return 0;
-}
+};
