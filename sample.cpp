918bb568c1602abe3c0ced66d9bf0741588cf14e
mrigank-45 <mrigankshukla2015@gmail.com>
2025-07-17 01:11:03 +0530

    Random Pick Index

--- sample.cpp
+++ sample.cpp
@@ -1,30 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> singleNumber(vector<int>& nums) {
-        int overall_xor = 0;
-        for (int n : nums) {
-            overall_xor ^= n;
+    unordered_map<int, vector<int>> mp;
+
+    Solution(vector<int> &nums)
+    {
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            mp[nums[i]].push_back(i);
         }
+    }
 
-        int first_group_xor = 0;
-        int second_group_xor = 0;
-
-        int bit_pos_dif = 0;
-        while (((overall_xor >> bit_pos_dif) & 1) != 1) {
-            bit_pos_dif++;
-        }
-
-        for (int num : nums) {
-            if (((num >> bit_pos_dif) & 1) == 1) {
-                first_group_xor ^= num;
-            } else {
-                second_group_xor ^= num;
-            }
-        }
-
-        return {first_group_xor, second_group_xor};
+    int pick(int target)
+    {
+        int n = mp[target].size();
+        int rand();
+        int index = rand() % n;
+        return mp[target][index];
     }
 };
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * int param_1 = obj->pick(target);
+ */
