083db5f995e7d0b832ad301da5f8b6355c54e44a
mrigank-45 <mrigankshukla2015@gmail.com>
2025-07-19 23:47:17 +0530

    Ones and Zeroes

--- sample.cpp
+++ sample.cpp
@@ -1,26 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class NumMatrix
+class Solution
 {
 public:
-    vector<vector<int>> sumMatrix;
-    NumMatrix(vector<vector<int>> &matrix)
+    unordered_map<int, pair<int, int>> mp;
+    int solve(vector<string> strs, int s, int i, int m, int n, int M, int N, vector<vector<vector<int>>> &dp)
     {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        sumMatrix.resize(n + 1, vector<int>(m + 1, 0));
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-            {
-                sumMatrix[i][j] = matrix[i - 1][j - 1] + sumMatrix[i - 1][j] + sumMatrix[i][j - 1] - sumMatrix[i - 1][j - 1];
-            }
-        }
-    }
+        if (i == s)
+            return 0;
+        
+        if (dp[i][m][n] != -1)
+            return dp[i][m][n];
 
-    int sumRegion(int row1, int col1, int row2, int col2)
+        int ans = 0;
+        // not choose
+        ans = max(ans, solve(strs, s, i + 1, m, n, M, N, dp));
+
+        // choose
+        if (mp[i].first + m <= M && mp[i].second + n <= N)
+        {
+            ans = max(ans, 1 + solve(strs, s, i + 1, m + mp[i].first, n + mp[i].second, M, N, dp));
+        }
+
+        return dp[i][m][n] = ans;
+    }
+    int findMaxForm(vector<string> &strs, int m, int n)
     {
-        return sumMatrix[row2 + 1][col2 + 1] - sumMatrix[row1][col2 + 1] - sumMatrix[row2 + 1][col1] + sumMatrix[row1][col1];
+        int s = strs.size();
+        for (int i = 0; i < s; i++)
+        {
+            string str = strs[i];
+            int m = 0, n = 0;
+            for (int j = 0; j < str.size(); j++)
+            {
+                if (str[j] == '0')
+                {
+                    m++;
+                }
+                else
+                {
+                    n++;
+                }
+            }
+            mp[i] = {m, n};
+        }
+        vector<vector<vector<int>>> dp(s + 1, vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
+        return solve(strs, s, 0, 0, 0, m, n, dp);
     }
 };
