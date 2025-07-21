23e833515976fcfcb8a07a74c2b5943482b27077
mrigank-45 <mrigankshukla2015@gmail.com>
2025-07-21 17:57:47 +0530

    Find Largest Value in Each Tree Row

--- sample.cpp
+++ sample.cpp
@@ -1,51 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 class Solution
 {
 public:
-    unordered_map<int, pair<int, int>> mp;
-    int solve(vector<string> strs, int s, int i, int m, int n, int M, int N, vector<vector<vector<int>>> &dp)
+    vector<int> largestValues(TreeNode *root)
     {
-        if (i == s)
-            return 0;
-        
-        if (dp[i][m][n] != -1)
-            return dp[i][m][n];
+        if(root == NULL)  return {};
+        vector<int> ans;
+        queue<TreeNode *> q;
+        q.push(root);
 
-        int ans = 0;
-        // not choose
-        ans = max(ans, solve(strs, s, i + 1, m, n, M, N, dp));
-
-        // choose
-        if (mp[i].first + m <= M && mp[i].second + n <= N)
+        while(!q.empty())
         {
-            ans = max(ans, 1 + solve(strs, s, i + 1, m + mp[i].first, n + mp[i].second, M, N, dp));
-        }
-
-        return dp[i][m][n] = ans;
-    }
-    int findMaxForm(vector<string> &strs, int m, int n)
-    {
-        int s = strs.size();
-        for (int i = 0; i < s; i++)
-        {
-            string str = strs[i];
-            int m = 0, n = 0;
-            for (int j = 0; j < str.size(); j++)
+            int size = q.size();
+            int maxVal = INT_MIN;
+            for(int i = 0; i < size; i++)
             {
-                if (str[j] == '0')
-                {
-                    m++;
-                }
-                else
-                {
-                    n++;
-                }
+                TreeNode *node = q.front();
+                q.pop();
+                maxVal = max(maxVal, node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
             }
-            mp[i] = {m, n};
+            ans.push_back(maxVal);
         }
-        vector<vector<vector<int>>> dp(s + 1, vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
-        return solve(strs, s, 0, 0, 0, m, n, dp);
+
+        return ans;
     }
 };
