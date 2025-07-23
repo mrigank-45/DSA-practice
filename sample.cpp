d5195462da75a4cc22bcec8040666df36e6553b6
mrigank-45 <mrigankshukla2015@gmail.com>
2025-07-23 15:08:23 +0530

    Minimum Number of Flips to Make Binary Grid Palindromic I

--- sample.cpp
+++ sample.cpp
@@ -4,35 +4,42 @@ using namespace std;
 class Solution
 {
 public:
-    string replaceWords(vector<string> &dictionary, string sentence)
+    int solveColumn(int n, int m, vector<vector<int>> &grid)
     {
-        unordered_set<string> s(dictionary.begin(), dictionary.end());
-        string ans;
-
-        for (int i = 0; i < sentence.size(); i++)
+        int ans = 0;
+        for (int i = 0; i < m; i++)
         {
-            if (sentence[i] == ' ')
-                continue;
-
-            string word = "";
-            while (i < sentence.size() && sentence[i] != ' ')
+            for (int j = 0; j < n; j++)
             {
-                word += sentence[i];
-                i++;
-            }
-
-            for (int j = 1; j <= word.size(); j++)
-            {
-                if (s.find(word.substr(0, j)) != s.end())
-                {
-                    word = word.substr(0, j);
+                int k = n - j - 1;
+                if (k <= j)
                     break;
-                }
+                if (grid[j][i] != grid[k][i])
+                    ans++;
             }
-            ans += word + " ";
         }
-        if (!ans.empty())
-            ans.pop_back(); 
         return ans;
+    }
+    int solveRow(int n, int m, vector<vector<int>> &grid)
+    {
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                int k = m - j - 1;
+                if (k <= j)
+                    break;
+                if (grid[i][j] != grid[i][k])
+                    ans++;
+            }
+        }
+        return ans;
+    }
+    int minFlips(vector<vector<int>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        return min(solveRow(n, m, grid), solveColumn(n, m, grid));
     }
 };
