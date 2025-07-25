7ab1ba83b82c24b11487ad6fce264fd504c50719
mrigank-45 <mrigankshukla2015@gmail.com>
2025-07-25 22:09:33 +0530

    Implement Router

--- sample.cpp
+++ sample.cpp
@@ -1,35 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+class Router
 {
 public:
-    string smallestPalindrome(string s)
+    struct custom_hash
     {
-        int n = s.size();
-        map<char, int> freq;
-        for (char c : s)
+        size_t operator()(const pair<int, pair<int, int>> &p) const
         {
-            freq[c]++;
+            return hash<int>()(p.first) ^ hash<int>()(p.second.first) ^ hash<int>()(p.second.second);
         }
-        char odd = '#';
-        string half = "";
-        for (auto &p : freq)
+    };
+    deque<pair<int, pair<int, int>>> q; // {source, {destination, timestamp}}
+    unordered_set<pair<int, pair<int, int>>, custom_hash> s;
+    int memoryLimit;
+    Router(int memoryLimit)
+    {
+        this->memoryLimit = memoryLimit;
+    }
+
+    bool addPacket(int source, int destination, int timestamp)
+    {
+        if (s.find({source, {destination, timestamp}}) != s.end())
         {
-            if (p.second % 2 == 1)
-            {
-                odd = p.first;
-            }
-            half += string(p.second / 2, p.first);
-        }
-        string otherHalf = half;
-        reverse(otherHalf.begin(), otherHalf.end());
-        if (odd != '#')
-        {
-            half += odd;
+            return false;
         }
 
-        half += otherHalf;
-        return half;
+        if (q.size() == memoryLimit)
+        {
+            auto oldestPacket = q.front();
+            s.erase({oldestPacket.first, {oldestPacket.second.first, oldestPacket.second.second}});
+            q.pop_front();
+        }
+
+        q.push_back({source, {destination, timestamp}});
+        s.insert({source, {destination, timestamp}});
+        return true;
+    }
+
+    vector<int> forwardPacket()
+    {
+        if (q.empty())
+        {
+            return {};
+        }
+        vector<int> result = {q.front().first, q.front().second.first, q.front().second.second};
+        s.erase({q.front().first, {q.front().second.first, q.front().second.second}});
+        q.pop_front();
+        return result;
+    }
+
+    int getCount(int destination, int startTime, int endTime)
+    {
+        int cnt = 0;
+        for (auto &packet : s)
+        {
+            if (packet.second.first == destination && packet.second.second >= startTime && packet.second.second <= endTime)
+            {
+                cnt++;
+            }
+        }
+        return cnt;
     }
 };
