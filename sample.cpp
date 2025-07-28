605a2d55c3113e0febc394abb247a27eaa9805b6
mrigank-45 <mrigankshukla2015@gmail.com>
2025-07-29 01:22:22 +0530

    Maximum Sum With at Most K Elements

--- sample.cpp
+++ sample.cpp
@@ -1,46 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<long long> findMaxSum(vector<int>& nums1, vector<int>& nums2, int k) {
-        int n = nums1.size();
-        vector<long long> ans(n, 0); 
-        
-        vector<pair<int, int>> idx;
-        for (int i = 0; i < n; i++) 
-            idx.push_back({nums1[i], i});
-        
-        sort(idx.begin(), idx.end());
-        
-        priority_queue<int, vector<int>, greater<int>> pq;
-        unordered_map<int, long long> mp; 
-        long long sum = 0;
-        int j = 0;
-        
-        for (int i = 0; i < n; i++) {
-            int index = idx[i].second; 
-            
-            while (j < i) {
-                int prev = idx[j].second; 
-                
-                if (idx[j].first >= idx[i].first) break;
+    long long maxSum(vector<vector<int>> &grid, vector<int> &limits, int k)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        priority_queue<pair<int,int>> pq;
 
-                pq.push(nums2[prev]);
-                sum += nums2[prev];
-
-                if (pq.size() > k) {
-                    sum -= pq.top();
-                    pq.pop();
-                }
-                j++;
+        for(int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                pq.push({grid[i][j], i});
             }
-            
-            mp[index] = sum; 
         }
-        
-        for (int i = 0; i < n; i++)
-            ans[i] = mp[i];
+        unordered_map<int, int> mp;
+
+        long long ans = 0;
+        while (!pq.empty() && k > 0)
+        {
+            int val = pq.top().first;
+            int row = pq.top().second;
+            pq.pop();
+
+            if (mp[row] < limits[row])
+            {
+                ans += val;
+                mp[row]++;
+                k--;
+            }
+        }
 
         return ans;
     }
