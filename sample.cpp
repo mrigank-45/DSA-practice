79a6c48da2c69ec33adace3c5f8323ed5569e8f6
mrigank-45 <mrigankshukla2015@gmail.com>
2025-08-01 16:45:44 +0530

    Maximize the Number of Target Nodes After Connecting Trees I

--- sample.cpp
+++ sample.cpp
@@ -4,47 +4,68 @@ using namespace std;
 class Solution
 {
 public:
-    int findMinimumTime(vector<int> &strength, int k)
+    int bfs(unordered_map<int, vector<int>> mp, int k, int node)
     {
-        int n = strength.size();
-        sort(strength.begin(), strength.end());
-
-        int t = 0, x = 1, i = 0, e = 0;
-
-        while (i < n)
+        if (k < 0)
+            return 0;
+        int ans = 0;
+        queue<int> q;
+        q.push(node);
+        unordered_set<int> visited;
+        while (!q.empty())
         {
-            if (e >= strength[i])
+            int size = q.size();
+            ans += size;
+            for (int i = 0; i < size; i++)
             {
-                int j = i + 1;
-                while (j < n)
+                int temp = q.front();
+                q.pop();
+                visited.insert(temp);
+                for (auto it : mp[temp])
                 {
-                    if (e >= strength[j])
-                    {
-                        j++;
-                    }
-                    else
-                    {
-                        break;
-                    }
+                    if (visited.find(it) != visited.end())
+                        continue;
+                    q.push(it);
                 }
-                j--;
-                while (i < j)
-                {
-                    strength[j] = strength[j - 1];
-                    j--;
-                }
-                i++;
-                e = 0;
-                x += k;
             }
-            if (i == n)
+            k--;
+            if (k < 0)
                 break;
-
-            int diff = strength[i] - e;
-            int op = (diff / x) + (diff % x != 0);
-            t += op;
-            e += op * x;
         }
-        return t;
+        return ans;
+    }
+    vector<int> maxTargetNodes(vector<vector<int>> &edges1, vector<vector<int>> &edges2, int k)
+    {
+        int n = edges1.size() + 1;
+        int m = edges2.size() + 1;
+        unordered_map<int, vector<int>> mp1;
+        unordered_map<int, vector<int>> mp2;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            mp1[edges1[i][0]].push_back(edges1[i][1]);
+            mp1[edges1[i][1]].push_back(edges1[i][0]);
+        }
+
+        for (int i = 0; i < m - 1; i++)
+        {
+            mp2[edges2[i][0]].push_back(edges2[i][1]);
+            mp2[edges2[i][1]].push_back(edges2[i][0]);
+        }
+
+        int maxi = 0;
+        for (int i = 0; i < m; i++)
+        {
+            if (k < 0)
+                break;
+            maxi = max(maxi, bfs(mp2, k - 1, i));
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < n; i++)
+        {
+            ans.push_back(bfs(mp1, k, i) + maxi);
+        }
+        return ans;
     }
 };
--- test.cpp
+++ test.cpp
@@ -1,49 +1,98 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <queue>
+#include <unordered_set>
+#include <algorithm>
 using namespace std;
 
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
 class Solution
 {
 public:
-    int solve(TreeNode *root, int prev)
+    vector<int> maxTargetNodes(vector<vector<int>> &edges1, vector<vector<int>> &edges2, int k)
     {
-        if (root == NULL)
-            return 0;
-
-        int ans = 0;
-
-        // not pick
-        int sum1 = 0, sum2 = 0;
-        if (root->left)
-            sum1 = solve(root->left, 0);
-        if (root->right)
-            sum2 = solve(root->right, 0);
-
-        ans = max(ans, sum1 + sum2);
-
-        // pick
-        if (prev == 0)
+        int n = 0, m = 0;
+        for (auto &e : edges1)
         {
-            int sum1 = 0, sum2 = 0;
-            if (root->left)
-                sum1 = solve(root->left, 1);
-            if (root->right)
-                sum2 = solve(root->right, 1);
-            ans = max(ans, root->val + sum1 + sum2);
+            n = max(n, max(e[0], e[1]));
+        }
+        n++;
+
+        for (auto &e : edges2)
+        {
+            m = max(m, max(e[0], e[1]));
+        }
+        m++;
+
+        vector<vector<int>> adjList1(n), adjList2(m);
+
+        for (auto &e : edges1)
+        {
+            adjList1[e[0]].push_back(e[1]);
+            adjList1[e[1]].push_back(e[0]);
         }
 
-        return ans;
+        for (auto &e : edges2)
+        {
+            adjList2[e[0]].push_back(e[1]);
+            adjList2[e[1]].push_back(e[0]);
+        }
+
+        vector<int> path1(n);
+        for (int i = 0; i < n; ++i)
+        {
+            path1[i] = bfs(i, adjList1, k);
+        }
+
+        int maxFound = 0;
+        if (k > 0)
+        {
+            for (int i = 0; i < m; ++i)
+            {
+                int count = bfs(i, adjList2, k - 1);
+                maxFound = max(maxFound, count);
+            }
+        }
+
+        for (int i = 0; i < n; ++i)
+        {
+            path1[i] += maxFound;
+        }
+
+        return path1;
     }
-    int rob(TreeNode *root)
+
+private:
+    int bfs(int start, vector<vector<int>> &adjList, int k)
     {
-        return solve(root, 0);
+        if (k == 0)
+            return 1;
+
+        unordered_set<int> visited;
+        queue<int> q;
+        q.push(start);
+        visited.insert(start);
+
+        int level = 0, nodesReached = 1;
+
+        while (!q.empty() && level < k)
+        {
+            int size = q.size();
+            for (int i = 0; i < size; ++i)
+            {
+                int node = q.front();
+                q.pop();
+                for (int neighbor : adjList[node])
+                {
+                    if (visited.count(neighbor) == 0)
+                    {
+                        visited.insert(neighbor);
+                        q.push(neighbor);
+                        ++nodesReached;
+                    }
+                }
+            }
+            ++level;
+        }
+
+        return nodesReached;
     }
 };
