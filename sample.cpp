40cf998f3a1027c7c3a304a99fb0968b092761ef
mrigank-45 <mrigankshukla2015@gmail.com>
2025-08-06 20:07:02 +0530

    Repeated String Match

--- sample.cpp
+++ sample.cpp
@@ -4,26 +4,76 @@ using namespace std;
 class Solution
 {
 public:
-    int totalHammingDistance(vector<int> &nums)
+    int repeatedStringMatch(string a, string b)
     {
-        int n = nums.size();
-        vector<int> v(32, 0);
-
-        for (int i = 0; i < 32; i++)
+        int n = a.size();
+        if (b.size() <= n)
         {
-            for (int j = 0; j < n; j++)
+            bool flag = false;
+            for (int i = 0; i <= a.size() - b.size(); i++)
             {
-                if (nums[j] & (1 << i))
+                if (a.substr(i, b.size()) == b)
                 {
-                    v[i]++;
+                    flag = true;
+                    break;
                 }
+            }
+            if (flag)
+            {
+                return 1;
+            }
+        }
+        if (b.size() < 2 * n)
+        {
+            bool flag2 = false;
+            string a1 = a + a;
+            for (int i = 0; i <= a1.size() - b.size(); i++)
+            {
+                if (a1.substr(i, b.size()) == b)
+                {
+                    flag2 = true;
+                    break;
+                }
+            }
+            if (flag2)
+            {
+                return 2;
             }
         }
         int ans = 0;
-        for (int i = 0; i < 32; i++)
+        string s1 = "", s2 = "";
+        int i = 0;
+        while (i < b.size())
         {
-            ans += v[i] * (n - v[i]);
+            if (b.substr(i, n) == a)
+            {
+                while (b.substr(i, n) == a)
+                {
+                    i += n;
+                    ans++;
+                    if (i + n > b.size())
+                    {
+                        break;
+                    }
+                }
+                s2 = b.substr(i, b.size() - i);
+                break;
+            }
+            else
+            {
+                s1 += b[i];
+                i++;
+            }
         }
-        return ans;
+
+        if (s1.size() < n && s2.size() < n && a.substr(n - s1.size(), s1.size()) == s1 && a.substr(0, s2.size()) == s2)
+        {
+            ans = ans + (s1.size() != 0) + (s2.size() != 0);
+            return ans;
+        }
+        else
+        {
+            return -1;
+        }
     }
 };
