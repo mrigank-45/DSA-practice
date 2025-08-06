0988a5c094c0b6bd695f3f43361a9efd83143704
mrigank-45 <mrigankshukla2015@gmail.com>
2025-08-06 21:54:14 +0530

    Partition to K Equal Sum Subsets

--- sample.cpp
+++ sample.cpp
@@ -4,76 +4,37 @@ using namespace std;
 class Solution
 {
 public:
-    int repeatedStringMatch(string a, string b)
+    bool solve(int i, vector<int> &nums, vector<int> &v, int n)
     {
-        int n = a.size();
-        if (b.size() <= n)
+        if (i == n)
         {
-            bool flag = false;
-            for (int i = 0; i <= a.size() - b.size(); i++)
+            for (int j = 1; j < v.size(); j++)
             {
-                if (a.substr(i, b.size()) == b)
+                if (v[j] != v[j - 1])
                 {
-                    flag = true;
-                    break;
+                    return false;
                 }
             }
-            if (flag)
-            {
-                return 1;
-            }
-        }
-        if (b.size() < 2 * n)
-        {
-            bool flag2 = false;
-            string a1 = a + a;
-            for (int i = 0; i <= a1.size() - b.size(); i++)
-            {
-                if (a1.substr(i, b.size()) == b)
-                {
-                    flag2 = true;
-                    break;
-                }
-            }
-            if (flag2)
-            {
-                return 2;
-            }
-        }
-        int ans = 0;
-        string s1 = "", s2 = "";
-        int i = 0;
-        while (i < b.size())
-        {
-            if (b.substr(i, n) == a)
-            {
-                while (b.substr(i, n) == a)
-                {
-                    i += n;
-                    ans++;
-                    if (i + n > b.size())
-                    {
-                        break;
-                    }
-                }
-                s2 = b.substr(i, b.size() - i);
-                break;
-            }
-            else
-            {
-                s1 += b[i];
-                i++;
-            }
+            return true;
         }
 
-        if (s1.size() < n && s2.size() < n && a.substr(n - s1.size(), s1.size()) == s1 && a.substr(0, s2.size()) == s2)
+        for (int j = 0; j < v.size(); j++)
         {
-            ans = ans + (s1.size() != 0) + (s2.size() != 0);
-            return ans;
+            v[j] += nums[i];
+            if (solve(i+1, nums, v, n))
+            {
+                return true;
+            }
+            v[j] -= nums[i];
         }
-        else
-        {
-            return -1;
-        }
+        return false;
+    }
+    bool canPartitionKSubsets(vector<int> &nums, int k)
+    {
+        if (k == 1)
+            return true;
+        int n = nums.size();
+        vector<int> v(k, 0);
+        return solve(0, nums, v, n);
     }
 };
