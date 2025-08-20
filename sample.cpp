1f0d7689c03d2706ceffe8cd9c1c05933afdf3ef
mrigank-45 <mrigankshukla2015@gmail.com>
2025-08-20 22:03:47 +0530

    H-Index II

--- sample.cpp
+++ sample.cpp
@@ -1,157 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node
+class Solution
 {
 public:
-    int data;
-    Node *next;
-
-    Node(int d)
+    int hIndex(vector<int> &citations)
     {
-        this->data = d;
-        this->next = NULL;
-    }
-
-    ~Node()
-    {
-        int value = this->data;
-        if (this->next != NULL)
+        int n = citations.size();
+        int ans = 0, l = 0, h = n - 1;
+        while (l <= h)
         {
-            delete next;
-            next = NULL;
-        }
-    }
-};
-
-class MyLinkedList
-{
-public:
-    Node *head, *tail;
-    MyLinkedList()
-    {
-        head = NULL;
-        tail = NULL;
-    }
-
-    int get(int index)
-    {
-        if (index < 0 || head == NULL)
-        {
-            return -1;
-        }
-        Node *current = head;
-        while (index--)
-        {
-            current = current->next;
-            if (current == NULL)
+            int mid = (l + h) / 2;
+            if (n - mid >= citations[mid])
             {
-                return -1;
+                if (citations[mid] > ans)
+                {
+                    ans = citations[mid];
+                }
+                l = mid + 1;
+            }
+            else
+            {
+                if (citations[mid] >= n - mid && n - mid > ans)
+                {
+                    ans = n - mid;
+                }
+                h = mid - 1;
             }
         }
-        return current->data;
-    }
-
-    void addAtHead(int val)
-    {
-        Node *newNode = new Node(val);
-        if (head == NULL)
-        {
-            head = newNode;
-            tail = newNode;
-            newNode->next = NULL;
-        }
-        else
-        {
-            newNode->next = head;
-            head = newNode;
-        }
-    }
-
-    void addAtTail(int val)
-    {
-        Node *newNode = new Node(val);
-        if (tail == NULL)
-        {
-            head = newNode;
-            tail = newNode;
-            newNode->next = NULL;
-        }
-        else
-        {
-            tail->next = newNode;
-            tail = newNode;
-            newNode->next = NULL;
-        }
-    }
-
-    void addAtIndex(int index, int val)
-    {
-        if (index == 0)
-        {
-            addAtHead(val);
-            return;
-        }
-        
-        index--;
-
-        Node *current = head;
-        while (index--)
-        {
-            current = current->next;
-            if (current == NULL)
-            {
-                return;
-            }
-        }
-        if (current == NULL)
-        {
-            return;
-        }
-
-        Node *newNode = new Node(val);
-        newNode->next = current->next;
-        current->next = newNode;
-
-        if (newNode->next == NULL)
-        {
-            tail = newNode;
-        }
-    }
-
-    void deleteAtIndex(int index)
-    {
-        if(head == NULL || index < 0)
-        {
-            return;
-        }
-        if (index == 0)
-        {
-            head = head->next;
-            if (head == NULL)
-            {
-                tail = NULL;
-            }
-            return;
-        }
-        index--;
-        Node *current = head;
-        while (index--)
-        {
-            current = current->next;
-            if (current->next == NULL)
-            {
-                return;
-            }
-        }
-        if (current->next == NULL)
-        {
-            return;
-        }
-        current->next = current->next->next;
-        if (current->next == NULL)
-        {
-            tail = current;
-        }
+        return ans;
     }
 };
