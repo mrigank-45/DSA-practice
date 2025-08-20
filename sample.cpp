0c31ae475cf400f2328a189cdf34226f94a135ed
mrigank-45 <mrigankshukla2015@gmail.com>
2025-08-20 22:33:53 +0530

    Minesweeper

--- sample.cpp
+++ sample.cpp
@@ -4,30 +4,77 @@ using namespace std;
 class Solution
 {
 public:
-    int hIndex(vector<int> &citations)
+    vector<vector<char>> updateBoard(vector<vector<char>> &board, vector<int> &click)
     {
-        int n = citations.size();
-        int ans = 0, l = 0, h = n - 1;
-        while (l <= h)
+        int n = board.size();
+        int m = board[0].size();
+        if (board[click[0]][click[1]] == 'M')
         {
-            int mid = (l + h) / 2;
-            if (n - mid >= citations[mid])
+            board[click[0]][click[1]] = 'X';
+            return board;
+        }
+        queue<pair<int, int>> q;
+        q.push({click[0], click[1]});
+        unordered_set<string> visited;
+        visited.insert(to_string(click[0]) + to_string(click[1]));
+        bool flag = false;
+        while (!q.empty())
+        {
+            int size = q.size();
+            while (size--)
             {
-                if (citations[mid] > ans)
+                int x = q.front().first, y = q.front().second;
+                q.pop();
+                if (board[x][y] == 'M')
                 {
-                    ans = citations[mid];
+                    board[x][y] = 'X';
+                    flag = true;
+                    continue;
                 }
-                l = mid + 1;
+                vector<int> dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+                vector<int> dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+                int count = 0;
+                for (int i = 0; i < 8; i++)
+                {
+                    int newX = x + dx[i];
+                    int newY = y + dy[i];
+                    if (newX >= 0 && newX < n && newY >= 0 && newY < m)
+                    {
+                        if (board[newX][newY] == 'M')
+                        {
+                            count++;
+                        }
+                    }
+                }
+                if (count > 0)
+                {
+                    board[x][y] = count + '0';
+                }
+                else
+                {
+                    board[x][y] = 'B';
+                    for (int i = 0; i < 8; i++)
+                    {
+                        int newX = x + dx[i];
+                        int newY = y + dy[i];
+                        if (newX >= 0 && newX < n && newY >= 0 && newY < m)
+                        {
+                            string pos = to_string(newX) + to_string(newY);
+                            if (visited.find(pos) == visited.end())
+                            {
+                                visited.insert(pos);
+                                q.push({newX, newY});
+                            }
+                        }
+                    }
+                }
             }
-            else
+            if (flag)
             {
-                if (citations[mid] >= n - mid && n - mid > ans)
-                {
-                    ans = n - mid;
-                }
-                h = mid - 1;
+                break;
             }
         }
-        return ans;
+
+        return board;
     }
 };
