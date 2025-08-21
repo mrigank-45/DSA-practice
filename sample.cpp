2c2f8a6bec6d70054c81329d7e55c126edf93fe6
mrigank-45 <mrigankshukla2015@gmail.com>
2025-08-21 19:28:08 +0530

    Minimum Time Difference

--- sample.cpp
+++ sample.cpp
@@ -4,77 +4,22 @@ using namespace std;
 class Solution
 {
 public:
-    vector<vector<char>> updateBoard(vector<vector<char>> &board, vector<int> &click)
+    int findMinDifference(vector<string> &timePoints)
     {
-        int n = board.size();
-        int m = board[0].size();
-        if (board[click[0]][click[1]] == 'M')
-        {
-            board[click[0]][click[1]] = 'X';
-            return board;
-        }
-        queue<pair<int, int>> q;
-        q.push({click[0], click[1]});
-        unordered_set<string> visited;
-        visited.insert(to_string(click[0]) + to_string(click[1]));
-        bool flag = false;
-        while (!q.empty())
-        {
-            int size = q.size();
-            while (size--)
-            {
-                int x = q.front().first, y = q.front().second;
-                q.pop();
-                if (board[x][y] == 'M')
-                {
-                    board[x][y] = 'X';
-                    flag = true;
-                    continue;
-                }
-                vector<int> dx = {-1, -1, -1, 0, 0, 1, 1, 1};
-                vector<int> dy = {-1, 0, 1, -1, 1, -1, 0, 1};
-                int count = 0;
-                for (int i = 0; i < 8; i++)
-                {
-                    int newX = x + dx[i];
-                    int newY = y + dy[i];
-                    if (newX >= 0 && newX < n && newY >= 0 && newY < m)
-                    {
-                        if (board[newX][newY] == 'M')
-                        {
-                            count++;
-                        }
-                    }
-                }
-                if (count > 0)
-                {
-                    board[x][y] = count + '0';
-                }
-                else
-                {
-                    board[x][y] = 'B';
-                    for (int i = 0; i < 8; i++)
-                    {
-                        int newX = x + dx[i];
-                        int newY = y + dy[i];
-                        if (newX >= 0 && newX < n && newY >= 0 && newY < m)
-                        {
-                            string pos = to_string(newX) + to_string(newY);
-                            if (visited.find(pos) == visited.end())
-                            {
-                                visited.insert(pos);
-                                q.push({newX, newY});
-                            }
-                        }
-                    }
-                }
-            }
-            if (flag)
-            {
-                break;
-            }
-        }
+        int n = timePoints.size();
+        vector<int> v(n, -1);
 
-        return board;
+        for (int i = 0; i < n; i++)
+        {
+            int minutes = (stoi(timePoints[i].substr(0, 2)) * 60) + stoi(timePoints[i].substr(3, 2));
+            v[i] = minutes;
+        }
+        sort(v.begin(), v.end());
+        int minDiff = ((24 * 60) + v[0]) - v[n - 1];
+        for(int i = 1; i < n; i++)
+        {
+            minDiff = min(minDiff, v[i] - v[i - 1]);
+        }
+        return minDiff;
     }
 };
