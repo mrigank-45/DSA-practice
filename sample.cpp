c05a19cbd8956b41ec3c1de860aab42878500102
mrigank-45 <mrigankshukla2015@gmail.com>
2025-08-25 17:55:20 +0530

    Most Profit Assigning Work

--- sample.cpp
+++ sample.cpp
@@ -4,39 +4,26 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> prefix;
-    vector<vector<double>> dp;
-
-    double solve(int i, int k, vector<int> &nums)
+    int maxProfitAssignment(vector<int> &difficulty, vector<int> &profit, vector<int> &worker)
     {
-        int n = nums.size();
-        if (k == 1)
-        {
-            return (prefix[n] - prefix[i]) * 1.0 / (n - i);
-        }
-
-        if (dp[i][k] >= 0)
-            return dp[i][k];
-
-        double ans = 0;
-        for (int j = i + 1; j <= n - k + 1; j++)
-        {
-            double avg = (prefix[j] - prefix[i]) * 1.0 / (j - i);
-            ans = max(ans, avg + solve(j, k - 1, nums));
-        }
-
-        return dp[i][k] = ans;
-    }
-
-    double largestSumOfAverages(vector<int> &nums, int k)
-    {
-        int n = nums.size();
-        prefix.assign(n + 1, 0);
+        int n = difficulty.size(), m = worker.size(), ans = 0;
+        vector<pair<int, int>> v;
         for (int i = 0; i < n; i++)
-            prefix[i + 1] = prefix[i] + nums[i];
-        
-        dp.assign(n, vector<double>(k + 1, -1));
-        
-        return solve(0, k, nums);
+        {
+            v.push_back({profit[i], difficulty[i]});
+        }
+        sort(v.rbegin(), v.rend());
+        sort(worker.rbegin(), worker.rend());
+        int i = 0, j = 0;
+        while (i < m && j < n)
+        {
+            while (i < m && worker[i] >= v[j].second)
+            {
+                ans += v[j].first;
+                i++;
+            }
+            j++;
+        }
+        return ans;
     }
 };
