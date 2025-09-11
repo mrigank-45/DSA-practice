ba640e7cf89742f03da06ab7b328082934fe4141
mrigank-45 <mrigankshukla2015@gmail.com>
2025-09-12 01:21:04 +0530

    Maximum Distance Between a Pair of Values

--- sample.cpp
+++ sample.cpp
@@ -4,23 +4,21 @@ using namespace std;
 class Solution
 {
 public:
-    int minDeletion(vector<int> &nums)
+    int maxDistance(vector<int> &nums1, vector<int> &nums2)
     {
-        int n = nums.size();
-        int ans = 0, parity = 1;
-        for (int i = 1; i < n; i++)
+        int n = nums1.size(), m = nums2.size();
+        reverse(nums2.begin(), nums2.end());
+        int ans = 0;
+        for (int i = 0; i < n; i++)
         {
-            if (parity % 2 == 1 && nums[i] == nums[i - 1])
+            int val = nums1[i];
+            int index = lower_bound(nums2.begin(), nums2.end(), val) - nums2.begin();
+            index = m - 1 - index;
+            if (index != m)
             {
-                ans++;
-            }
-            else
-            {
-                parity++;
+                ans = max(ans, index - i);
             }
         }
-        if(parity % 2 == 1)
-            ans++;  
         return ans;
     }
 };
