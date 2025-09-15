ecc6a2bc88998e2d58a4bdc255056cf72887e53f
mrigank-45 <mrigankshukla2015@gmail.com>
2025-09-15 17:16:12 +0530

    Minimum Number of Vertices to Reach All Nodes

--- sample.cpp
+++ sample.cpp
@@ -1,54 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int minNumberOfFrogs(string cOf)
+class Solution
 {
-    int onGoing = 0;
-    int maxOngoing = 0;
-    vector<int> curr(5, 0);
-    for (auto a : cOf)
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>> &edges)
     {
-        if (a == 'c')
+        vector<int> indegree(n,0);
+        for(auto it:edges)
         {
-            curr[0]++;
-            onGoing++;
-            maxOngoing = max(maxOngoing, onGoing);
+            indegree[it[1]]++;
         }
-        else
+        vector<int> ans;
+        for(int i=0;i<n;i++)
         {
-            if (a == 'r')
-            {
-                if (curr[0] <= curr[1])
-                    return -1;
-                curr[1]++;
-            }
-            else if (a == 'o')
-            {
-                if (curr[1] <= curr[2])
-                    return -1;
-                curr[2]++;
-            }
-            else if (a == 'a')
-            {
-                if (curr[2] <= curr[3])
-                    return -1;
-                curr[3]++;
-            }
-            else if (a == 'k')
-            {
-                if (curr[3] <= curr[4])
-                    return -1;
-                curr[4]++;
-                onGoing--;
-            }
+            if(indegree[i]==0)
+                ans.push_back(i);
         }
+        return ans;
     }
-    int total = curr[0];
-    for (int i = 1; i < 5; i++)
-    {
-        if (curr[i] != total)
-            return -1;
-    }
-
-    return maxOngoing;
-}
+};
