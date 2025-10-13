b23d233e1d472047d4f2cee8e75924a8f84923bd
mrigank-45 <mrigankshukla2015@gmail.com>
2025-10-13 18:23:13 +0530

    Find in Mountain Array

--- sample.cpp
+++ sample.cpp
@@ -1,21 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class MountainArray
+{
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        int n = arr.size();
+    int get(int index);
+    int length();
+};
+
+class Solution
+{
+public:
+    int peakIndexInMountainArray(MountainArray &arr)
+    {
+        int n = arr.length();
         int start = 0, end = n - 1;
 
-        while(start<end){
-            int mid = start + (end - start)/2;
-            if(arr[mid] < arr[mid+1]){
+        while (start < end)
+        {
+            int mid = start + (end - start) / 2;
+            if (arr.get(mid) < arr.get(mid + 1))
+            {
                 start = mid + 1;
-            } else {
+            }
+            else
+            {
                 end = mid;
             }
         }
         return start;
-        
+    }
+    int findInMountainArray(int target, MountainArray &mountainArr)
+    {
+        int n = mountainArr.length();
+        int peak = peakIndexInMountainArray(mountainArr);
+        if(mountainArr.get(peak) == target) return peak;
+        if(target > mountainArr.get(peak)) return -1;
+        int start = 0, end = peak;
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if (mountainArr.get(mid) == target)
+            {
+                return mid;
+            }
+            else if (mountainArr.get(mid) < target)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        start = peak, end = n - 1;
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if (mountainArr.get(mid) == target)
+            {
+                return mid;
+            }
+            else if (mountainArr.get(mid) < target)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return -1;
     }
 };
