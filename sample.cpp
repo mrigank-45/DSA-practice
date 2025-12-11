16d48550697c0435c696aaf8c9298d11ed105002
Mrigank Mani Shukla <mriganksmacbook@Mriganks-MacBook-Air.local>
2025-12-12 02:03:38 +0530

    Design Add and Search Words Data Structure

--- sample.cpp
+++ sample.cpp
@@ -1,20 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
+class WordDictionary {
 public:
-    string shiftingLetters(string s, vector<int> &shifts)
-    {
-        int n = shifts.size();
-        int curr = 0;
-
-        for (int i = n - 1; i >= 0; i--)
-        {
-            curr += shifts[i];
-            curr %= 26;
-            s[i] = (s[i] - 'a' + curr) % 26 + 'a';
+    unordered_set<string> st;
+    WordDictionary() {
+    }
+    
+    void addWord(string word) {
+        st.insert(word);
+    }
+    
+    bool search(string word) {
+        if (st.count(word)) return true;
+        int n = word.size();
+        int p1 = -1, p2 = -1;
+        for(int i = 0; i < n; i++) {
+            if (word[i] == '.') {
+                if (p1 == -1) p1 = i;
+                else p2 = i;
+            }
         }
-        return s;
+        if (p1 == -1) return false;
+        if(p2 == -1) {
+            for(int i = 0; i < 26; i++) {
+                string modified = word;
+                modified[p1] = 'a' + i;
+                if (st.count(modified)) return true;
+            }
+        } else {
+            for(int i = 0; i < 26; i++) {
+                for(int j = 0; j < 26; j++) {
+                    string modified = word;
+                    modified[p1] = 'a' + i;
+                    modified[p2] = 'a' + j;
+                    if (st.count(modified)) return true;
+                }
+            }
+        }
+        return false;
     }
 };
