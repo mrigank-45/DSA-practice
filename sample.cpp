6d9c8572c73eee970b6115b9cbe7c4d2e765bd11
Mrigank Mani Shukla <mriganksmacbook@Mriganks-MacBook-Air.local>
2025-12-12 04:36:45 +0530

    Unique Substrings in Wraparound String

--- sample.cpp
+++ sample.cpp
@@ -3,20 +3,33 @@ using namespace std;
 
 class Solution {
 public:
-    bool check(vector<int> nums, int l, int r, int sum1, int sum2, bool turn) {
-        if (l > r) {
-            return sum1 >= sum2;
+    int findSubstringInWraproundString(string s) {
+        int n = s.size();
+        int ans = 0;
+        int curr = 1;
+        string curr_str = "";
+        unordered_set<string> st;
+        vector<bool> count(26, 0);
+        for (int i = 0; i < n; ++i) {
+            if (i > 0 && ((s[i] - s[i - 1]) + 26) % 26 == 1) {
+                curr++;
+                curr_str += s[i];
+
+            } else {
+                if(curr>1 && st.find(curr_str) == st.end()) {
+                    ans += (curr * (curr - 1)) / 2;
+                    st.insert(curr_str);
+                }
+                curr = 1;
+                curr_str = "";
+                curr_str += s[i];
+            }
+            if (!count[s[i] - 'a']) {
+                count[s[i] - 'a'] = 1;
+                ans++;
+            }
         }
-        if (turn) {
-            return check(nums, l + 1, r, sum1 + nums[l], sum2, !turn) ||
-                   check(nums, l, r - 1, sum1 + nums[r], sum2, !turn);
-        } else {
-            return check(nums, l + 1, r, sum1, sum2 + nums[l], !turn) &&
-                   check(nums, l, r - 1, sum1, sum2 + nums[r], !turn);
-        }
-    }
-    bool predictTheWinner(vector<int>& nums) {
-        int n = nums.size();
-        return check(nums, 0, n - 1, 0, 0, true);
+        if(curr>1 && st.find(curr_str) == st.end()) ans += (curr * (curr - 1)) / 2;
+        return ans;
     }
 };
