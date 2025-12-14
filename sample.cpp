4b86258b173178587d4eaf07db790b0e9f8c4a14
Mrigank Mani Shukla <mriganksmacbook@Mriganks-MacBook-Air.local>
2025-12-14 19:04:59 +0530

    Find And Replace in String

--- sample.cpp
+++ sample.cpp
@@ -1,30 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution
 {
 public:
-    TreeNode *pruneTree(TreeNode *root)
+    string findReplaceString(string s, vector<int> &indices, vector<string> &sources, vector<string> &targets)
     {
-        if (!root)
-            return nullptr;
+        int n = s.size(), k = indices.size(), i = 0, j = 0;
+        string ans = "";
 
-        root->left = pruneTree(root->left);
-        root->right = pruneTree(root->right);
+        vector<pair<int,pair<string,string>> > v;
+        for(int idx=0; idx<k; idx++)
+        {
+            v.push_back({indices[idx], {sources[idx], targets[idx]}});
+        }
+        sort(v.begin(), v.end());
 
-        if (!root->left && !root->right && root->val == 0)
-            return nullptr;
+        while (i < n)
+        {
+            if (j<k && i == v[j].first)
+            {
+                if (s.substr(i, v[j].second.first.length()) == v[j].second.first)
+                {
+                    ans += v[j].second.second;
+                    i += v[j].second.first.length();
+                    j++;
+                }
+                else
+                {
+                    ans += s[i];
+                    i++;
+                    j++;
+                }
+            }
+            else
+            {
+                ans += s[i];
+                i++;
+            }
+        }
 
-        return root;
+        return ans;
     }
 };
