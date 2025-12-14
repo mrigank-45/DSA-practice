063538793e209060c634f4571b699a34ef0bdad8
Mrigank Mani Shukla <mriganksmacbook@Mriganks-MacBook-Air.local>
2025-12-14 12:48:11 +0530

    Binary Tree Pruning

--- sample.cpp
+++ sample.cpp
@@ -1,38 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution
 {
 public:
-    struct Compare
+    TreeNode *pruneTree(TreeNode *root)
     {
-        bool operator()(const string &p1, const string &p2)
-        {
-            if (p1.length() == p2.length())
-            {
-                int i = 0;
-                while(i<p1.length()){
-                    if(p1[i] != p2[i]){
-                        return p1[i] > p2[i];
-                    }
-                    i++;
-                }
-                return true;
-            }
-            return p1.length() > p2.length();
-        }
-    };
-    string kthLargestNumber(vector<string> &nums, int k)
-    {
-        int n = nums.size();
-        priority_queue<string,vector<string>,Compare> pq;
+        if (!root)
+            return nullptr;
 
-        for(int i =0; i<n;i++){
-            pq.push(nums[i]);
-            if(pq.size()>k){
-                pq.pop();
-            }
-        }
-        return pq.top();
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+
+        if (!root->left && !root->right && root->val == 0)
+            return nullptr;
+
+        return root;
     }
 };
