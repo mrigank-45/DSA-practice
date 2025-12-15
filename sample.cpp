3c7c6001e347b17fd9656dfd15ce700da5a2c23c
Mrigank Mani Shukla <mriganksmacbook@Mriganks-MacBook-Air.local>
2025-12-15 22:02:57 +0530

    Number of Smooth Descent Periods of a Stock

--- sample.cpp
+++ sample.cpp
@@ -1,45 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    string findReplaceString(string s, vector<int> &indices, vector<string> &sources, vector<string> &targets)
-    {
-        int n = s.size(), k = indices.size(), i = 0, j = 0;
-        string ans = "";
+    long long getDescentPeriods(vector<int>& prices) {
+        int n = prices.size();
+        long long ans = 1;
+        int curr = 1;
 
-        vector<pair<int,pair<string,string>> > v;
-        for(int idx=0; idx<k; idx++)
-        {
-            v.push_back({indices[idx], {sources[idx], targets[idx]}});
-        }
-        sort(v.begin(), v.end());
-
-        while (i < n)
-        {
-            if (j<k && i == v[j].first)
-            {
-                if (s.substr(i, v[j].second.first.length()) == v[j].second.first)
-                {
-                    ans += v[j].second.second;
-                    i += v[j].second.first.length();
-                    j++;
-                }
-                else
-                {
-                    ans += s[i];
-                    i++;
-                    j++;
-                }
+        for(int i = 1; i<n; i++){
+            if(prices[i] == prices[i-1] - 1){
+                curr++;
             }
-            else
-            {
-                ans += s[i];
-                i++;
+            else{
+                curr = 1;
             }
+            ans += curr;
         }
-
         return ans;
+        
     }
 };
