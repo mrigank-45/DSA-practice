c13e1da41c81a4dcb246bb713c3f29d1d5672446
Mrigank Mani Shukla <mriganksmacbook@Mriganks-MacBook-Air.local>
2025-12-29 18:06:19 +0530

    Partition Array for Maximum Sum

--- sample.cpp
+++ sample.cpp
@@ -3,20 +3,29 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> findLonely(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int,int> mp;
-        for(int i =0; i<n;i++){
-            mp[nums[i]]++;
+    int solve(vector<int>& arr, int k, int n, int i, vector<int>& dp) {
+        if (i == n) {
+            return 0;
         }
-        vector<int> ans;
-        for(auto it : mp){
-            if(it.second==1){
-                if((mp.find(it.first + 1) == mp.end()) && (mp.find(it.first - 1) == mp.end())){
-                    ans.push_back(it.first);
-                }
-            }
+        if (dp[i] != -1) {
+            return dp[i];
         }
-        return ans;
+
+        int max_val = 0;
+        int max_ans = 0;
+
+        for (int j = i; j < min(n, i + k); j++) {
+            int len = j - i + 1;
+            max_val = max(max_val, arr[j]);
+            int sum = (len * max_val) + solve(arr, k, n, j + 1, dp);
+            max_ans = max(max_ans, sum);
+        }
+        return dp[i] = max_ans;
+    }
+
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> dp(n, -1);
+        return solve(arr, k, n, 0, dp);
     }
 };
