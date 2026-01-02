bb8a9abdb32b8dcfba6977c0f9f6e0d2c9ec9c31
Mrigank Mani Shukla <mriganksmacbook@Mriganks-MacBook-Air.local>
2026-01-03 03:34:30 +0530

    Maximum Number of Non-Overlapping Subarrays With Sum Equals Target

--- sample.cpp
+++ sample.cpp
@@ -3,29 +3,22 @@ using namespace std;
 
 class Solution {
 public:
-    int solve(vector<int>& arr, int k, int n, int i, vector<int>& dp) {
-        if (i == n) {
-            return 0;
+    int maxNonOverlapping(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int,int> mp; // sum before index -> index
+        mp[0] = 0;
+        int sum = 0, ans = 0, prev = -1;
+        for(int i =0; i<n; i++){
+            sum+= nums[i];
+            if(mp.find(sum - target) != mp.end()){
+                if(mp[sum-target]>prev){
+                    ans++;
+                    prev = i;
+                }
+            }
+            mp[sum] = i + 1;
         }
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-
-        int max_val = 0;
-        int max_ans = 0;
-
-        for (int j = i; j < min(n, i + k); j++) {
-            int len = j - i + 1;
-            max_val = max(max_val, arr[j]);
-            int sum = (len * max_val) + solve(arr, k, n, j + 1, dp);
-            max_ans = max(max_ans, sum);
-        }
-        return dp[i] = max_ans;
-    }
-
-    int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        int n = arr.size();
-        vector<int> dp(n, -1);
-        return solve(arr, k, n, 0, dp);
+        return ans;
+        
     }
 };
