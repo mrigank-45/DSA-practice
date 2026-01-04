10f4938372dc903d070ea0d988dffc9dec65c8f0
Mrigank Mani Shukla <mriganksmacbook@Mriganks-MacBook-Air.local>
2026-01-04 23:29:52 +0530

    Greatest Sum Divisible by Three

--- sample.cpp
+++ sample.cpp
@@ -3,27 +3,44 @@ using namespace std;
 
 class Solution {
 public:
-    int edgeScore(vector<int>& edges) {
-        int n = edges.size();
-        unordered_map<int,vector<int>> mp;
+    int maxSumDivThree(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        vector<int> rem1, rem2;
         for(int i =0; i<n;i++){
-            mp[edges[i]].push_back(i);
-        }
-        long long max_score = 0;
-        int ans = 0;
-        for(auto it: mp){
-            long long score = 0;
-            for(int i = 0; i<it.second.size();i++){
-                score += it.second[i];
+            sum += nums[i];
+            if(nums[i]%3 == 1){
+                rem1.push_back(nums[i]);
             }
-            if(score>max_score){
-                max_score = score;
-                ans = it.first;
-            }
-            if(score==max_score && it.first<ans){
-                ans = it.first;
+            if(nums[i]%3 == 2){
+                rem2.push_back(nums[i]);
             }
         }
-        return ans;
+        sort(rem1.begin(), rem1.end());
+        sort(rem2.begin(), rem2.end());
+
+        if(sum%3 == 0){
+            return sum;
+        }
+        else if(sum%3 == 1){
+            int op1 = INT_MIN, op2 = INT_MIN;
+            if(rem1.size()>=1){
+                op1 = sum - rem1[0];
+            }
+            if(rem2.size()>=2){
+                op2 = sum - rem2[0] - rem2[1];
+            }
+            return max(op1, op2);
+        }
+        else{
+            int op1 = INT_MIN, op2 = INT_MIN;
+            if(rem2.size()>=1){
+                op1 = sum - rem2[0];
+            }
+            if(rem1.size()>=2){
+                op2 = sum - rem1[0] - rem1[1];
+            }
+            return max(op1, op2);
+        }
     }
 };
