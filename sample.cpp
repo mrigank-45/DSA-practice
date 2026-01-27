ce1742c9c904d7000cdc99d23d87eef9914b7a12
Mrigank Mani Shukla <mriganksmacbook@Mriganks-MacBook-Air.local>
2026-01-27 17:56:35 +0530

    Reduction Operations to Make the Array Elements Equal

--- sample.cpp
+++ sample.cpp
@@ -1,32 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    long long maximumImportance(int n, vector<vector<int>>& roads) {
-        unordered_map<int,int> deg;
-        for(int i =0; i<roads.size();i++){
-            deg[roads[i][0]]++;
-            deg[roads[i][1]]++;
-        }
-        vector<pair<int,int>> v;
-        for(auto it : deg){
-            v.push_back({it.second,it.first});
-        }
-        sort(v.rbegin(),v.rend());
-        unordered_map<int,int> mp;
-        int curr = n;
-
-        for(int i =0; i<v.size(); i++){
-            mp[v[i].second] = curr;
-            curr--;
-        }
-        long long ans = 0;
-        for(int i =0; i<roads.size();i++){
-            ans += mp[roads[i][0]];
-            ans += mp[roads[i][1]];
+    int reductionOperations(vector<int> &nums)
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        unordered_set<int> st;
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (st.find(nums[i]) == st.end())
+            {
+                st.insert(nums[i]);
+                ans += st.size() - 1;
+            }
+            else
+            {
+                ans += st.size() - 1;
+            }
         }
         return ans;
-
     }
 };
