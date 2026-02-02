ad5d261afced849cebbd82dd478474a82d13c67c
Mrigank Mani Shukla <mriganksmacbook@Mriganks-MacBook-Air.local>
2026-02-02 22:45:34 +0530

    Find the City With the Smallest Number of Neighbors at a Threshold Distance

--- sample.cpp
+++ sample.cpp
@@ -1,30 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int maxSubarrayLength(vector<int> &nums, int k)
-    {
-        int n = nums.size();
-        unordered_map<int, int> freq;
-
-        int i = 0, j = 1, ans = 1;
-        freq[nums[i]]++;
-
-        while (j < n)
-        {
-            freq[nums[j]]++;
-            
-            while(i<=j && freq[nums[j]]>k){
-                freq[nums[i]]--;
-                i++;
-            }
-
-            ans = max(ans, j - i + 1);
-            j++;
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& edge : edges) {
+            adj[edge[0]].push_back({edge[1], edge[2]});
+            adj[edge[1]].push_back({edge[0], edge[2]});
         }
 
-        return ans;
+        int minReachable = n; 
+        int bestCity = -1;
+
+        for (int i = 0; i < n; i++) {
+            
+            vector<int> dist(n, INT_MAX);
+            queue<int> q;
+
+            dist[i] = 0;
+            q.push(i);
+
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+
+                for (auto& neighbor : adj[u]) {
+                    int v = neighbor.first;
+                    int weight = neighbor.second;
+
+                    if (dist[u] + weight < dist[v]) {
+                        dist[v] = dist[u] + weight;
+                        
+                        if (dist[v] <= distanceThreshold) {
+                            q.push(v);
+                        }
+                    }
+                }
+            }
+
+            int count = 0;
+            for (int d : dist) {
+                if (d <= distanceThreshold && d > 0) {
+                    count++;
+                }
+            }
+
+            if (count <= minReachable) {
+                minReachable = count;
+                bestCity = i;
+            }
+        }
+
+        return bestCity;
     }
 };
