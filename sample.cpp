8c62dabc6c4356ce308cdd7910ce1a0ff6560db5
Mrigank Mani Shukla <mriganksmacbook@Mriganks-MacBook-Air.local>
2026-02-11 21:21:24 +0530

    Find Minimum Cost to Remove Array Elements

--- sample.cpp
+++ sample.cpp
@@ -1,57 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto& edge : edges) {
-            adj[edge[0]].push_back({edge[1], edge[2]});
-            adj[edge[1]].push_back({edge[0], edge[2]});
+    int solve(int curr, int prev, vector<int> &nums, int n, vector<vector<int>> &dp)
+    {
+        if (curr == n - 1)
+        {
+            return max(nums[n - 1], nums[prev]);
+        }
+        if (curr == n)
+        {
+            return nums[prev];
         }
 
-        int minReachable = n; 
-        int bestCity = -1;
-
-        for (int i = 0; i < n; i++) {
-            
-            vector<int> dist(n, INT_MAX);
-            queue<int> q;
-
-            dist[i] = 0;
-            q.push(i);
-
-            while (!q.empty()) {
-                int u = q.front();
-                q.pop();
-
-                for (auto& neighbor : adj[u]) {
-                    int v = neighbor.first;
-                    int weight = neighbor.second;
-
-                    if (dist[u] + weight < dist[v]) {
-                        dist[v] = dist[u] + weight;
-                        
-                        if (dist[v] <= distanceThreshold) {
-                            q.push(v);
-                        }
-                    }
-                }
-            }
-
-            int count = 0;
-            for (int d : dist) {
-                if (d <= distanceThreshold && d > 0) {
-                    count++;
-                }
-            }
-
-            if (count <= minReachable) {
-                minReachable = count;
-                bestCity = i;
-            }
+        if(dp[curr][prev] != -1){
+            return dp[curr][prev];
         }
 
-        return bestCity;
+        int c1 = max(nums[curr], nums[curr + 1]) + solve(curr + 2, prev, nums, n, dp);
+        int c2 = max(nums[prev], nums[curr]) + solve(curr + 2, curr + 1, nums, n, dp);
+        int c3 = max(nums[prev], nums[curr + 1]) + solve(curr + 2, curr, nums, n, dp);
+
+        return dp[curr][prev] = min(c1, min(c2, c3));
+    }
+    int minCost(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+
+        return solve(1,0,nums,n, dp);
     }
 };
